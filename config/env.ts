25dc1338f3bd5f3592ecfd2bca4ace4d14764863
macs <massimodemarchi91@gmail.com>
2021-07-12 23:31:07 +0200

    fix: Improve env variable check function

--- config/env.ts
+++ config/env.ts
@@ -37,6 +37,12 @@ export const WP_BASIC_AUTH_PASSWORD = checkEnv(
 
 function checkEnv(env: string | null | undefined, envName: string): string {
    if (env == null) {
+      if (process.browser) {
+         if (envName.startsWith('NEXT_PUBLIC')) {
+            console.warn(`environment variable "${envName}" is not defined`);
+         }
+         return env as any;
+      }
       throw new Error(`environment variable "${envName}" is not defined`);
    }
    return env;
