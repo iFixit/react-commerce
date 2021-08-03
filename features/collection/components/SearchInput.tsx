3c0138c20d20ac73543e360d9b7d1a9fedecd2a7
macs <massimodemarchi91@gmail.com>
2021-08-03 18:36:25 +0200

    feat: Add clear button to collection search input

--- features/collection/components/SearchInput.tsx
+++ features/collection/components/SearchInput.tsx
@@ -1,12 +1,15 @@
 import {
    chakra,
    Icon,
+   IconButton,
    Input,
    InputGroup,
    InputLeftElement,
+   InputRightElement,
 } from '@chakra-ui/react';
 import { useSearch } from '@lib/algolia';
 import * as React from 'react';
+import { HiXCircle } from 'react-icons/hi';
 import { RiSearchLine } from 'react-icons/ri';
 
 interface SearchInputProps {
@@ -15,6 +18,10 @@ interface SearchInputProps {
 
 export const SearchInput = chakra(({ className }: SearchInputProps) => {
    const [query, search] = useSearch();
+
+   const clearSearch = React.useCallback(() => {
+      search('');
+   }, [search]);
 
    return React.useMemo(
       () => (
@@ -29,8 +36,31 @@ export const SearchInput = chakra(({ className }: SearchInputProps) => {
                onChange={(event) => search(event.currentTarget.value)}
                value={query}
             />
+            {query.length > 0 && (
+               <InputRightElement>
+                  <IconButton
+                     variant="ghost"
+                     aria-label="clear search"
+                     onClick={clearSearch}
+                     _hover={{
+                        bg: 'transparent',
+                     }}
+                     _active={{
+                        bg: 'transparent',
+                     }}
+                     icon={
+                        <Icon
+                           as={HiXCircle}
+                           color="gray.300"
+                           transition="color 300ms"
+                           _hover={{ color: 'gray.500' }}
+                        />
+                     }
+                  />
+               </InputRightElement>
+            )}
          </InputGroup>
       ),
-      [className, query, search]
+      [className, clearSearch, query, search]
    );
 });
