0e83f23d9d09eaf99bcae1f5e1657882ff19c891
macs <massimodemarchi91@gmail.com>
2021-10-06 13:38:53 +0200

    refactor: Add links to product page

--- features/collection/components/sections/FilterableProductsSection/ProductList.tsx
+++ features/collection/components/sections/FilterableProductsSection/ProductList.tsx
@@ -8,6 +8,8 @@ import {
    Heading,
    HStack,
    Img,
+   LinkBox,
+   LinkOverlay,
    Stack,
    Text,
    VStack,
@@ -54,210 +56,216 @@ export function ProductListItem({ product }: ProductListItemProps) {
       : 0;
 
    return (
-      <Stack
-         key={product.handle}
-         direction={{
-            base: 'column',
-            sm: 'row',
-            md: 'column',
-            lg: 'row',
-         }}
-         spacing={{
-            base: 2,
-            sm: 4,
-         }}
-         py="7"
-         alignItems="flex-start"
-         px="4"
-      >
-         <AspectRatio
-            flexGrow={1}
-            flexShrink={0}
-            w={{
-               base: '100px',
-               sm: '160px',
-               md: '140px',
-               lg: '160px',
+      <LinkBox as="article">
+         <Stack
+            direction={{
+               base: 'column',
+               sm: 'row',
+               md: 'column',
+               lg: 'row',
             }}
-            ratio={1}
-         >
-            {product.product_image == null ? (
-               <Img
-                  sizes="180px"
-                  src={placeholderImageUrl}
-                  alt={product.title}
-               />
-            ) : (
-               <ShopifyImage
-                  src={product.product_image || placeholderImageUrl}
-                  alt={product.title}
-                  sizes="180px"
-               />
-            )}
-         </AspectRatio>
-         <VStack
-            align="flex-start"
             spacing={{
-               base: 4,
+               base: 2,
+               sm: 4,
             }}
-            flexShrink={1}
-            w="full"
+            py="7"
+            alignItems="flex-start"
+            px="4"
          >
-            <Heading
-               as="h2"
-               fontSize={{
-                  base: 'sm',
-                  sm: 'md',
-                  lg: 'lg',
+            <AspectRatio
+               flexGrow={1}
+               flexShrink={0}
+               w={{
+                  base: '100px',
+                  sm: '160px',
+                  md: '140px',
+                  lg: '160px',
                }}
+               ratio={1}
             >
-               {product.title}
-            </Heading>
-            <Text
-               noOfLines={3}
-               fontSize={{
-                  base: 'xs',
-                  sm: 'sm',
-                  lg: 'md',
+               {product.product_image == null ? (
+                  <Img
+                     sizes="180px"
+                     src={placeholderImageUrl}
+                     alt={product.title}
+                  />
+               ) : (
+                  <ShopifyImage
+                     src={product.product_image || placeholderImageUrl}
+                     alt={product.title}
+                     sizes="180px"
+                  />
+               )}
+            </AspectRatio>
+            <VStack
+               align="flex-start"
+               spacing={{
+                  base: 4,
                }}
+               flexShrink={1}
+               w="full"
             >
-               {product.body_html_safe}
-            </Text>
-            <HStack>
-               <Rating value={4} />
-               <Text>102</Text>
-            </HStack>
-            <Box>
-               <Flex
-                  wrap="wrap"
-                  mt="-1"
-                  sx={{
-                     '& > *': {
-                        mr: 1,
-                        mt: 1,
-                     },
+               <Heading
+                  as="h2"
+                  fontSize={{
+                     base: 'sm',
+                     sm: 'md',
+                     lg: 'lg',
                   }}
                >
-                  {product.inventory_quantity > 0 ? (
-                     <>
-                        {percentage > 0 && (
+                  {product.title}
+               </Heading>
+               <Text
+                  noOfLines={3}
+                  fontSize={{
+                     base: 'xs',
+                     sm: 'sm',
+                     lg: 'md',
+                  }}
+               >
+                  {product.body_html_safe}
+               </Text>
+               <HStack>
+                  <Rating value={4} />
+                  <Text>102</Text>
+               </HStack>
+               <Box>
+                  <Flex
+                     wrap="wrap"
+                     mt="-1"
+                     sx={{
+                        '& > *': {
+                           mr: 1,
+                           mt: 1,
+                        },
+                     }}
+                  >
+                     {product.inventory_quantity > 0 ? (
+                        <>
+                           {percentage > 0 && (
+                              <Badge
+                                 colorScheme="red"
+                                 textTransform="none"
+                                 borderRadius="lg"
+                                 px="2.5"
+                                 py="1"
+                              >
+                                 {percentage}% Off
+                              </Badge>
+                           )}
                            <Badge
-                              colorScheme="red"
+                              colorScheme="blue"
                               textTransform="none"
                               borderRadius="lg"
                               px="2.5"
                               py="1"
                            >
-                              {percentage}% Off
+                              Lifetime warranty
                            </Badge>
-                        )}
+                           <Badge
+                              colorScheme="blue"
+                              textTransform="none"
+                              borderRadius="lg"
+                              px="2.5"
+                              py="1"
+                           >
+                              Ship today if ordered by 5pm
+                           </Badge>
+                        </>
+                     ) : (
                         <Badge
-                           colorScheme="blue"
+                           colorScheme="gray"
                            textTransform="none"
                            borderRadius="lg"
                            px="2.5"
                            py="1"
                         >
-                           Lifetime warranty
+                           Sold out
                         </Badge>
-                        <Badge
-                           colorScheme="blue"
-                           textTransform="none"
-                           borderRadius="lg"
-                           px="2.5"
-                           py="1"
-                        >
-                           Ship today if ordered by 5pm
-                        </Badge>
-                     </>
-                  ) : (
-                     <Badge
-                        colorScheme="gray"
-                        textTransform="none"
-                        borderRadius="lg"
-                        px="2.5"
-                        py="1"
-                     >
-                        Sold out
-                     </Badge>
-                  )}
-               </Flex>
-            </Box>
-         </VStack>
-         <VStack
-            flexShrink={0}
-            align="flex-end"
-            alignSelf={{
-               base: 'flex-end',
-               sm: 'flex-start',
-               md: 'flex-end',
-               lg: 'flex-start',
-            }}
-         >
-            <VStack
-               align="flex-end"
-               spacing="0"
-               mt={{
-                  base: 2,
-               }}
-            >
-               <Text
-                  color={isDiscounted ? 'red.700' : 'inherit'}
-                  fontWeight="bold"
-                  fontSize="xl"
-                  lineHeight="1em"
-               >
-                  ${product.price}
-               </Text>
-               {isDiscounted && (
-                  <Text
-                     lineHeight="1em"
-                     textDecoration="line-through"
-                     color="gray.400"
-                  >
-                     ${product.compare_at_price}
-                  </Text>
-               )}
+                     )}
+                  </Flex>
+               </Box>
             </VStack>
-            <Stack
-               direction={{
-                  base: 'row-reverse',
-                  sm: 'column',
-                  md: 'row-reverse',
-                  lg: 'column',
-               }}
-               align={{
-                  base: 'center',
-                  sm: 'flex-end',
-                  md: 'center',
-                  lg: 'flex-end',
-               }}
-               spacing={{
-                  base: 3,
-                  sm: 2,
+            <VStack
+               flexShrink={0}
+               align="flex-end"
+               alignSelf={{
+                  base: 'flex-end',
+                  sm: 'flex-start',
+                  md: 'flex-end',
+                  lg: 'flex-start',
                }}
             >
-               <Button
-                  minW={{
-                     base: '100px',
-                     sm: '60px',
-                     md: '100px',
-                     lg: '60px',
+               <VStack
+                  align="flex-end"
+                  spacing="0"
+                  mt={{
+                     base: 2,
                   }}
-                  colorScheme="brand"
                >
-                  View
-               </Button>
-               <Text color="gray.500" fontSize="14px">
-                  {product.inventory_quantity > 0 &&
-                  product.inventory_quantity < 10
-                     ? `
+                  <Text
+                     color={isDiscounted ? 'red.700' : 'inherit'}
+                     fontWeight="bold"
+                     fontSize="xl"
+                     lineHeight="1em"
+                  >
+                     ${product.price}
+                  </Text>
+                  {isDiscounted && (
+                     <Text
+                        lineHeight="1em"
+                        textDecoration="line-through"
+                        color="gray.400"
+                     >
+                        ${product.compare_at_price}
+                     </Text>
+                  )}
+               </VStack>
+               <Stack
+                  direction={{
+                     base: 'row-reverse',
+                     sm: 'column',
+                     md: 'row-reverse',
+                     lg: 'column',
+                  }}
+                  align={{
+                     base: 'center',
+                     sm: 'flex-end',
+                     md: 'center',
+                     lg: 'flex-end',
+                  }}
+                  spacing={{
+                     base: 3,
+                     sm: 2,
+                  }}
+               >
+                  <LinkOverlay
+                     href={`https://ifixit.com/Store/Product/${product.sku}`}
+                  >
+                     <Button
+                        as="div"
+                        minW={{
+                           base: '100px',
+                           sm: '60px',
+                           md: '100px',
+                           lg: '60px',
+                        }}
+                        colorScheme="brand"
+                     >
+                        View
+                     </Button>
+                  </LinkOverlay>
+                  <Text color="gray.500" fontSize="14px">
+                     {product.inventory_quantity > 0 &&
+                     product.inventory_quantity < 10
+                        ? `
                Only ${product.inventory_quantity} left in stock
                `
-                     : `${product.inventory_quantity} in stock`}
-               </Text>
-            </Stack>
-         </VStack>
-      </Stack>
+                        : `${product.inventory_quantity} in stock`}
+                  </Text>
+               </Stack>
+            </VStack>
+         </Stack>
+      </LinkBox>
    );
 }
