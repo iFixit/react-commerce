4968908d4bc58e3d43cf7ba5d8ab074dde172764
macs <massimodemarchi91@gmail.com>
2021-11-04 20:13:33 +0100

    chore: Prettier format

--- features/collection/components/MetaTags.tsx
+++ features/collection/components/MetaTags.tsx
@@ -20,7 +20,9 @@ export function MetaTags({ collection }: MetaTagsProps) {
             <>
                <link
                   rel="canonical"
-                  href={`https://www.ifixit.com/collections/${collection.handle}${
+                  href={`https://www.ifixit.com/collections/${
+                     collection.handle
+                  }${
                      searchParams.page > 1
                         ? `?${COLLECTION_PAGE_PARAM}=${searchParams.page}`
                         : ''
