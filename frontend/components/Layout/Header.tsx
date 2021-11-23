6d22376532e0ef4a33535297f05fa7c5f22f85c0
Massimo De Marchi <massimo@inkofpixel.com>
2021-11-23 18:26:46 +0100

    feat: Add basic menubar

--- frontend/components/Layout/Header.tsx
+++ frontend/components/Layout/Header.tsx
@@ -1,6 +1,16 @@
-import { chakra, Flex, HStack } from '@chakra-ui/react';
+import {
+   Box,
+   BoxProps,
+   chakra,
+   Divider,
+   Flex,
+   FlexProps,
+   forwardRef,
+   HStack,
+   VStack,
+} from '@chakra-ui/react';
 import { LayoutData } from '@lib/api';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import * as React from 'react';
 import { Wordmark } from './Wordmark';
 
@@ -13,17 +23,137 @@ export function Header({ data }: HeaderProps) {
    return (
       <Flex bg="trueGray.900" color="white" height="68px">
          <Flex alignItems="center" pl={6}>
-            <Link href="/" passHref>
+            <NextLink href="/" passHref>
                <chakra.a cursor="pointer">
                   <Wordmark />
                </chakra.a>
-            </Link>
-            <HStack ml={6}>
-               <Link href="#">
-                  <chakra.a px={3}>Fix Your Stuff</chakra.a>
-               </Link>
-            </HStack>
+            </NextLink>
+            <Box as="nav" h="full" ml="6">
+               <MenuBar h="full" position="relative">
+                  {data.menu?.items.map((item, index) => {
+                     switch (item.type) {
+                        case 'submenu': {
+                           return (
+                              <MenuItem
+                                 key={index}
+                                 _hover={{
+                                    bg: 'trueGray.800',
+                                    '& > *': {
+                                       display: 'flex',
+                                    },
+                                 }}
+                              >
+                                 <MenuButton
+                                    align="center"
+                                    px="6"
+                                    cursor="pointer"
+                                    tabIndex={0}
+                                 >
+                                    <span>{item.name}</span>
+                                 </MenuButton>
+                                 <Menu
+                                    position="absolute"
+                                    zIndex={1000}
+                                    left="0"
+                                    bottom="0"
+                                    transform="translateY(100%)"
+                                    display="none"
+                                    bg="trueGray.800"
+                                 >
+                                    {item.submenu.items.map(
+                                       (subitem, subIndex) => {
+                                          if (subitem.type !== 'link') {
+                                             return null;
+                                          }
+                                          return (
+                                             <MenuItem
+                                                key={subIndex}
+                                                flexGrow={1}
+                                                w="222px"
+                                                _hover={{
+                                                   bg: 'blue.500',
+                                                   '& .divider': {
+                                                      borderColor: 'white',
+                                                   },
+                                                   '& p': {
+                                                      color: 'white',
+                                                   },
+                                                }}
+                                             >
+                                                <NextLink
+                                                   href={subitem.url}
+                                                   passHref
+                                                >
+                                                   <MenuLink
+                                                      p="6"
+                                                      flexGrow={1}
+                                                      direction="column"
+                                                      tabIndex={-1}
+                                                   >
+                                                      <Box
+                                                         as="span"
+                                                         fontWeight="bold"
+                                                      >
+                                                         {subitem.name}
+                                                      </Box>
+                                                      <Divider
+                                                         className="divider"
+                                                         borderBottomWidth="4px"
+                                                         borderRadius="2px"
+                                                         my="4"
+                                                         borderColor="trueGray.600"
+                                                      />
+                                                      {subitem.descriptionHtml && (
+                                                         <Box
+                                                            as="p"
+                                                            fontSize="14px"
+                                                            color="trueGray.400"
+                                                            dangerouslySetInnerHTML={{
+                                                               __html:
+                                                                  subitem.descriptionHtml,
+                                                            }}
+                                                         />
+                                                      )}
+                                                   </MenuLink>
+                                                </NextLink>
+                                             </MenuItem>
+                                          );
+                                       }
+                                    )}
+                                 </Menu>
+                              </MenuItem>
+                           );
+                        }
+                        default: {
+                           return null;
+                        }
+                     }
+                  })}
+               </MenuBar>
+            </Box>
          </Flex>
       </Flex>
    );
 }
+
+const MenuBar = forwardRef<FlexProps, 'ul'>((props, ref) => {
+   return <Flex ref={ref} as="ul" role="menubar" {...props} />;
+});
+
+const Menu = forwardRef<FlexProps, 'ul'>((props, ref) => {
+   return <Flex ref={ref} as="ul" role="menu" {...props} />;
+});
+
+const MenuItem = forwardRef<FlexProps, 'li'>((props, ref) => {
+   return <Flex ref={ref} as="li" role="none" {...props} />;
+});
+
+const MenuButton = forwardRef<FlexProps, 'button'>((props, ref) => {
+   return (
+      <Flex ref={ref} as="ul" role="menuitem" aria-haspopup="true" {...props} />
+   );
+});
+
+const MenuLink = forwardRef<FlexProps, 'a'>((props, ref) => {
+   return <Flex ref={ref} as="a" role="menuitem" {...props} />;
+});
