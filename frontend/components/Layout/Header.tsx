bc12f2c40ebb65e8178467ca88d5cf64368746f7
Massimo De Marchi <massimo@inkofpixel.com>
2021-11-24 15:50:22 +0100

    feat: Add searchbar

--- frontend/components/Layout/Header.tsx
+++ frontend/components/Layout/Header.tsx
@@ -3,6 +3,7 @@ import { LayoutData } from '@lib/api';
 import NextLink from 'next/link';
 import * as React from 'react';
 import { Navigation } from './Navigation';
+import { SearchBar } from './SearchBar';
 import { Wordmark } from './Wordmark';
 
 export interface HeaderProps {
@@ -19,6 +20,7 @@ export function Header({ data }: HeaderProps) {
                </chakra.a>
             </NextLink>
             {data.menu && <Navigation menu={data.menu} />}
+            <SearchBar />
          </Flex>
       </Flex>
    );
---
+++ frontend/components/Layout/SearchBar.tsx
@@ -0,0 +1,52 @@
+import {
+   Flex,
+   FlexProps,
+   forwardRef,
+   Icon,
+   Input,
+   InputGroup,
+   InputGroupProps,
+   InputLeftElement,
+} from '@chakra-ui/react';
+import * as React from 'react';
+import { RiSearchLine } from 'react-icons/ri';
+
+export const SearchBar = forwardRef<FlexProps, 'form'>((props, ref) => {
+   return (
+      <Flex
+         ref={ref}
+         as="form"
+         method="GET"
+         action="https://www.ifixit.com/Search"
+         mx="8"
+         {...props}
+      >
+         <SearchInput />
+      </Flex>
+   );
+});
+
+const SearchInput = forwardRef<InputGroupProps, 'input'>((props, ref) => {
+   return (
+      <InputGroup {...props}>
+         <InputLeftElement pointerEvents="none">
+            <Icon as={RiSearchLine} color="white" mr="-2" mb="-1px" />
+         </InputLeftElement>
+         <Input
+            name="query"
+            placeholder="Search"
+            tabIndex={0}
+            variant="filled"
+            bg="trueGray.800"
+            fontSize="sm"
+            borderRadius="full"
+            _hover={{
+               bg: 'trueGray.700',
+            }}
+            _focus={{
+               bg: 'trueGray.700',
+            }}
+         />
+      </InputGroup>
+   );
+});
