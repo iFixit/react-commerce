cfad00e0152950308a21889681e972efcb3196a3
Massimo De Marchi <massimo@inkofpixel.com>
2022-01-11 11:57:10 +0100

    refactor: Use postgres for production/staging environments

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,37 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", "strapi"),
+          password: env("DATABASE_PASSWORD", "strapi"),
+          schema: env("DATABASE_SCHEMA", "public"), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+          },
+        },
+        debug: false,
+      },
+    };
+  } else {
+    return {
+      connection: {
+        client: "sqlite",
+        connection: {
+          filename: path.join(
+            __dirname,
+            "..",
+            env("DATABASE_FILENAME", ".tmp/data.db")
+          ),
+        },
+        useNullAsDefault: true,
+      },
+    };
+  }
+};
