451323ad62a81ed8ed161e52e8e12a6e162cfc7f
Massimo De Marchi <massimo@inkofpixel.com>
2022-01-16 18:28:12 +0100

    feat: Add subnavigation to product list

---
+++ frontend/components/product-list/ProductListBreadcrumb.tsx
@@ -0,0 +1,105 @@
+import {
+   Breadcrumb,
+   BreadcrumbItem,
+   BreadcrumbLink,
+   Icon,
+   Text,
+   BreadcrumbProps,
+   Box,
+   Menu,
+   MenuList,
+   MenuItem,
+   IconButton,
+   MenuButton,
+} from '@chakra-ui/react';
+import { ProductList } from '@models/product-list';
+import NextLink from 'next/link';
+import * as React from 'react';
+import { HiChevronRight, HiDotsHorizontal } from 'react-icons/hi';
+
+export type ProductListBreadcrumbProps = BreadcrumbProps & {
+   productList: ProductList;
+};
+
+export function ProductListBreadcrumb({
+   productList,
+   ...otherProps
+}: ProductListBreadcrumbProps) {
+   const reverseAncestorList = React.useMemo(() => {
+      return [...productList.ancestors].reverse();
+   }, [productList.ancestors]);
+
+   return (
+      <Breadcrumb
+         spacing={1}
+         separator={<Icon as={HiChevronRight} color="gray.300" />}
+         fontSize="sm"
+         display="flex"
+         flexWrap="nowrap"
+         flexShrink={1}
+         minW="0"
+         sx={{
+            '& > *': {
+               display: 'flex',
+            },
+         }}
+         {...otherProps}
+      >
+         {productList.ancestors.map((ancestor) => (
+            <BreadcrumbItem
+               key={ancestor.handle}
+               display={{
+                  base: 'none',
+                  lg: 'inline-flex',
+               }}
+            >
+               <NextLink href={`/store/${ancestor.handle}`} passHref>
+                  <BreadcrumbLink color="gray.400" whiteSpace="nowrap">
+                     {ancestor.title}
+                  </BreadcrumbLink>
+               </NextLink>
+            </BreadcrumbItem>
+         ))}
+         {reverseAncestorList.length > 0 && (
+            <BreadcrumbItem
+               display={{
+                  base: 'inline-flex',
+                  lg: 'none',
+               }}
+            >
+               <Menu>
+                  <MenuButton
+                     as={IconButton}
+                     aria-label="Options"
+                     icon={<Icon as={HiDotsHorizontal} color="gray.400" />}
+                     variant="solid"
+                     bg="gray.200"
+                     size="xs"
+                  />
+                  <MenuList>
+                     {reverseAncestorList.map((ancestor) => (
+                        <NextLink
+                           key={ancestor.handle}
+                           href={`/store/${ancestor.handle}`}
+                           passHref
+                        >
+                           <MenuItem as="a">{ancestor.title}</MenuItem>
+                        </NextLink>
+                     ))}
+                  </MenuList>
+               </Menu>
+            </BreadcrumbItem>
+         )}
+         <BreadcrumbItem isCurrentPage>
+            <Text
+               color="black"
+               fontWeight="bold"
+               whiteSpace="nowrap"
+               isTruncated
+            >
+               {productList.title}
+            </Text>
+         </BreadcrumbItem>
+      </Breadcrumb>
+   );
+}
--- frontend/components/product-list/ProductListView.tsx
+++ frontend/components/product-list/ProductListView.tsx
@@ -1,4 +1,11 @@
-import { ALGOLIA_API_KEY, ALGOLIA_APP_ID } from '@config/env';
+import { Flex, VStack } from '@chakra-ui/react';
+import {
+   PageContentWrapper,
+   SecondaryNavbar,
+   SecondaryNavbarItem,
+   SecondaryNavbarLink,
+} from '@components/common';
+import { ALGOLIA_API_KEY, ALGOLIA_APP_ID, IFIXIT_ORIGIN } from '@config/env';
 import { AlgoliaProvider, SearchContext } from '@lib/algolia';
 import { GlobalSettings } from '@models/global-settings';
 import {
@@ -6,8 +13,10 @@ import {
    ProductListSectionType,
    ProductSearchHit,
 } from '@models/product-list';
+import snakeCase from 'lodash/snakeCase';
+import NextLink from 'next/link';
 import { MetaTags } from './MetaTags';
-import { PageLayout } from './PageLayout';
+import { ProductListBreadcrumb } from './ProductListBreadcrumb';
 import {
    BannerSection,
    FeaturedProductListSection,
@@ -31,100 +40,165 @@ export function ProductListView({
    globalSettings,
 }: ProductListViewProps) {
    const { newsletterForm } = globalSettings;
+   const isRootProductList = productList.ancestors.length === 0;
    return (
-      <PageLayout>
-         <AlgoliaProvider
-            key={productList.handle}
-            appId={ALGOLIA_APP_ID}
-            apiKey={ALGOLIA_API_KEY}
-            initialIndexName={searchContext.params.indexName}
-            initialContext={searchContext}
-         >
-            <MetaTags productList={productList} />
-            <HeroSection productList={productList} />
-            {productList.children.length > 0 && (
-               <ProductListChildrenSection
-                  heading={productList.title}
-                  productListChildren={productList.children}
-               />
-            )}
-            <FilterableProductsSection />
-            {productList.sections.map((section, index) => {
-               switch (section.type) {
-                  case ProductListSectionType.Banner: {
-                     return (
-                        <BannerSection
-                           key={index}
-                           title={section.title}
-                           description={section.description}
-                           callToActionLabel={section.callToActionLabel}
-                           url={section.url}
-                        />
-                     );
-                  }
-                  case ProductListSectionType.RelatedPosts: {
-                     const tags = [productList.title].concat(
-                        section.tags?.split(',').map((tag) => tag.trim()) || []
-                     );
-                     return <RelatedPostsSection key={index} tags={tags} />;
-                  }
-                  case ProductListSectionType.FeaturedProductList: {
-                     const { productList } = section;
-                     if (productList) {
-                        return (
-                           <FeaturedProductListSection
-                              key={index}
-                              handle={productList.handle}
-                              algoliaIndexName={searchContext.params.indexName}
-                              title={productList.title}
-                              description={productList.description}
-                              imageSrc={
-                                 productList.image
-                                    ? `${productList.image.url}`
-                                    : undefined
-                              }
-                              imageAlt={
-                                 productList.image?.alternativeText || undefined
-                              }
-                           />
-                        );
+      <>
+         <SecondaryNavbar>
+            <PageContentWrapper h="full">
+               <Flex
+                  h="full"
+                  w="full"
+                  boxSizing="border-box"
+                  justify="space-between"
+                  px={{ base: 3, sm: 0 }}
+                  overflow="hidden"
+               >
+                  <ProductListBreadcrumb productList={productList} />
+                  <Flex
+                     h="full"
+                     align="stretch"
+                     borderLeftWidth={{
+                        base: '1px',
+                        md: '0',
+                     }}
+                     bg="white"
+                  >
+                     <SecondaryNavbarItem isCurrent>Parts</SecondaryNavbarItem>
+                     <SecondaryNavbarItem>
+                        <NextLink
+                           href={
+                              isRootProductList
+                                 ? `${IFIXIT_ORIGIN}/Guide`
+                                 : `${IFIXIT_ORIGIN}/Device/${snakeCase(
+                                      productList.handle
+                                   )}`
+                           }
+                           passHref
+                        >
+                           <SecondaryNavbarLink>Guides</SecondaryNavbarLink>
+                        </NextLink>
+                     </SecondaryNavbarItem>
+                     <SecondaryNavbarItem>
+                        <NextLink
+                           href={
+                              isRootProductList
+                                 ? `${IFIXIT_ORIGIN}/Answers`
+                                 : `${IFIXIT_ORIGIN}/Answers/Device/${snakeCase(
+                                      productList.handle
+                                   )}`
+                           }
+                           passHref
+                        >
+                           <SecondaryNavbarLink>Answers</SecondaryNavbarLink>
+                        </NextLink>
+                     </SecondaryNavbarItem>
+                  </Flex>
+               </Flex>
+            </PageContentWrapper>
+         </SecondaryNavbar>
+         <PageContentWrapper py="10">
+            <VStack align="stretch" spacing="12">
+               <AlgoliaProvider
+                  key={productList.handle}
+                  appId={ALGOLIA_APP_ID}
+                  apiKey={ALGOLIA_API_KEY}
+                  initialIndexName={searchContext.params.indexName}
+                  initialContext={searchContext}
+               >
+                  <MetaTags productList={productList} />
+                  <HeroSection productList={productList} />
+                  {productList.children.length > 0 && (
+                     <ProductListChildrenSection
+                        heading={productList.title}
+                        productListChildren={productList.children}
+                     />
+                  )}
+                  <FilterableProductsSection />
+                  {productList.sections.map((section, index) => {
+                     switch (section.type) {
+                        case ProductListSectionType.Banner: {
+                           return (
+                              <BannerSection
+                                 key={index}
+                                 title={section.title}
+                                 description={section.description}
+                                 callToActionLabel={section.callToActionLabel}
+                                 url={section.url}
+                              />
+                           );
+                        }
+                        case ProductListSectionType.RelatedPosts: {
+                           const tags = [productList.title].concat(
+                              section.tags
+                                 ?.split(',')
+                                 .map((tag) => tag.trim()) || []
+                           );
+                           return (
+                              <RelatedPostsSection key={index} tags={tags} />
+                           );
+                        }
+                        case ProductListSectionType.FeaturedProductList: {
+                           const { productList } = section;
+                           if (productList) {
+                              return (
+                                 <FeaturedProductListSection
+                                    key={index}
+                                    handle={productList.handle}
+                                    algoliaIndexName={
+                                       searchContext.params.indexName
+                                    }
+                                    title={productList.title}
+                                    description={productList.description}
+                                    imageSrc={
+                                       productList.image
+                                          ? `${productList.image.url}`
+                                          : undefined
+                                    }
+                                    imageAlt={
+                                       productList.image?.alternativeText ||
+                                       undefined
+                                    }
+                                 />
+                              );
+                           }
+                           return null;
+                        }
+                        case ProductListSectionType.ProductListSet: {
+                           const { title, productLists } = section;
+                           if (productLists.length > 0) {
+                              return (
+                                 <ProductListSetSection
+                                    key={index}
+                                    title={title}
+                                    productLists={productLists}
+                                 />
+                              );
+                           }
+                           return null;
+                        }
+                        default: {
+                           console.warn(
+                              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                              // @ts-ignore
+                              `Section ${section.__typename} not implemented`
+                           );
+                           return null;
+                        }
                      }
-                     return null;
-                  }
-                  case ProductListSectionType.ProductListSet: {
-                     const { title, productLists } = section;
-                     if (productLists.length > 0) {
-                        return (
-                           <ProductListSetSection
-                              key={index}
-                              title={title}
-                              productLists={productLists}
-                           />
-                        );
-                     }
-                     return null;
-                  }
-                  default: {
-                     console.warn(
-                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                        // @ts-ignore
-                        `Section ${section.__typename} not implemented`
-                     );
-                     return null;
-                  }
-               }
-            })}
-            {newsletterForm && (
-               <NewsletterSection
-                  title={newsletterForm.title}
-                  description={newsletterForm.subtitle}
-                  emailPlaceholder={
-                     newsletterForm.inputPlaceholder || undefined
-                  }
-                  subscribeLabel={newsletterForm.callToActionButtonTitle}
-               />
-            )}
-         </AlgoliaProvider>
-      </PageLayout>
+                  })}
+                  {newsletterForm && (
+                     <NewsletterSection
+                        title={newsletterForm.title}
+                        description={newsletterForm.subtitle}
+                        emailPlaceholder={
+                           newsletterForm.inputPlaceholder || undefined
+                        }
+                        subscribeLabel={newsletterForm.callToActionButtonTitle}
+                     />
+                  )}
+               </AlgoliaProvider>
+            </VStack>
+         </PageContentWrapper>
+      </>
    );
 }
