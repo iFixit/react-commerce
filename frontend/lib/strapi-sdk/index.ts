22f166a0aa9dd97a1db9e3637b88c9c28e1cfa7e
Massimo De Marchi <massimo@inkofpixel.com>
2022-01-17 19:50:40 +0100

    refactor: Improve error logging for GraphQL queries

--- frontend/lib/strapi-sdk/index.ts
+++ frontend/lib/strapi-sdk/index.ts
@@ -16,12 +16,24 @@ const requester: Requester = async <R, V>(
          variables,
       }),
    });
+   let result: any;
+   try {
+      result = await response.json();
+   } catch (error) {
+      throw new Error(`Response is not a json: ${response.statusText}`);
+   }
    if (response.ok) {
-      const result = await response.json();
       if (result?.data) {
          return result.data;
       }
       throw new Error('Data not available in GraphQL response');
+   }
+   if (Array.isArray(result.errors)) {
+      console.log('GraphQL query failed with errors:');
+      result.errors.map((error: any) => {
+         const code = error.extensions?.code || 'UNKNOWN';
+         console.log(`\t[${code}]`, error.message);
+      });
    }
    throw new Error(`GraphQL query failed to execute: ${response.statusText}`);
 };
