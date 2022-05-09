67a8ba2e6c882dde7276f93e85d73e25acbc8d7f
Parker <34843669+lithobraking@users.noreply.github.com>
2022-05-09 08:46:15 -0700

    placed "iFixit" after both product list title and page number
    Co-authored-by: Massimo De Marchi <massimo@inkofpixel.com>

--- frontend/components/product-list/MetaTags.tsx
+++ frontend/components/product-list/MetaTags.tsx
@@ -13,10 +13,11 @@ export function MetaTags({ productList }: MetaTagsProps) {
    const searchParams = useSearchParams();
    const isFiltered =
       searchParams.query.length > 0 || searchParams.filters.allIds.length > 0;
-   let title = `${productList.title} | iFixit`;
+   let title = productList.title;
    if (!isFiltered && searchParams.page > 1) {
       title += ` - Page ${searchParams.page}`;
    }
+   title += ' | iFixit';
    const canonicalUrl = `${IFIXIT_ORIGIN}${productList.path}${
       searchParams.page > 1
          ? `?${PRODUCT_LIST_PAGE_PARAM}=${searchParams.page}`
