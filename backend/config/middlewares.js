22f2a8ee3da1fd8a932795237ca66ffe53ac84ec
Daniel Beardsley <daniel@ifixit.com>
2022-05-26 11:10:41 -0700

    Strapi config: define `env`
    Derp! I was wondering where env came from and hadn't noticed this
    style of exports in other strapi configs.

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,38 +1,40 @@
-module.exports = [
-   'strapi::errors',
-   env('S3_BUCKET') ? securitySectionWithS3() : 'strapi::security',
-   'strapi::cors',
-   'strapi::poweredBy',
-   'strapi::logger',
-   'strapi::query',
-   'strapi::body',
-   'strapi::favicon',
-   'strapi::public',
-];
+module.exports = ({ env }) => {
+   return [
+      'strapi::errors',
+      env('S3_BUCKET') ? securitySectionWithS3() : 'strapi::security',
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::logger',
+      'strapi::query',
+      'strapi::body',
+      'strapi::favicon',
+      'strapi::public',
+   ];
 
-function securitySectionWithS3() {
-   return {
-      name: 'strapi::security',
-      config: {
-         contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-               'connect-src': ["'self'", 'https:'],
-               'img-src': [
-                  "'self'",
-                  'data:',
-                  'blob:',
-                  `${env("S3_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
-               ],
-               'media-src': [
-                  "'self'",
-                  'data:',
-                  'blob:',
-                  `${env("S3_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
-               ],
-               upgradeInsecureRequests: null,
+   function securitySectionWithS3() {
+      return {
+         name: 'strapi::security',
+         config: {
+            contentSecurityPolicy: {
+               useDefaults: true,
+               directives: {
+                  'connect-src': ["'self'", 'https:'],
+                  'img-src': [
+                     "'self'",
+                     'data:',
+                     'blob:',
+                     `${env("S3_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+                  ],
+                  'media-src': [
+                     "'self'",
+                     'data:',
+                     'blob:',
+                     `${env("S3_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+                  ],
+                  upgradeInsecureRequests: null,
+               },
             },
          },
-      },
-   };
-}
+      };
+   }
+};
