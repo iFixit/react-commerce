f022ae9e8e10d835977b615ec74bbc1b1521a30c
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-06-03 14:48:28 -0700

    feat: Add default Sentry reporting to frontend/
    Ran `npx @sentry/wizard -i nextjs` in frontend/, which generated most of
    the files here.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,21 @@ const withTM = require('next-transpile-modules')([
    '@ifixit/helpers',
 ]);
 
-module.exports = withTM({
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const sentryWebpackPluginOptions = {
+   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+   // the following options are set automatically, and overriding them is not
+   // recommended:
+   //   release, url, org, project, authToken, configFile, stripPrefix,
+   //   urlPrefix, include, ignore
+
+   silent: true, // Suppresses all logs
+   // For all available options, see:
+   // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+const moduleExports = {
    env: {
       ALGOLIA_API_KEY: process.env.ALGOLIA_API_KEY,
       NEXT_PUBLIC_ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
@@ -47,4 +61,11 @@ module.exports = withTM({
       locales: ['en-US'],
       defaultLocale: 'en-US',
    },
-});
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(
+   withTM(moduleExports),
+   sentryWebpackPluginOptions
+);
