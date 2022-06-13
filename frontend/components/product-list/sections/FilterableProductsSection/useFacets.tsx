4c29724a8d0e995b8ba3d52385c188520c9fad5d
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-06-13 16:36:00 -0700

    facets: Use hard coded list
    Until we can get `useDynamicWidgets()` working with iFixit's Algolia
    app. Currently, this hook just returns an empty array. The facets
    included here are the same that appear on `main` as of
    2342ff10c8aabe4356b9a7cd6fe8163814d115f5.

--- frontend/components/product-list/sections/FilterableProductsSection/useFacets.tsx
+++ frontend/components/product-list/sections/FilterableProductsSection/useFacets.tsx
@@ -5,5 +5,17 @@ export function useFacets() {
       maxValuesPerFacet: 200,
    });
 
-   return attributesToRender;
+   return [
+      'facet_tags.Capacity',
+      'device',
+      'facet_tags.Device Brand',
+      'facet_tags.Device Category',
+      'facet_tags.Device Type',
+      'facet_tags.Item Type',
+      'facet_tags.OS',
+      'facet_tags.Part or Kit',
+      'price_range',
+      'worksin',
+   ];
+   // return attributesToRender;
 }
