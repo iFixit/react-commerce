296bedffe7e509da2b769dd29ff85baa1ef51d85
Daniel Beardsley <daniel@ifixit.com>
2022-06-15 17:34:30 -0700

    ifixit-image: use pass-through loader for all images
    Detect all three sources by their urls and effectively include those
    images directly (opt out of using next's image resizing pipeline).
    
    This isn't the long term plan, it's just where we're going to start
    from before we opt-in specific image sources and maybe auto-choose the
    right size.

--- frontend/components/ifixit-image/ifixit-image.tsx
+++ frontend/components/ifixit-image/ifixit-image.tsx
@@ -1,5 +1,37 @@
-import Image, { ImageProps } from 'next/image';
+import Image, { ImageProps, ImageLoader } from 'next/image';
 
 export function IfixitImage(props: ImageProps) {
+   if (typeof props.src === 'string') {
+      if (isGuideImage(props.src)) {
+         props.loader = passThroughLoader;
+      } else if (isCartImage(props.src)) {
+         props.loader = passThroughLoader;
+      } else if (isStrapiImage(props.src)) {
+         props.loader = passThroughLoader;
+      }
+   }
+
    return <Image {...props} />;
+}
+
+function isGuideImage(src: string) {
+   return src.match(
+      /^https:\/\/(guide-images\.cdn\.ifixit\.com\/|([^/]+\.(ubreakit|cominor)\.com\/igi))\//
+   );
+}
+
+function isCartImage(src: string) {
+   return src.match(
+      /^https:\/\/(cart-products\.cdn\.ifixit\.com\/|([^/]+\.(ubreakit|cominor)\.com\/cart-products))\//
+   );
+}
+
+function isStrapiImage(src: string) {
+   return src.match(
+      /^https:\/\/ifixit-(dev-)?strapi-uploads.s3.amazonaws.com\//
+   );
+}
+
+function passThroughLoader({ src }: { src: string }) {
+   return src;
 }
