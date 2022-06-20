78c0a11c218d53dee9f56087431da154e731b178
Massimo De Marchi <massimo@inkofpixel.com>
2022-06-20 18:35:21 +0200

    fix: Add temporary fix to avoid page crash

---
+++ frontend/components/common/InstantSearchProvider.tsx
@@ -0,0 +1,115 @@
+import { ALGOLIA_APP_ID } from '@config/env';
+import algoliasearch, { SearchClient } from 'algoliasearch/lite';
+import { history } from 'instantsearch.js/es/lib/routers';
+import * as React from 'react';
+import {
+   InstantSearch,
+   InstantSearchServerState,
+   InstantSearchSSRProvider,
+} from 'react-instantsearch-hooks-web';
+
+type InstantSearchProviderProps = React.PropsWithChildren<AlgoliaProps>;
+
+export type AlgoliaProps = {
+   url: string;
+   indexName: string;
+   routing?: boolean;
+   serverState?: Partial<InstantSearchServerState>;
+   apiKey: string;
+};
+
+type RouteState = Partial<{
+   q: string;
+   p: number;
+   filter: Record<string, any>;
+   range: Record<string, string>;
+}>;
+
+export function InstantSearchProvider({
+   children,
+   url,
+   indexName,
+   serverState,
+   apiKey,
+   routing,
+}: InstantSearchProviderProps) {
+   const algoliaClientRef = React.useRef<SearchClient>();
+   algoliaClientRef.current =
+      algoliaClientRef.current ?? algoliasearch(ALGOLIA_APP_ID, apiKey);
+
+   // We're using this to make `InstantSearch` unmount at every re-render, since as of version 6.28.0 it breaks when
+   // it re-renders. Re-rendering though should be relatively infrequent in this component, so this should be fine.
+   const count = useCountRenders();
+
+   const provider = (
+      <InstantSearchSSRProvider {...serverState}>
+         <InstantSearch
+            key={count}
+            searchClient={algoliaClientRef.current}
+            indexName={indexName}
+            routing={{
+               stateMapping: {
+                  stateToRoute(uiState): any {
+                     const indexUiState = uiState[indexName];
+                     const routeState: RouteState = {};
+                     if (indexUiState.query) {
+                        routeState.q = indexUiState.query;
+                     }
+                     if (indexUiState.page) {
+                        routeState.p = indexUiState.page;
+                     }
+                     if (indexUiState.refinementList) {
+                        routeState.filter = indexUiState.refinementList;
+                     }
+                     if (indexUiState.range != null) {
+                        routeState.range = indexUiState.range;
+                     }
+                     return routeState;
+                  },
+                  routeToState(routeState: RouteState) {
+                     const stateObject: Record<string, any> = {};
+                     if (routeState.q != null) {
+                        stateObject.query = routeState.q;
+                     }
+                     if (routeState.p != null) {
+                        stateObject.page = routeState.p;
+                     }
+                     if (routeState.filter != null) {
+                        stateObject.refinementList = routeState.filter;
+                     }
+                     if (routeState.range != null) {
+                        stateObject.range = routeState.range;
+                     }
+                     return {
+                        [indexName]: stateObject,
+                     };
+                  },
+               },
+               router: history({
+                  getLocation() {
+                     if (typeof window === 'undefined') {
+                        return new URL(url) as unknown as Location;
+                     }
+
+                     return window.location;
+                  },
+               }),
+            }}
+         >
+            {children}
+         </InstantSearch>
+      </InstantSearchSSRProvider>
+   );
+   if (routing) {
+      <InstantSearchSSRProvider {...serverState}>
+         {provider}
+      </InstantSearchSSRProvider>;
+   }
+   return provider;
+}
+
+function useCountRenders() {
+   const countRef = React.useRef(0);
+   countRef.current++;
+   return countRef.current;
+}
--- frontend/components/product-list/ProductListView.tsx
+++ frontend/components/product-list/ProductListView.tsx
@@ -1,19 +1,8 @@
 import { Flex, VStack } from '@chakra-ui/react';
 import { PageContentWrapper, SecondaryNavbar } from '@components/common';
-import { ALGOLIA_APP_ID } from '@config/env';
 import { computeProductListAlgoliaFilterPreset } from '@helpers/product-list-helpers';
 import { ProductList, ProductListSectionType } from '@models/product-list';
-import type { SearchClient } from 'algoliasearch/lite';
-import algoliasearch from 'algoliasearch/lite';
-import { history } from 'instantsearch.js/es/lib/routers';
-import * as React from 'react';
-import {
-   Configure,
-   Index,
-   InstantSearch,
-   InstantSearchServerState,
-   InstantSearchSSRProvider,
-} from 'react-instantsearch-hooks-web';
+import { Configure, Index } from 'react-instantsearch-hooks-web';
 import { MetaTags } from './MetaTags';
 import { ProductListBreadcrumb } from './ProductListBreadcrumb';
 import { ProductListDeviceNavigation } from './ProductListDeviceNavigation';
@@ -29,22 +18,13 @@ import {
 
 export interface ProductListViewProps {
    productList: ProductList;
-   url: string;
-   serverState?: Partial<InstantSearchServerState>;
    indexName: string;
 }
 
 export function ProductListView({
    productList,
-   url,
-   serverState,
    indexName,
 }: ProductListViewProps) {
-   const algoliaClientRef = React.useRef<SearchClient>();
-   algoliaClientRef.current =
-      algoliaClientRef.current ??
-      algoliasearch(ALGOLIA_APP_ID, productList.algolia.apiKey);
-
    const filters = computeProductListAlgoliaFilterPreset(productList);
 
    return (
@@ -65,128 +45,71 @@ export function ProductListView({
          </SecondaryNavbar>
          <PageContentWrapper py="10">
             <VStack align="stretch" spacing="12">
-               <InstantSearchSSRProvider
-                  {...serverState}
-                  key={productList.handle}
-               >
-                  <InstantSearch
-                     searchClient={algoliaClientRef.current}
-                     indexName={indexName}
-                     routing={{
-                        stateMapping: {
-                           stateToRoute(uiState) {
-                              const indexUiState = uiState[indexName];
-                              const routeObject: any = {
-                                 q: indexUiState.query,
-                                 p: indexUiState.page,
-                              };
-                              routeObject.filter = indexUiState.refinementList;
-                              if (indexUiState.range != null) {
-                                 routeObject.range = indexUiState.range;
-                              }
-                              return routeObject;
-                           },
-                           routeToState(routeState: any) {
-                              const stateObject: any = {
-                                 query: routeState.q,
-                                 page: routeState.p,
-                                 refinementList: routeState.filter,
-                              };
-                              if (routeState.range != null) {
-                                 stateObject.range = routeState.range;
-                              }
-                              return {
-                                 [indexName]: stateObject,
-                              };
-                           },
-                        },
-                        router: history({
-                           getLocation() {
-                              if (typeof window === 'undefined') {
-                                 return new URL(url) as unknown as Location;
-                              }
-
-                              return window.location;
-                           },
-                        }),
-                     }}
-                  >
-                     <Index indexName={indexName}>
-                        <Configure filters={filters} />
-                        <MetaTags productList={productList} />
-                        <HeroSection productList={productList} />
-                        {productList.children.length > 0 && (
-                           <ProductListChildrenSection
-                              productList={productList}
+               <Index indexName={indexName}>
+                  <Configure filters={filters} />
+                  <MetaTags productList={productList} />
+                  <HeroSection productList={productList} />
+                  {productList.children.length > 0 && (
+                     <ProductListChildrenSection productList={productList} />
+                  )}
+                  <FilterableProductsSection wikiInfo={productList.wikiInfo} />
+               </Index>
+               {productList.sections.map((section, index) => {
+                  switch (section.type) {
+                     case ProductListSectionType.Banner: {
+                        return (
+                           <BannerSection
+                              key={index}
+                              title={section.title}
+                              description={section.description}
+                              callToActionLabel={section.callToActionLabel}
+                              url={section.url}
                            />
-                        )}
-                        <FilterableProductsSection
-                           wikiInfo={productList.wikiInfo}
-                        />
-                     </Index>
-                     {productList.sections.map((section, index) => {
-                        switch (section.type) {
-                           case ProductListSectionType.Banner: {
-                              return (
-                                 <BannerSection
-                                    key={index}
-                                    title={section.title}
-                                    description={section.description}
-                                    callToActionLabel={
-                                       section.callToActionLabel
-                                    }
-                                    url={section.url}
-                                 />
-                              );
-                           }
-                           case ProductListSectionType.RelatedPosts: {
-                              const tags = [productList.title].concat(
-                                 section.tags
-                                    ?.split(',')
-                                    .map((tag) => tag.trim()) || []
-                              );
-                              return (
-                                 <RelatedPostsSection key={index} tags={tags} />
-                              );
-                           }
-                           case ProductListSectionType.FeaturedProductList: {
-                              const { productList } = section;
-                              if (productList) {
-                                 return (
-                                    <FeaturedProductListSection
-                                       key={index}
-                                       productList={productList}
-                                       index={index}
-                                    />
-                                 );
-                              }
-                              return null;
-                           }
-                           case ProductListSectionType.ProductListSet: {
-                              const { title, productLists } = section;
-                              if (productLists.length > 0) {
-                                 return (
-                                    <ProductListSetSection
-                                       key={index}
-                                       title={title}
-                                       productLists={productLists}
-                                    />
-                                 );
-                              }
-                              return null;
-                           }
-                           default: {
-                              console.warn(
-                                 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                                 // @ts-ignore
-                                 `Section ${section.__typename} not implemented`
-                              );
-                              return null;
-                           }
+                        );
+                     }
+                     case ProductListSectionType.RelatedPosts: {
+                        const tags = [productList.title].concat(
+                           section.tags?.split(',').map((tag) => tag.trim()) ||
+                              []
+                        );
+                        return <RelatedPostsSection key={index} tags={tags} />;
+                     }
+                     case ProductListSectionType.FeaturedProductList: {
+                        const { productList } = section;
+                        if (productList) {
+                           return (
+                              <FeaturedProductListSection
+                                 key={index}
+                                 productList={productList}
+                                 index={index}
+                              />
+                           );
                         }
-                     })}
-                  </InstantSearch>
-               </InstantSearchSSRProvider>
+                        return null;
+                     }
+                     case ProductListSectionType.ProductListSet: {
+                        const { title, productLists } = section;
+                        if (productLists.length > 0) {
+                           return (
+                              <ProductListSetSection
+                                 key={index}
+                                 title={title}
+                                 productLists={productLists}
+                              />
+                           );
+                        }
+                        return null;
+                     }
+                     default: {
+                        console.warn(
+                           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                           // @ts-ignore
+                           `Section ${section.__typename} not implemented`
+                        );
+                        return null;
+                     }
+                  }
+               })}
             </VStack>
          </PageContentWrapper>
       </>
