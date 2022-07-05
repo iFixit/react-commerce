ebf5a7e7914dd7bfb4b6f61cec0cd99bc1375fdd
Daniel Beardsley <daniel@ifixit.com>
2022-07-05 16:56:59 -0700

    Device Parts page: remove unused prop
    This prop was removed some commits ago. This page was added in the
    recent merge of main.

--- frontend/pages/Parts/[deviceHandle]/[itemTypeHandle].tsx
+++ frontend/pages/Parts/[deviceHandle]/[itemTypeHandle].tsx
@@ -85,7 +85,6 @@ export const getServerSideProps: GetServerSideProps<AppPageProps> = async (
       algolia: {
          indexName,
          url,
-         routing: true,
          apiKey: productList.algolia.apiKey,
       },
    };
