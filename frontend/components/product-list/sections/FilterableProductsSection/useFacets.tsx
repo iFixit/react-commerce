04312b906d8453bcf63d47496b13b4fdaa9967e9
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-07-06 18:08:18 -0700

    facets: Add custom accordian sorting
    Algolia's Facet Display doesn't work for nested facets like facet_tags.
    This sorts the facets in code instead.
    
    On parts pages:
    - Item Type
    - Part or Kit
    - Alphabetical for everything else
    
    On tools pages:
    - Tool Category
    - Price Range
    - Alphabetical
    
    All other pages will default to alphabetical.

--- frontend/components/product-list/sections/FilterableProductsSection/useFacets.tsx
+++ frontend/components/product-list/sections/FilterableProductsSection/useFacets.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useDynamicWidgets } from 'react-instantsearch-hooks-web';
 import { ProductList, ProductListType } from '@models/product-list';
+import { formatFacetName } from '@helpers/algolia-helpers';
 
 export function useFacets() {
    const { attributesToRender } = useDynamicWidgets({
@@ -34,15 +35,65 @@ export function useFilteredFacets(productList: ProductList) {
       );
    }, [productList.wikiInfo]);
 
+   const sortBy = React.useMemo(() => {
+      return sortFacets(productList.type);
+   }, [productList.type]);
+
    const usefulFacets = React.useMemo(() => {
-      const usefulFacets = facets.slice().filter((facet) => {
-         if (facet === 'facet_tags.Item Type' && isItemTypeProductList) {
-            return false;
-         }
-         return !infoNames.has(facet);
-      });
+      const usefulFacets = facets
+         .slice()
+         .filter((facet) => {
+            if (facet === 'facet_tags.Item Type' && isItemTypeProductList) {
+               return false;
+            }
+            return !infoNames.has(facet);
+         })
+         .sort(sortBy);
       return usefulFacets;
-   }, [facets, infoNames, isItemTypeProductList]);
+   }, [facets, infoNames, isItemTypeProductList, sortBy]);
 
    return usefulFacets;
+}
+
+const partsFacetRanking = new Map([
+   ['facet_tags.Item Type', 1],
+   ['facet_tags.Part or Kit', 2],
+]);
+
+const toolsFacetRanking = new Map([
+   ['facet_tags.Tool Category', 1],
+   ['price_range', 2],
+]);
+
+function sortFacets(productListType: ProductListType) {
+   switch (productListType) {
+      case ProductListType.AllParts:
+      case ProductListType.DeviceParts:
+      case ProductListType.DeviceItemTypeParts:
+         return sortFacetsWithRanking(partsFacetRanking);
+      case ProductListType.AllTools:
+      case ProductListType.ToolsCategory:
+         return sortFacetsWithRanking(toolsFacetRanking);
+      default:
+         return sortFacetsAlphabetically;
+   }
+}
+
+function sortFacetsWithRanking(ranking: Map<string, number>) {
+   return (a: string, b: string): number => {
+      if (ranking.get(a) && ranking.get(b)) {
+         return ranking.get(a)! - ranking.get(b)!;
+      } else if (ranking.get(a)) {
+         return -1;
+      } else if (ranking.get(b)) {
+         return 1;
+      } else {
+         return sortFacetsAlphabetically(a, b);
+      }
+   };
+}
+
+const enCollator = new Intl.Collator('en');
+function sortFacetsAlphabetically(a: string, b: string) {
+   return enCollator.compare(formatFacetName(a), formatFacetName(b));
 }
