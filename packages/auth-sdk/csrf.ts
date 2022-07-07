37d66ebd281cb98c3fd5a2404dc33ed051a885fa
sumukhi <sumukhi@ifixit.com>
2022-07-06 23:13:02 -0700

    Delete function setCSRFCookie

--- packages/auth-sdk/csrf.ts
+++ packages/auth-sdk/csrf.ts
@@ -7,23 +7,6 @@ type CSRFCookieInput = {
    origin: string;
 };
 
-export function setCSRFCookie(
-   context: GetServerSidePropsContext,
-   input: CSRFCookieInput
-): void {
-   const domain = parseCSRFCookieDomain(input.origin);
-   context.res.setHeader(
-      'Set-Cookie',
-      cookie.serialize('csrf', input.csrfToken, {
-         sameSite: 'none',
-         secure: true,
-         domain,
-         maxAge: 30 * 60, // 30 minutes
-         path: '/',
-      })
-   );
-}
-
 function parseCSRFCookieDomain(origin: string): string {
    const url = new URL(origin);
    return url.hostname.replace('www.', '');
