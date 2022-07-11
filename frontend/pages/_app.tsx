957e1151abdf4f05af1c154d08047ac719ace69f
Daniel Beardsley <daniel@ifixit.com>
2022-07-11 11:29:01 -0700

    Next Links: add progress bar for transitions
    Previously, clicking a link gave no indication that something was
    happening.
    
    Now at least we have a progress bar on top. The design doesn't mesh 100%
    but it's better than no feedback.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProviders } from '@components/common';
 import { AppProps } from 'next/app';
 import * as React from 'react';
+import NextNProgress from 'nextjs-progressbar';
 
 type AppPropsWithLayout = AppProps & {
    Component: NextPageWithLayout;
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
    const getLayout = Component.getLayout ?? ((page) => page);
    return (
       <AppProviders {...pageProps.appProps}>
+         <NextNProgress />
          {getLayout(<Component {...pageProps} />, pageProps)}
       </AppProviders>
    );
