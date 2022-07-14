90165dc4d20cc331a3b6ca50f0470f118b61ec35
Bat Battur <bat@ifixit.com>
2022-07-14 15:52:58 -0700

    Use @ts-ignore comments instead of @ts-nocheck.

--- frontend/test/jest/tests/ProductListItem.test.tsx
+++ frontend/test/jest/tests/ProductListItem.test.tsx
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { render, screen } from '@testing-library/react';
 import { ProductListItem } from '@components/product-list/sections/FilterableProductsSection/ProductList';
 import * as ProductSearch from '@components/product-list/sections/FilterableProductsSection/useProductSearchHitPricing';
@@ -6,6 +5,7 @@ import { mockProduct } from 'test/jest/__mocks__/mockProduct';
 
 describe('ProductListItem', () => {
    beforeEach(() => {
+      // @ts-ignore
       ProductSearch.useProductSearchHitPricing = jest.fn().mockReturnValue({
          price: 5,
          compareAtPrice: 5,
@@ -19,12 +19,13 @@ describe('ProductListItem', () => {
    });
 
    it('renders and matches the snapshot', () => {
+      // @ts-ignore
       const { asFragment } = render(<ProductListItem product={mockProduct} />);
       const shortDescription = screen.getByText(
          'Replace a dead or malfunctioning model EB-BG96aasd5ABE battery in a Samsung Galaxy S9 Plus smartphone.'
       );
 
-      expect(shortDescription).toBeInTheDocument();
-      expect(asFragment()).toMatchSnapshot();
+      (expect(shortDescription) as any).toBeInTheDocument();
+      (expect(asFragment()) as any).toMatchSnapshot();
    });
 });
