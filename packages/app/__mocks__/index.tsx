c2e05d63dcaa2e51a23ba748dd49b0054a1558cb
Bat Battur <bat@ifixit.com>
2022-07-15 09:30:51 -0700

    Only mock useAppContext when mocking.
    All other functions from the module will be from the
    actual module so they will work as they are supposed to
    originally.

--- packages/app/__mocks__/index.tsx
+++ packages/app/__mocks__/index.tsx
@@ -1,3 +1,15 @@
-export function useAppContext() {
+/**
+ * Returns the actual module instead of a mock
+ * and only mocks useAppContext().
+ */
+
+const app = jest.requireActual('../index.tsx');
+
+function useAppContext() {
     return { ifixitOrigin: 'https://www.cominor.com' };
 }
+
+app.useAppContext = useAppContext;
+module.exports = app;
+
+export {};
