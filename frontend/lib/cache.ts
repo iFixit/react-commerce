de13f87529dfe5d600dc368ef0b048ad578b4851
Daniel Beardsley <daniel@ifixit.com>
2022-07-19 11:54:02 -0700

    Cache: We're only caching Promises
    So let's type it that way. Probably doesn't make sense to cache
    synchronous code (at least until we show that's a bottleneck).

--- frontend/lib/cache.ts
+++ frontend/lib/cache.ts
@@ -1,8 +1,12 @@
 import LRU from 'lru-cache';
 const cacheStore = new LRU({ max: 100 });
 
-export function cache<T>(key: string, fetch: () => T, ttl: number): T {
-   const fromCache = cacheStore.get(key) as T | undefined;
+export function cache<T>(
+   key: string,
+   fetch: () => Promise<T>,
+   ttl: number
+): Promise<T> {
+   const fromCache = cacheStore.get(key) as Promise<T> | undefined;
    if (fromCache !== undefined) {
       console.log(`Got '${key}' from cache`);
       return fromCache;
