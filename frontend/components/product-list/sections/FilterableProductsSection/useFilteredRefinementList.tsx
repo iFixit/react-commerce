fc987fb9cdf0f894a47ca6bcaa4b4ad88b04d52d
sumukhipandey <sumukhi.a.pandey@gmail.com>
2022-07-21 11:15:39 -0700

    Get rid of previous logic
    We do not need this anymore with the new logic that will be introduced in the next commit.

--- frontend/components/product-list/sections/FilterableProductsSection/useFilteredRefinementList.tsx
+++ frontend/components/product-list/sections/FilterableProductsSection/useFilteredRefinementList.tsx
@@ -10,17 +10,9 @@ export function useFilteredRefinementList(props: UseRefinementListProps) {
    const hitsCount = results?.nbHits ?? 0;
    const isAnyRefined = items.some((item) => item.isRefined);
 
-   let filteredItems = isAnyRefined
+   const filteredItems = isAnyRefined
       ? items
       : items.filter((item) => hitsCount !== item.count);
 
-   let emptyFacetState = false;
-
-   if (filteredItems.length == 0) {
-      filteredItems = items.filter((item) => hitsCount === item.count);
-      if (filteredItems.length > 0) {
-         emptyFacetState = true;
-      }
-   }
-   return { ...rest, items: filteredItems, isEmptyFacetState: emptyFacetState };
+   return { ...rest, items: filteredItems };
 }
