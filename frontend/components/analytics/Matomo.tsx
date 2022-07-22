551fa50ca675f899e8230af33f66f0b90dadf7aa
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-07-22 14:02:17 -0700

    analytics: Add Matomo script

---
+++ frontend/components/analytics/Matomo.tsx
@@ -0,0 +1,27 @@
+import { MATOMO_URL } from '@config/env';
+import Script from 'next/script';
+
+export function Matomo() {
+   return MATOMO_URL ? (
+      <Script
+         id="matomo-analytics"
+         onError={(e) => {
+            console.error('Failed to load Matomo analytics script', e);
+         }}
+      >
+         {`
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+               var u='${MATOMO_URL}';
+               _paq.push(['setTrackerUrl', u+'js/tracker.php']);
+               _paq.push(['setSiteId', '1']);
+               var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+               g.async=true; g.src=u+'js/tracker.php'; s.parentNode.insertBefore(g,s);
+            })();
+         `}
+      </Script>
+   ) : null;
+}
