08c3b69a869e7a31cf862511096e58ba18905428
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-07-22 14:24:27 -0700

    fix: Missing slash in url
    The url's didn't have a trailing slash, and the path the url was
    concatenated with in the Matomo script didn't have a leading slash, so
    the result was a bad url.
    
    Let's add a leading slash to the path. If for some reason the url's have
    a trailing slash (or don't) in any environment, the link will still be
    valid.

--- frontend/components/analytics/Matomo.tsx
+++ frontend/components/analytics/Matomo.tsx
@@ -16,10 +16,10 @@ export function Matomo() {
             _paq.push(['enableLinkTracking']);
             (function() {
                var u='${MATOMO_URL}';
-               _paq.push(['setTrackerUrl', u+'js/tracker.php']);
+               _paq.push(['setTrackerUrl', u+'/js/tracker.php']);
                _paq.push(['setSiteId', '1']);
                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-               g.async=true; g.src=u+'js/tracker.php'; s.parentNode.insertBefore(g,s);
+               g.async=true; g.src=u+'/js/tracker.php'; s.parentNode.insertBefore(g,s);
             })();
          `}
       </Script>
