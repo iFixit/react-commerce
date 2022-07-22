9b30b303f31127e742bc4f799f1a0196b182f4d8
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-07-22 16:09:50 -0700

    matomo: dangerouslySetInnerHTML
    When we dropped the body of the script between the script tags, special
    characters in the script were getting html escaped.
    
    We can set the html directly with this attribute instead.
    https://nextjs.org/docs/basic-features/script#configuration

--- frontend/components/analytics/Matomo.tsx
+++ frontend/components/analytics/Matomo.tsx
@@ -4,21 +4,24 @@ import Head from 'next/head';
 export function Matomo() {
    return MATOMO_URL ? (
       <Head>
-         <script key="matomo-analytics">
-            {`
-            var _paq = window._paq = window._paq || [];
-            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-               var u='${MATOMO_URL}';
-               _paq.push(['setTrackerUrl', u+'/js/tracker.php']);
-               _paq.push(['setSiteId', '1']);
-               var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-               g.async=true; g.src=u+'/js/tracker.php'; s.parentNode.insertBefore(g,s);
-            })();
-            `}
-         </script>
+         <script
+            key="matomo-analytics"
+            dangerouslySetInnerHTML={{
+               __html: `
+                  var _paq = window._paq = window._paq || [];
+                  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                  _paq.push(['trackPageView']);
+                  _paq.push(['enableLinkTracking']);
+                  (function() {
+                     var u='${MATOMO_URL}';
+                     _paq.push(['setTrackerUrl', u+'/js/tracker.php']);
+                     _paq.push(['setSiteId', '1']);
+                     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                     g.async=true; g.src=u+'/js/tracker.php'; s.parentNode.insertBefore(g,s);
+                  })();
+               `,
+            }}
+         />
       </Head>
    ) : null;
 }
