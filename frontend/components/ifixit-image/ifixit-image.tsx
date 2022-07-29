2acf53bdae757c7c1edf0c5e0f0c1686702957f8
John Yee <johnyee@ifixit.com>
2022-07-29 09:29:37 -0700

    Cleanup: Reorder function/variable definitions

--- frontend/components/ifixit-image/ifixit-image.tsx
+++ frontend/components/ifixit-image/ifixit-image.tsx
@@ -1,5 +1,33 @@
 import Image, { ImageProps, ImageLoader, ImageLoaderProps } from 'next/image';
 
+interface SizeMap {
+   [index: string]: number;
+}
+
+const guideImageSizeMap: SizeMap = {
+   mini: 56,
+   thumbnail: 96,
+   '140x105': 140,
+   '200x150': 200,
+   standard: 300,
+   '440x330': 440,
+   medium: 592,
+   large: 800,
+   huge: 1600,
+};
+
+const cartImageSizeMap: SizeMap = {
+   mini: 41,
+   thumbnail: 70,
+   size110: 110,
+   size170: 170,
+   size250: 250,
+   size400: 400,
+   medium: 600,
+   size1000: 1000,
+   large: 3000,
+};
+
 export function IfixitImage(props: ImageProps) {
    let loader = props.loader;
 
@@ -40,34 +68,6 @@ function getImageLoader(sizeMap: SizeMap, defaultSize: string): ImageLoader {
       return baseSrc.concat('.', sizeName, `?width=${width}`);
    };
 }
-
-interface SizeMap {
-   [index: string]: number;
-}
-
-const guideImageSizeMap: SizeMap = {
-   mini: 56,
-   thumbnail: 96,
-   '140x105': 140,
-   '200x150': 200,
-   standard: 300,
-   '440x330': 440,
-   medium: 592,
-   large: 800,
-   huge: 1600,
-};
-
-const cartImageSizeMap: SizeMap = {
-   mini: 41,
-   thumbnail: 70,
-   size110: 110,
-   size170: 170,
-   size250: 250,
-   size400: 400,
-   medium: 600,
-   size1000: 1000,
-   large: 3000,
-};
 
 const getImageSize = (width: number, sizeMap: SizeMap, defaultSize: string) => {
    const sortedSizes = Object.entries(sizeMap).sort((a, b) => a[1] - b[1]);
