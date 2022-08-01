6897be440754ec32d45c5927599a58a3f5980937
Daniel Beardsley <daniel@ifixit.com>
2022-08-01 16:17:43 -0700

    Frontend User API: catch 401s and return null
    401 is a successful response from this API. The new ifixit-api-client
    throws an exception in this case, so let's catch that and turn it into
    null.
    
    Also, this catches invariant failures and returns null. I think this
    makes sense cause rendering the page in anon mod is preferable to not
    rendering it at all.

--- packages/auth-sdk/user.tsx
+++ packages/auth-sdk/user.tsx
@@ -20,7 +20,7 @@ export function useAuthenticatedUser() {
    const apiClient = useIFixitApiClient();
    const query = useQuery(
       userKeys.user,
-      () => fetchAuthenticatedUser(apiClient),
+      () => fetchAuthenticatedUser(apiClient).catch(() => null),
       {
          retryOnMount: false,
          staleTime: Infinity,
@@ -39,50 +39,44 @@ async function fetchAuthenticatedUser(
       },
    });
 
-   if (response.ok) {
-      const payload = await response.json();
-      invariant(isRecord(payload), 'unexpected api response');
-      invariant(typeof payload.userid === 'number', 'User ID is not a number');
-      invariant(
-         payload.algoliaApiKeyProducts === null ||
-            payload.algoliaApiKeyProducts === undefined ||
-            typeof payload.algoliaApiKeyProducts === 'string',
-         'algoliaApiKeyProducts should be a string or null'
-      );
-      invariant(
-         typeof payload.username === 'string',
-         'User username is not a string'
-      );
-      let thumbnailUrl: string | null = null;
-      if (
-         isRecord(payload.image) &&
-         typeof payload.image.thumbnail === 'string'
-      ) {
-         thumbnailUrl = payload.image.thumbnail;
-      }
-
-      const unique_username =
-         typeof payload.unique_username == 'string'
-            ? payload.unique_username
-            : null;
-
-      const discountTier =
-         typeof payload.discount_tier === 'string'
-            ? payload.discount_tier
-            : null;
-      return {
-         id: payload.userid,
-         username: payload.username,
-         handle: unique_username,
-         thumbnail: thumbnailUrl,
-         is_pro: discountTier != null,
-         algoliaApiKeyProducts: payload.algoliaApiKeyProducts,
-         discountTier,
-      };
+   const payload = await response.json();
+   invariant(isRecord(payload), 'unexpected api response');
+   invariant(typeof payload.userid === 'number', 'User ID is not a number');
+   invariant(
+      payload.algoliaApiKeyProducts === null ||
+         payload.algoliaApiKeyProducts === undefined ||
+         typeof payload.algoliaApiKeyProducts === 'string',
+      'algoliaApiKeyProducts should be a string or null'
+   );
+   invariant(
+      typeof payload.username === 'string',
+      'User username is not a string'
+   );
+   let thumbnailUrl: string | null = null;
+   if (
+      isRecord(payload.image) &&
+      typeof payload.image.thumbnail === 'string'
+   ) {
+      thumbnailUrl = payload.image.thumbnail;
    }
 
-   if (response.status === 401) {
-      return null;
-   }
-   throw new Error(response.statusText);
+   const unique_username =
+      typeof payload.unique_username == 'string'
+         ? payload.unique_username
+         : null;
+
+   const discountTier =
+      typeof payload.discount_tier === 'string'
+         ? payload.discount_tier
+         : null;
+
+   return {
+      id: payload.userid,
+      username: payload.username,
+      handle: unique_username,
+      thumbnail: thumbnailUrl,
+      is_pro: discountTier != null,
+      algoliaApiKeyProducts: payload.algoliaApiKeyProducts,
+      discountTier,
+   };
 }
