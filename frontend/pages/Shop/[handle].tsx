9816070e4553cfa7c29ae0bbaf86cb40a5d534bb
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-03 09:10:59 -0700

    Revert #341: Cache for only 10 sec, not 10 min
    We were setting the max age of the cache to 10 min when we were on Cloudfront because Cloudfront doesn't respect the SWR header.
    
    Now that we're on Vercel, we can drop max age back to 10 seconds and set a 10 minute SWR.
    
    https://vercel.com/docs/concepts/edge-network/caching#serverless-functions---lambdas

--- frontend/pages/Shop/[handle].tsx
+++ frontend/pages/Shop/[handle].tsx
@@ -25,7 +25,7 @@ const getServerSidePropsInternal: GetServerSideProps<AppPageProps> = async (
 ) => {
    context.res.setHeader(
       'Cache-Control',
-      'public, s-maxage=600, stale-while-revalidate=1200'
+      'public, s-maxage=10, stale-while-revalidate=600'
    );
 
    const { handle } = context.params || {};
