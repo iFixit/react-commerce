7ff5c5564c74db2fbfd0c1b0114f097f45861b64
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-03 14:57:31 -0700

    rename: Use official `fetch()` param names
    According to MDN, the fetch params are `resource` and `options`:
    https://developer.mozilla.org/en-US/docs/Web/API/fetch
    
    According to typescript, they're `input` and `init`.
    https://github.com/microsoft/TypeScript/blob/d6d26430c7a64d3a740a7705db6ad63a8bd1c59c/lib/lib.dom.d.ts#L18664
    
    I find MDN's convention to be more clear.

--- packages/sentry/index.tsx
+++ packages/sentry/index.tsx
@@ -1,15 +1,15 @@
 import * as Sentry from '@sentry/nextjs';
 
-export const sentryFetch: typeof fetch = async (input, init) => {
+export const sentryFetch: typeof fetch = async (resource, options) => {
    const context = {
-      input,
-      credentials: init?.credentials,
-      headers: init?.headers,
-      method: init?.method,
-      parameters: init?.body,
+      resource,
+      credentials: options?.credentials,
+      headers: options?.headers,
+      method: options?.method,
+      parameters: options?.body,
    };
    Sentry.configureScope((scope) => scope.setContext('request', context));
-   return fetch(input, init).catch((error) => {
+   return fetch(resource, options).catch((error) => {
       console.error(error, context);
       throw error;
    });
