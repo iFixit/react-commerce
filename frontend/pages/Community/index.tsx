cf768058876aea4a7abd2c85e424255acf40a44a
Andrew Gilbert <andrewg@ifixit.com>
2022-08-04 15:09:44 -0700

    Add a Community page

---
+++ frontend/pages/Community/index.tsx
@@ -0,0 +1,15 @@
+export function getServerSideProps() {
+   return {props: {name: "World"}}
+}
+
+type CommunityPageProps = {
+   name: string;
+}
+
+function CommunityPage({name}: CommunityPageProps) {
+   return <p>
+      Hello {name}
+   </p>
+}
+
+export default CommunityPage;
