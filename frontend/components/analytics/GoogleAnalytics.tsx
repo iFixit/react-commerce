5058ebc6fd29ce89c7b7815947cba3b2f3c7a9cf
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-05 09:02:26 -0700

    GA: Use Next.js `<Script>` component
    This is equivalent to the last implementation, except the script is now in the <body> instead of the <head>. I don't think that makes a functional difference, as pageviews still get sent.
    
    This is how Next.js suggests analytics.js be implemented. Ref:
    
    https://nextjs.org/docs/messages/next-script-for-ga#using-analyticsjs

--- frontend/components/analytics/GoogleAnalytics.tsx
+++ frontend/components/analytics/GoogleAnalytics.tsx
@@ -1,35 +1,33 @@
 import { GA_URL, GA_KEY, GA_DEBUG } from '@config/env';
-import Head from 'next/head';
+import Script from 'next/script';
 
 export function GoogleAnalytics() {
    return GA_URL ? (
-      <Head>
-         <script
-            dangerouslySetInnerHTML={{
-               __html: `
-                  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+      <>
+         <Script id="google-analytics" strategy="afterInteractive">
+            {`
+               window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 
-                  if (${GA_DEBUG} === true) {
-                     window.ga_debug = {trace: true};
-                  }
+               if (${GA_DEBUG} === true) {
+                  window.ga_debug = {trace: true};
+               }
 
-                  ga('create', '${GA_KEY}', 'auto', 'ifixit', {'legacyCookieDomain': 'ifixit.com'});
-                  ga('ifixit.set', 'anonymizeIp', true);
+               ga('create', '${GA_KEY}', 'auto', 'ifixit', {'legacyCookieDomain': 'ifixit.com'});
+               ga('ifixit.set', 'anonymizeIp', true);
 
-                  // Do not lazy load.
-                  ga('ifixit.set', 'dimension2', '0');
+               // Do not lazy load.
+               ga('ifixit.set', 'dimension2', '0');
 
-                  // Load the enhanced ecommerce plug-in.
-                  ga('ifixit.require', 'ec');
+               // Load the enhanced ecommerce plug-in.
+               ga('ifixit.require', 'ec');
 
-                  // Enable Remarketing and Advertising Reporting Features in GA
-                  ga('ifixit.require', 'displayfeatures');
+               // Enable Remarketing and Advertising Reporting Features in GA
+               ga('ifixit.require', 'displayfeatures');
 
-                  ga('ifixit.send', 'pageview');
-               `,
-            }}
-         />
-         <script async src={`${GA_URL}`}></script>
-      </Head>
+               ga('ifixit.send', 'pageview');
+            `}
+         </Script>
+         <Script src={`${GA_URL}`} strategy="afterInteractive" />
+      </>
    ) : null;
 }
