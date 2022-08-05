e1d9f016f7533b532f470517fe3edff935b9f68f
hcutrone <hcutrone@calpoly.edu>
2022-08-05 09:34:35 -0700

    Minor white space fix

--- packages/homepage-kpis/index.tsx
+++ packages/homepage-kpis/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
-  Box,
-  SimpleGrid,
-  Stat,
-  StatHelpText,
-  StatNumber,
+   Box,
+   SimpleGrid,
+   Stat,
+   StatHelpText,
+   StatNumber,
 } from '@chakra-ui/react';
 import { PageContentWrapper } from './PageContentWrapper';
 
@@ -14,7 +14,7 @@ interface Stat {
 }
 
 export interface StatsSectionProps {
-  data: SectionData;
+   data: SectionData;
 }
 
 export interface SectionData {
@@ -24,58 +24,58 @@ export interface SectionData {
 }
 
 export function StatsSection({ data: { stats } }: StatsSectionProps) {
-  return (
-     <Box
-        as="section"
-        position="relative"
-        w="full"
-        bg="blue.50"
-        borderWidth={1}
-        borderColor="blue.100"
-     >
-        <PageContentWrapper>
-           <SimpleGrid
-              columns={{
-                 base: 1,
-                 sm: 2,
-                 lg: 4,
-              }}
-              spacing="10"
-              py="10"
-           >
-              {stats.map((stat, index) => (
-                 <Stats
-                    key={index}
-                    number={stat.value}
-                    helpText={stat.label}
-                 />
-              ))}
-           </SimpleGrid>
-        </PageContentWrapper>
-     </Box>
-  );
+   return (
+      <Box
+         as="section"
+         position="relative"
+         w="full"
+         bg="blue.50"
+         borderWidth={1}
+         borderColor="blue.100"
+      >
+         <PageContentWrapper>
+            <SimpleGrid
+               columns={{
+                  base: 1,
+                  sm: 2,
+                  lg: 4,
+               }}
+               spacing="10"
+               py="10"
+            >
+               {stats.map((stat, index) => (
+                  <Stats
+                     key={index}
+                     number={stat.value}
+                     helpText={stat.label}
+                  />
+               ))}
+            </SimpleGrid>
+         </PageContentWrapper>
+      </Box>
+   );
 }
 
 interface StatsProps {
-  number: string;
-  helpText: string;
+   number: string;
+   helpText: string;
 }
 
 function Stats({ number, helpText }: StatsProps) {
-  return (
-     <Stat
-        sx={{
-           dl: {
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-           },
-        }}
-     >
-        <StatNumber color="blue.500" fontSize="3xl">
-           {number}
-        </StatNumber>
-        <StatHelpText m="0">{helpText}</StatHelpText>
-     </Stat>
-  );
+   return (
+      <Stat
+         sx={{
+            dl: {
+               display: 'flex',
+               flexDirection: 'column',
+               alignItems: 'center',
+            },
+         }}
+      >
+         <StatNumber color="blue.500" fontSize="3xl">
+            {number}
+         </StatNumber>
+         <StatHelpText m="0">{helpText}</StatHelpText>
+      </Stat>
+   );
 }
