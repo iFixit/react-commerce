2da764b049cc89585e055fc63d0178d9be85a069
hcutrone <hcutrone@calpoly.edu>
2022-08-05 09:30:07 -0700

    Grab KIP banner from WIP branch
    Our updated footer does not include our KPIs, but we still want to
    show them on the homepage. We have a KPI banner in a WIP branch,
    so let's grab that and put it in its own package to be used in
    IFixit/ifixit.

---
+++ packages/homepage-kpis/index.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  Box,
+  SimpleGrid,
+  Stat,
+  StatHelpText,
+  StatNumber,
+} from '@chakra-ui/react';
+import { PageContentWrapper } from './PageContentWrapper';
+
+interface Stat {
+   value: string;
+   label: string;
+}
+
+export interface StatsSectionProps {
+  data: SectionData;
+}
+
+export interface SectionData {
+   type: 'Stats';
+   id: string;
+   stats: Stat[];
+}
+
+export function StatsSection({ data: { stats } }: StatsSectionProps) {
+  return (
+     <Box
+        as="section"
+        position="relative"
+        w="full"
+        bg="blue.50"
+        borderWidth={1}
+        borderColor="blue.100"
+     >
+        <PageContentWrapper>
+           <SimpleGrid
+              columns={{
+                 base: 1,
+                 sm: 2,
+                 lg: 4,
+              }}
+              spacing="10"
+              py="10"
+           >
+              {stats.map((stat, index) => (
+                 <Stats
+                    key={index}
+                    number={stat.value}
+                    helpText={stat.label}
+                 />
+              ))}
+           </SimpleGrid>
+        </PageContentWrapper>
+     </Box>
+  );
+}
+
+interface StatsProps {
+  number: string;
+  helpText: string;
+}
+
+function Stats({ number, helpText }: StatsProps) {
+  return (
+     <Stat
+        sx={{
+           dl: {
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+           },
+        }}
+     >
+        <StatNumber color="blue.500" fontSize="3xl">
+           {number}
+        </StatNumber>
+        <StatHelpText m="0">{helpText}</StatHelpText>
+     </Stat>
+  );
+}
