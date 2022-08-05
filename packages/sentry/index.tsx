6dab9b05a6f200a324b30aef6dbc92a0707be6b2
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-05 13:24:35 -0700

    sentry: Report `fetch()` failures
    Sentry only captures unhandled errors. `fetch()` doesn't throw for HTTP status errors, so we need to manually capture those events/log them.
    
    In the event of a network/CORS issue, `fetch()` will throw. We probably don't want to hear about most of these, so I opted to log them but not report them to Sentry.
    
    This also fixes an issue where we always added the request data to Sentry's global scope, whether the request succeeded or not. This only adds it to the scope when there is an HTTP error.

--- packages/sentry/index.tsx
+++ packages/sentry/index.tsx
@@ -2,15 +2,29 @@ import * as Sentry from '@sentry/nextjs';
 
 export const sentryFetch: typeof fetch = async (resource, options) => {
    const context = {
-      resource,
+      // Underscore sorts the resource first in Sentry's UI
+      _resource: resource,
       credentials: options?.credentials,
       headers: options?.headers,
       method: options?.method,
-      parameters: options?.body,
+      // Parse to pretty print GraphQL queries
+      body: options?.body ? JSON.parse(String(options?.body)) : undefined,
    };
-   Sentry.configureScope((scope) => scope.setContext('request', context));
-   return fetch(resource, options).catch((error) => {
-      console.error(error, context);
-      throw error;
-   });
+   return fetch(resource, options)
+      .then((response) => {
+         if (response.status >= 400 && response.status !== 401) {
+            const msg = `\`fetch()\` HTTP error: ${response.status} ${response.statusText}`;
+            Sentry.captureException(new Error(msg), (scope) => {
+               scope.setContext('request', context);
+               return scope;
+            });
+            console.error(msg, context);
+         }
+         return response;
+      })
+      .catch((error) => {
+         // We don't want to hear about network errors in Sentry
+         console.error(error, context);
+         throw error;
+      });
 };
