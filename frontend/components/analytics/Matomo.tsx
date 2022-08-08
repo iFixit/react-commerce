79caf451a51646df61ad4722cd3e50755573044e
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-08 10:26:45 -0700

    matomo: Track next router changes
    Similar to how we track route changes for Google Analytics.

--- frontend/components/analytics/Matomo.tsx
+++ frontend/components/analytics/Matomo.tsx
@@ -1,7 +1,28 @@
 import { MATOMO_URL } from '@config/env';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import * as React from 'react';
+
+declare const _paq: string[][] | undefined;
 
 export function Matomo() {
+   const router = useRouter();
+   React.useEffect(() => {
+      const handleRouteChange = (url: string) => {
+         if (typeof window !== 'undefined' && _paq) {
+            _paq.push(['setCustomUrl', url]);
+            _paq.push(['setDocumentTitle', document.title]);
+            _paq.push(['trackPageView']);
+         }
+      };
+      router.events.on('routeChangeComplete', handleRouteChange);
+      router.events.on('hashChangeComplete', handleRouteChange);
+      return () => {
+         router.events.off('routeChangeComplete', handleRouteChange);
+         router.events.off('hashChangeComplete', handleRouteChange);
+      };
+   }, [router?.events]);
+
    return MATOMO_URL ? (
       <Head>
          <script
