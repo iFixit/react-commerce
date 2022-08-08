8e4494d1e4516a4416182c2ca8bf0877d71dc7ff
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-08 10:36:21 -0700

    matomo: Use Next `<Script>` component
    This won't block the page loading on the analytics script running.
    
    This puts the Matomo script in the body instead of the head now. As long as it still runs successfully, I don't think it matters.

--- frontend/components/analytics/Matomo.tsx
+++ frontend/components/analytics/Matomo.tsx
@@ -1,7 +1,7 @@
 import { MATOMO_URL } from '@config/env';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
 import * as React from 'react';
+import Script from 'next/script';
 
 declare const _paq: string[][] | undefined;
 
@@ -24,25 +24,20 @@ export function Matomo() {
    }, [router?.events]);
 
    return MATOMO_URL ? (
-      <Head>
-         <script
-            key="matomo-analytics"
-            dangerouslySetInnerHTML={{
-               __html: `
-                  var _paq = window._paq = window._paq || [];
-                  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-                  _paq.push(['trackPageView']);
-                  _paq.push(['enableLinkTracking']);
-                  (function() {
-                     var u='${MATOMO_URL}';
-                     _paq.push(['setTrackerUrl', u+'/minerva.php']);
-                     _paq.push(['setSiteId', '1']);
-                     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                     g.async=true; g.src=u+'/minerva.php'; s.parentNode.insertBefore(g,s);
-                  })();
-               `,
-            }}
-         />
-      </Head>
+      <Script id="matomo-analytics" strategy="afterInteractive">
+         {`
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+               var u='${MATOMO_URL}';
+               _paq.push(['setTrackerUrl', u+'/minerva.php']);
+               _paq.push(['setSiteId', '1']);
+               var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+               g.async=true; g.src=u+'/minerva.php'; s.parentNode.insertBefore(g,s);
+            })();
+         `}
+      </Script>
    ) : null;
 }
