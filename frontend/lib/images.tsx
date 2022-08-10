ab09858f2a3ea9bd48cab97205bce5067dcbec80
hcutrone <hcutrone@calpoly.edu>
2022-08-10 16:13:18 -0700

    Community images: use cloudfront
    Let's make sure we are using cloudfront for our images and not S3
    nor www.ifixit.com

--- frontend/lib/images.tsx
+++ frontend/lib/images.tsx
@@ -1,5 +1,5 @@
 export function getImage(path: string): string {
-   const siteName = 'https://www.ifixit.com/';
+   const siteName = 'https://assets.cdn.ifixit.com/';
    const staticDir = 'static/images/';
    return siteName + staticDir + path;
 }
