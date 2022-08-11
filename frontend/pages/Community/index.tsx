5376b7d722488221be52d74277b321c086de5a20
danielcliu-ifixit <daniell@ifixit.com>
2022-08-11 14:40:12 -0700

    Minor prettier fixes.

--- frontend/pages/Community/index.tsx
+++ frontend/pages/Community/index.tsx
@@ -1,15 +1,13 @@
 export function getServerSideProps() {
-   return {props: {name: "World"}}
+   return { props: { name: 'World' } };
 }
 
 type CommunityPageProps = {
    name: string;
-}
+};
 
-function CommunityPage({name}: CommunityPageProps) {
-   return <p>
-      Hello {name}
-   </p>
+function CommunityPage({ name }: CommunityPageProps) {
+   return <p>Hello {name}</p>;
 }
 
 export default CommunityPage;
