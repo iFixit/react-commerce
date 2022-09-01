64981f9e0c24cb740e2cee6a205e4709a94532e6
hcutrone <hcutrone@calpoly.edu>
2022-09-01 09:14:20 -0700

    Guide Footer: add hardcoded data
    I am making this new footer so that we have a footer that is not
    completely reliant on cart-based props being passed in. For now,
    we are going to use hardcoded data until we wire in Strapi.
    
    Note: this data looks a bit different than the current footer. I
    followed the Figma for the new footer which has a slightly
    different layout.

---
+++ packages/ui/footer/guideData.ts
@@ -0,0 +1,98 @@
+const baseUrl = 'https://www.ifixit.com/';
+
+export const getGuideSocialMediaAccounts = () => {
+   return {
+      facebook: 'https://www.facebook.com/iFixit/',
+      twitter: 'https://twitter.com/ifixit',
+      instagram: 'https://www.instagram.com/ifixit/',
+      youtube: 'https://www.youtube.com/user/iFixitYourself',
+      repairOrg: 'https://www.repair.org/',
+   };
+};
+
+export const getGuideFooterMenus = () => {
+   return {
+      menu1: {
+         items: [
+            { type: 'link', name: 'About Us', url: baseUrl + 'index' },
+            {
+               type: 'link',
+               name: 'Customer Support',
+               url: 'https://help.ifixit.com',
+            },
+            { type: 'link', name: 'Careers', url: baseUrl + 'Info/jobs' },
+            { type: 'link', name: 'Feedback', url: 'https://meta.ifixit.com' },
+            { type: 'link', name: 'Newsletter', url: baseUrl + 'Newsletter' },
+            { type: 'link', name: 'API', url: baseUrl + 'api/2.0/doc' },
+         ],
+      },
+      menu2: {
+         items: [
+            { type: 'link', name: 'Press', url: baseUrl + 'Info/Media' },
+            {
+               type: 'link',
+               name: 'News',
+               url: baseUrl + 'Page/all-categories',
+            },
+            { type: 'link', name: 'Contribute', url: baseUrl + 'Contribute' },
+            {
+               type: 'link',
+               name: 'Pro Wholesale',
+               url: 'https://pro.ifixit.com/',
+            },
+            { type: 'link', name: 'Retail Locator', url: baseUrl + 'Retail' },
+            {
+               type: 'link',
+               name: 'For Manufacturers',
+               url: baseUrl + 'Page/services',
+            },
+         ],
+      },
+      menu3: {
+         items: [
+            {
+               type: 'link',
+               url: baseUrl + 'Info/Accessibility',
+               name: 'Accessibility',
+            },
+            { type: 'link', url: baseUrl + 'Info/Privacy', name: 'Privacy' },
+            { type: 'link', url: baseUrl + 'Info/Terms_of_Use', name: 'Terms' },
+         ],
+      },
+      bottomMenu: {
+         items: [
+            {
+               type: 'link',
+               url: baseUrl + 'Info/Licensing',
+               name: 'Licensed under Creative Commons',
+            },
+         ],
+      },
+   };
+};
+
+export const getNewsletterForm = () => {
+   return {
+      title: 'Stay in the loop',
+      subtitle: 'Learn something new every month!',
+      callToActionButtonTitle: 'Subscribe',
+      inputPlaceholder: 'Enter your email',
+   };
+};
+
+export const getStores = () => {
+   return [
+      {
+         code: 'us',
+         name: 'United States',
+         url: 'https://www.ifixit.com/Store',
+         currency: 'USD',
+      },
+      {
+         code: 'eu',
+         name: 'Europe',
+         url: 'https://eustore.ifixit.com/',
+         currency: 'EUR',
+      },
+   ];
+};
