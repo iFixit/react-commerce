f45c5415545371524d8f879e91061c0898da012b
hcutrone <hcutrone@calpoly.edu>
2022-09-02 15:16:25 -0700

    SocialMedia component: use React.memo
    This is a great opportunity to use React.memo! This component only
    relies on one prop, so we shouldn't re-render if any of the other props
    change.
    
    Also, let's make sure to not render anything if we don't have any
    accounts.

--- packages/ui/footer/guide/components/SocialMedia.tsx
+++ packages/ui/footer/guide/components/SocialMedia.tsx
@@ -8,25 +8,31 @@ import {
 } from '@ifixit/icons';
 import { SocialMediaAccounts } from '@models/store';
 import { FooterLink } from './Shared';
+import React from 'react';
 
-export const FooterSocialMediaSection = ({ accounts }: SocialMediaAccounts) => {
-   return (
-      <HStack spacing={4} justify={{ base: 'space-between', sm: 'center' }}>
-         {accounts.facebook && (
-            <FooterLink href={accounts.facebook} icon={FacebookLogo} />
-         )}
-         {accounts.twitter && (
-            <FooterLink href={accounts.twitter} icon={TwitterLogo} />
-         )}
-         {accounts.instagram && (
-            <FooterLink href={accounts.instagram} icon={InstagramLogo} />
-         )}
-         {accounts.youtube && (
-            <FooterLink href={accounts.youtube} icon={YoutubeLogo} />
-         )}
-         {accounts.repairOrg && (
-            <FooterLink href={accounts.repairOrg} icon={RepairOrgLogo} />
-         )}
-      </HStack>
-   );
-};
+export const FooterSocialMediaSection = React.memo(
+   ({ accounts }: SocialMediaAccounts) => {
+      if (!accounts) {
+         return null;
+      }
+      return (
+         <HStack spacing={4} justify={{ base: 'space-between', sm: 'center' }}>
+            {accounts.facebook && (
+               <FooterLink href={accounts.facebook} icon={FacebookLogo} />
+            )}
+            {accounts.twitter && (
+               <FooterLink href={accounts.twitter} icon={TwitterLogo} />
+            )}
+            {accounts.instagram && (
+               <FooterLink href={accounts.instagram} icon={InstagramLogo} />
+            )}
+            {accounts.youtube && (
+               <FooterLink href={accounts.youtube} icon={YoutubeLogo} />
+            )}
+            {accounts.repairOrg && (
+               <FooterLink href={accounts.repairOrg} icon={RepairOrgLogo} />
+            )}
+         </HStack>
+      );
+   }
+);
