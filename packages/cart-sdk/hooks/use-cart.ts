10d3995fb1046df80389d8596a5be03f2b0ff9d1
ricky <ricky@ifixit.com>
2022-09-09 14:32:59 -0700

    removed logic for cart

--- packages/cart-sdk/hooks/use-cart.ts
+++ packages/cart-sdk/hooks/use-cart.ts
@@ -10,16 +10,17 @@ const CART_NOT_AVAILABLE = 'cart is not available';
  * Get the cart from the API.
  */
 export function useCart() {
-   const client = useIFixitApiClient();
-   const query = useQuery(cartKeys.cart, async (): Promise<Cart | null> => {
-      const result = await client.get('store/user/cart');
-      invariant(isValidCartPayload(result), CART_NOT_AVAILABLE);
-      return {
-         ...result.cart,
-         hasItemsInCart: result.cart.totalNumItems > 0,
-      };
-   });
-   return query;
+   return null;
+   //const client = useIFixitApiClient();
+   //const query = useQuery(cartKeys.cart, async (): Promise<Cart | null> => {
+   //   const result = await client.get('store/user/cart');
+   //   invariant(isValidCartPayload(result), CART_NOT_AVAILABLE);
+   //   return {
+   //      ...result.cart,
+   //      hasItemsInCart: result.cart.totalNumItems > 0,
+   //   };
+   //});
+   // return query;
 }
 
 function isValidCartPayload(data: any): data is CartAPIResponse {
