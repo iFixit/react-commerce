1c8088f5ac1990d2773585d34f0718cac8632fe5
sumukhipandey <sumukhi.a.pandey@gmail.com>
2022-09-27 14:27:37 -0700

    Check if the item count for that filter > 0
    In the issue, we see that Hard Drives is being displayed even
    though it has an item count of 0. I think we should not display
    facets if their count = 0.

--- frontend/templates/product-list/sections/FilterableProductsSection/RefinementSingleSelect.tsx
+++ frontend/templates/product-list/sections/FilterableProductsSection/RefinementSingleSelect.tsx
@@ -144,17 +144,20 @@ const SingleSelectItem = React.memo(function SingleSelectItem({
       RefinementTitle = TitleText;
    }
 
-   return (
-      <HStack
-         key={item.label}
-         justify="space-between"
-         color={item.isRefined ? 'brand.500' : 'inherit'}
-         fontWeight={item.isRefined ? 'bold' : 'inherit'}
-      >
-         {RefinementTitle}
-         <Text size="sm" fontFamily="sans-serif" color={'gray.500'}>
-            {item.count}
-         </Text>
-      </HStack>
-   );
+   if (item.count > 0) {
+      return (
+         <HStack
+            key={item.label}
+            justify="space-between"
+            color={item.isRefined ? 'brand.500' : 'inherit'}
+            fontWeight={item.isRefined ? 'bold' : 'inherit'}
+         >
+            {RefinementTitle}
+            <Text size="sm" fontFamily="sans-serif" color={'gray.500'}>
+               {item.count}
+            </Text>
+         </HStack>
+      );
+   }
+   return null;
 });
