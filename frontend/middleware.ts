2d139a8ba175468683362df81646fbabc7407fdd
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-09-28 15:07:39 -0700

    refactor: simplify middleware header logic
    If we want to add more headers in the future, we'll refactor the code to accomodate that.

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,16 +1,11 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { PROD_HOSTNAME } from '@config/constants';
-import { isNonNullable } from '@ifixit/helpers';
-
-type Header = [string, string] | null;
 
 export function middleware(request: NextRequest) {
-   const robotTag: Header =
+   const headers =
       request.nextUrl.hostname === PROD_HOSTNAME
-         ? ['X-Robots-Tag', 'noindex, nofollow']
-         : null;
-
-   const headers = [robotTag].filter(isNonNullable);
+         ? { 'X-Robots-Tag': 'noindex, nofollow' }
+         : undefined;
    return NextResponse.next({ headers });
 }
