bac2cda2c2899eb6e30b3e647c25d07511a98ffd
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-10-03 10:21:31 -0700

    test: return plain next response from middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,11 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { PROD_HOSTNAME } from '@config/constants';
 
 export function middleware(request: NextRequest) {
-   const init =
-      request.nextUrl.hostname === PROD_HOSTNAME
-         ? { headers: [['X-Robots-Tag', 'noindex, nofollow']] }
-         : undefined;
-   return NextResponse.next(init);
+   return NextResponse.next();
 }
