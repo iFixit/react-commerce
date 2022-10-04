97d024258a85e33ef0b971ef161480f9d64bef42
hcutrone <harrison@ifixit.com>
2022-10-04 14:46:03 -0700

    KPIs: move to and export from footer package
    The KPI banner isn't _technically_ part of the footer, but it is
    displayed right along side it and uses the PageContentWrapper, so let's
    go ahead and move it into the footer package.

--- packages/footer/index.tsx
+++ packages/footer/index.tsx
@@ -4,3 +4,4 @@ export * from './components/Partners';
 export * from './components/Settings';
 export * from './components/Shared';
 export * from './components/StoreMenu';
+export * from './homepage-kpis';
