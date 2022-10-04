13a3b9a456d9c3e41092ee85fcad3144952d2a5b
Massimo De Marchi <massimo@inkofpixel.com>
2022-10-04 19:29:40 +0200

    fix(product page): Fix variant selection logic break on client side routing

--- frontend/templates/product/hooks/useSelectedVariant.ts
+++ frontend/templates/product/hooks/useSelectedVariant.ts
@@ -1,19 +1,38 @@
+import { usePrevious } from '@ifixit/ui';
 import { Product } from '@models/product';
 import React from 'react';
 
 export function useSelectedVariant(product: Product) {
-   const [selectedVariantId, setSelectedVariantId] = React.useState(() => {
-      const variant =
-         product.variants.find(
-            (variant) =>
-               variant.quantityAvailable && variant.quantityAvailable > 0
-         ) ?? product.variants[0];
-      return variant.id;
-   });
+   const defaultVariantId = getDefaultVariantId(product);
+   const previousDefaultVariantId = usePrevious(defaultVariantId);
+   const [selectedVariantId, setSelectedVariantId] = React.useState<
+      string | null
+   >();
+
+   const hasDefaultVariantChanged =
+      previousDefaultVariantId !== defaultVariantId;
+   const currentId =
+      hasDefaultVariantChanged || selectedVariantId == null
+         ? defaultVariantId
+         : selectedVariantId;
+
+   React.useEffect(() => {
+      if (hasDefaultVariantChanged) {
+         setSelectedVariantId(null);
+      }
+   }, [hasDefaultVariantChanged]);
+
    const selectedVariant = React.useMemo(() => {
-      return product.variants.find(
-         (variant) => variant.id === selectedVariantId
-      )!;
-   }, [product.variants, selectedVariantId]);
+      return product.variants.find((variant) => variant.id === currentId)!;
+   }, [product.variants, currentId]);
+
    return { selectedVariant, setSelectedVariantId };
+}
+
+function getDefaultVariantId(product: Product) {
+   const variant =
+      product.variants.find(
+         (variant) => variant.quantityAvailable && variant.quantityAvailable > 0
+      ) ?? product.variants[0];
+   return variant.id;
 }
