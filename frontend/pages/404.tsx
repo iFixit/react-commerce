4865bbc2276227aaea90a37a7daf31e776d61322
ricky <ricky@ifixit.com>
2022-10-07 14:27:08 -0700

    updated 404 page formatting

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,40 +1,47 @@
-import { Center, Box, Text, Button, Container, Flex } from '@chakra-ui/react';
-import { DEFAULT_STORE_CODE } from '@config/env';
-import { DefaultLayout, DefaultLayoutProps } from '@layouts/default';
-import { getGlobalSettings } from '@models/global-settings';
-import { getStoreByCode, getStoreList } from '@models/store';
+import { Box, Center, Text, Divider} from '@chakra-ui/react';
+import { DefaultLayout, DefaultLayoutProps, getLayoutServerSideProps } from '@layouts/default';
 
 export default function Custom404(layoutProps: DefaultLayoutProps) {
    return (
       <DefaultLayout {...layoutProps}>
-         {' '}
          {
-            <Center h="56.6vh">
-               <Text
-                  fontSize={20}
-                  fontWeight="bold"
-                  fontFamily="Lato"
-                  padding="16px"
-               >
-                  Error 404: Page not Found
-               </Text>
-            </Center>
+            <Box>
+               <Center h="60vh" flexDirection="column">
+                  <Text
+                     fontSize={36}
+                     fontWeight="bold"
+                     fontFamily="Lato"
+                  >
+                     404
+                  </Text>
+                  <Text
+                     fontSize={24}
+                     fontWeight="semibold"
+                     fontFamily="Lato"
+                     paddingBottom="24px"
+                  >
+                     Page not Found
+                  </Text>
+                  <Divider borderWidth="1px" borderColor="#ef4444" width="50px"/>
+                  <Text
+                     fontSize={20}
+                     fontFamily="Lato"
+                     paddingTop="24px"
+                     overflowWrap="break-word"
+                     align="center"
+                     maxW="360px"
+                  >
+                     We can't find the page you're looking for. Whoops.
+                  </Text>
+               </Center>
+            </Box>
          }
       </DefaultLayout>
    );
 }
 
 export async function getStaticProps() {
-   const [globalSettings, stores, currentStore] = await Promise.all([
-      getGlobalSettings(),
-      getStoreList(),
-      getStoreByCode(DEFAULT_STORE_CODE),
-   ]);
    return {
-      props: {
-         globalSettings,
-         currentStore,
-         stores,
-      },
+      props: await getLayoutServerSideProps()
    };
 }
