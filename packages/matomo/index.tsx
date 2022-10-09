a08ca531cfb2fb0463e159d48b8e708dcf67f5ab
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-10-08 17:11:17 -0700

    refactor(matomo): assign globabl `_paq` on client

--- packages/matomo/index.tsx
+++ packages/matomo/index.tsx
@@ -1,13 +1,14 @@
+const _paq: any[] | undefined =
+   typeof window === 'undefined' ? undefined : (window as any)._paq;
+
 /**
  * @see https://developer.matomo.org/api-reference/tracking-javascript
  */
-
-declare const _paq: string[][] | undefined;
-
 export function trackPageView(url: string) {
-   if (typeof window !== 'undefined' && _paq) {
-      _paq.push(['setCustomUrl', url]);
-      _paq.push(['setDocumentTitle', document.title]);
-      _paq.push(['trackPageView']);
+   if (!_paq) {
+      return;
    }
+   _paq.push(['setCustomUrl', url]);
+   _paq.push(['setDocumentTitle', document.title]);
+   _paq.push(['trackPageView']);
 }
