ae625fa2adbbbf8fb9c44efd488c2684b9ad2046
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-10-08 17:19:17 -0700

    feat: define matamo ecommerce functions
    This defines the functions to track ecommerce events in Matomo. This is adapted from our iFixit/ifixit implementation.
    https://github.com/iFixit/ifixit/tree/master/carpenter-frontend/Shared/Analytics/Matomo

--- packages/matomo/index.tsx
+++ packages/matomo/index.tsx
@@ -1,3 +1,6 @@
+import { CartLineItem } from '@ifixit/cart-sdk';
+import { Money, sumMoney } from '@ifixit/helpers';
+
 const _paq: any[] | undefined =
    typeof window === 'undefined' ? undefined : (window as any)._paq;
 
@@ -11,4 +14,97 @@ export function trackPageView(url: string) {
    _paq.push(['setCustomUrl', url]);
    _paq.push(['setDocumentTitle', document.title]);
    _paq.push(['trackPageView']);
+}
+
+/**
+ * @see https://developer.matomo.org/api-reference/tracking-javascript
+ * @see https://matomo.org/docs/ecommerce-analytics/#tracking-product-views-in-matomo
+ */
+type ProductData = {
+   productSku: string;
+   productName?: string;
+   /**
+    * Category name, or up to five unique categories, e.g. ["Books", "New
+    * Releases", "Technology"]
+    */
+   categoryName?: string | [string, string?, string?, string?, string?];
+   price: Money;
+};
+
+export function trackMatomoEcommerceView(product: ProductData) {
+   if (!_paq) {
+      return;
+   }
+   _paq.push([
+      'setEcommerceView',
+      product.productSku,
+      product.productName,
+      product.categoryName,
+      product.price.amount,
+   ]);
+}
+
+export function trackMatomoCartChange(items: CartLineItem[]) {
+   if (!_paq) {
+      return;
+   }
+   trackClearCart();
+   if (items.length === 0) {
+      return;
+   }
+   items.forEach((item) => {
+      trackAddToCart({
+         productSku: item.itemcode,
+         productName: item.internalDisplayName,
+         price: item.price,
+         quantity: item.quantity,
+      });
+   });
+
+   const totalPrice = sumMoney(items.map((i) => i.price));
+   trackCartUpdated(totalPrice);
+}
+
+/**
+ * @see https://developer.matomo.org/api-reference/tracking-javascript
+ * @see https://matomo.org/docs/ecommerce-analytics/#example-of-adding-a-product-to-the-order
+ */
+type AddToCartData = Omit<ProductData, 'price'> & {
+   price?: Money;
+   /**
+    * How many of this item to add (Defaults to 1)
+    */
+   quantity?: number;
+};
+
+/**
+ * @see https://developer.matomo.org/api-reference/tracking-javascript
+ * @see https://matomo.org/docs/ecommerce-analytics/#example-of-adding-a-product-to-the-order
+ */
+function trackAddToCart(product: AddToCartData) {
+   if (!_paq) {
+      return;
+   }
+   _paq.push([
+      'addEcommerceItem',
+      product.productSku,
+      product.productName,
+      product.categoryName,
+      product.price?.amount,
+      product.quantity,
+   ]);
+}
+
+function trackClearCart() {
+   if (!_paq) {
+      return;
+   }
+   _paq.push(['clearEcommerceCart']);
+}
+
+function trackCartUpdated(grandTotal: Money) {
+   if (!_paq) {
+      return;
+   }
+   _paq.push(['trackEcommerceCartUpdate', grandTotal.amount]);
 }
