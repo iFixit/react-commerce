feb2f1c5d2135dabbc433d372990c03fe3adad7b
Massimo De Marchi <massimo@inkofpixel.com>
2022-10-13 16:37:53 +0200

    feat(product page): Sync product page quantity with cart line item

--- packages/cart-sdk/index.tsx
+++ packages/cart-sdk/index.tsx
@@ -4,5 +4,6 @@ export { useAddToCart } from './hooks/use-add-to-cart';
 export type { AddToCartInput } from './hooks/use-add-to-cart';
 export { useRemoveLineItem } from './hooks/use-remove-line-item';
 export { useCheckout } from './hooks/use-checkout';
+export { useCartLineItem } from './hooks/use-cart-line-item';
 export type { Cart, CartLineItem } from './types';
 export { CartError } from './utils';
