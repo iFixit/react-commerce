66a06ea968ec751e946f573255a7741dc46ab0f4
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-10-27 16:05:00 -0700

    fix(cypress): drop unnecessary `wait()`'s
    Cypress considers overuse of `.wait()` an antipattern.
    https://docs.cypress.io/guides/references/best-practices#Unnecessary-Waiting
    
    It makes for slower tests, and it can be flaky if the wait wasn't long enough.
    
    The testing library we're using (the findBy.. functions) will keep retrying until the elements appear on the DOM, so let's just depend on that and drop the wait()s.
    https://testing-library.com/docs/cypress-testing-library/intro
    
    This also changes the 'cart-drawer-remove-item' click to be more idiomatic. What we had before didn't seem to click the right elements after dropping the wait().

--- frontend/test/cypress/integration/product/add_to_cart.cy.ts
+++ frontend/test/cypress/integration/product/add_to_cart.cy.ts
@@ -3,68 +3,61 @@ describe('product page add to cart', () => {
       cy.loadProductPageByPath(
          '/products/spudger-retail-3-pack?variantid=39419992408154'
       );
-      cy.findByTestId('cart-drawer-open').click().wait(10);
-      cy
-         .findAllByTestId('cart-drawer-item-count')
+      cy.findByTestId('cart-drawer-open').click();
+      cy.findAllByTestId('cart-drawer-item-count')
          .should('exist')
          .invoke('text')
          .should('eq', '0');
       cy.findByTestId('cart-drawer-quantity').should('not.exist');
-      cy.findByTestId('cart-drawer-close').click().wait(10);
+      cy.findByTestId('cart-drawer-close').click();
       cy.findByTestId('cart-drawer-close').should('not.exist');
    });
 
    it('Clicking Add To Cart Adds Items To Cart', () => {
       const genArr = Array.from({ length: 5 }, (v, k) => k + 1);
       cy.wrap(genArr).each((index) => {
-         cy.findByTestId('product-add-to-cart-button').click().wait(10);
-         cy
-            .findAllByTestId('cart-drawer-quantity')
+         cy.findByTestId('product-add-to-cart-button').click();
+         cy.findAllByTestId('cart-drawer-quantity')
             .invoke('text')
             .then(parseInt)
             .should('eq', index);
-         cy.findByTestId('cart-drawer-close').click().wait(10);
+         cy.findByTestId('cart-drawer-close').click();
       });
 
-      cy.findByTestId('cart-drawer-open').click().wait(10);
+      cy.findByTestId('cart-drawer-open').click();
       cy.removeItemsFromCartAndCloseDrawer();
    });
 
    it('Clicking + and - Buttons Changes Item Quantity in Cart', () => {
-      cy.findByTestId('product-add-to-cart-button').click().wait(10);
-      cy
-         .findAllByTestId('cart-drawer-item-count')
+      cy.findByTestId('product-add-to-cart-button').click();
+      cy.findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
       const genArr = Array.from({ length: 5 }, (v, k) => k + 2);
       cy.wrap(genArr).each((index) => {
-         cy.findByTestId('cart-drawer-increase-quantity').click().wait(10);
-         cy
-            .findAllByTestId('cart-drawer-quantity')
+         cy.findByTestId('cart-drawer-increase-quantity').click();
+         cy.findAllByTestId('cart-drawer-quantity')
             .invoke('text')
             .then(parseInt)
             .should('eq', index);
       });
-      cy.findByTestId('cart-drawer-close').click().wait(10);
+      cy.findByTestId('cart-drawer-close').click();
 
-      cy.findByTestId('cart-drawer-open').click().wait(10);
+      cy.findByTestId('cart-drawer-open').click();
       const genArrRev = Array.from({ length: 5 }, (v, k) => k + 1).reverse();
       cy.wrap(genArrRev).each((index) => {
-         cy.findByTestId('cart-drawer-decrease-quantity').click().wait(10);
-         cy
-            .findAllByTestId('cart-drawer-quantity')
+         cy.findByTestId('cart-drawer-decrease-quantity').click();
+         cy.findAllByTestId('cart-drawer-quantity')
             .invoke('text')
             .then(parseInt)
             .should('eq', index);
       });
-      cy
-         .findAllByTestId('cart-drawer-item-count')
+      cy.findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
-      cy
-         .findAllByTestId('cart-drawer-quantity')
+      cy.findAllByTestId('cart-drawer-quantity')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
@@ -72,27 +65,20 @@ describe('product page add to cart', () => {
    });
 
    it('Item Can Be Added Again After Removing The Item', () => {
-      cy.findByTestId('product-add-to-cart-button').click().wait(10);
-      cy
-         .findAllByTestId('cart-drawer-item-count')
+      cy.findByTestId('product-add-to-cart-button').click();
+      cy.findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
-      cy
-         .findAllByTestId('cart-drawer-remove-item')
-         .wait(10)
-         .then((items) => items.trigger('click'))
-         .wait(10);
-      cy
-         .findAllByTestId('cart-drawer-item-count')
+      cy.findAllByTestId('cart-drawer-remove-item').click();
+      cy.findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 0);
       cy.findAllByTestId('cart-drawer-quantity').should('not.exist');
-      cy.findByTestId('cart-drawer-close').click().wait(10);
-      cy.findByTestId('product-add-to-cart-button').click().wait(10);
-      cy
-         .findAllByTestId('cart-drawer-item-count')
+      cy.findByTestId('cart-drawer-close').click();
+      cy.findByTestId('product-add-to-cart-button').click();
+      cy.findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
@@ -100,35 +86,28 @@ describe('product page add to cart', () => {
    });
 
    it('Back to Shopping Button Works', () => {
-      cy.findByTestId('cart-drawer-open').click().wait(10);
-      cy
-         .findAllByTestId('cart-drawer-item-count')
+      cy.findByTestId('cart-drawer-open').click();
+      cy.findAllByTestId('cart-drawer-item-count')
          .should('exist')
          .invoke('text')
          .should('eq', '0');
       cy.findAllByTestId('cart-drawer-quantity').should('not.exist');
-      cy.findByTestId('back-to-shopping').click().wait(10);
+      cy.findByTestId('back-to-shopping').click();
       cy.findAllByTestId('cart-drawer-item-count').should('not.exist');
 
-      cy.findByTestId('product-add-to-cart-button').click().wait(10);
-      cy
-         .findAllByTestId('cart-drawer-item-count')
+      cy.findByTestId('product-add-to-cart-button').click();
+      cy.findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
       cy.findByTestId('back-to-shopping').should('not.be.visible');
-      cy
-         .findAllByTestId('cart-drawer-remove-item')
-         .wait(10)
-         .then((items) => items.trigger('click'))
-         .wait(10);
-      cy
-         .findAllByTestId('cart-drawer-item-count')
+      cy.findAllByTestId('cart-drawer-remove-item').click();
+      cy.findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 0);
       cy.findByTestId('cart-drawer-quantity').should('not.exist');
-      cy.findByTestId('back-to-shopping').click().wait(10);
+      cy.findByTestId('back-to-shopping').click();
       cy.findAllByTestId('cart-drawer-item-count').should('not.exist');
       cy.findByTestId('cart-drawer-close').should('not.exist');
    });
