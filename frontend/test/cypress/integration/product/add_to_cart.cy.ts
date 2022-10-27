420e25991bbcb56a45e3ccecbb24775082252ea6
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-10-27 14:16:49 -0700

    fix(cypress): don't reassign `cy`
    ESLint won't give us helpful warnings if we reassign the `cy` variable.

--- frontend/test/cypress/integration/product/add_to_cart.cy.ts
+++ frontend/test/cypress/integration/product/add_to_cart.cy.ts
@@ -1,138 +1,136 @@
 describe('product page add to cart', () => {
-   const user = cy;
-
    beforeEach(() => {
-      user.loadProductPageByPath(
+      cy.loadProductPageByPath(
          '/products/spudger-retail-3-pack?variantid=39419992408154'
       );
-      user.findByTestId('cart-drawer-open').click().wait(10);
-      user
+      cy.findByTestId('cart-drawer-open').click().wait(10);
+      cy
          .findAllByTestId('cart-drawer-item-count')
          .should('exist')
          .invoke('text')
          .should('eq', '0');
-      user.findByTestId('cart-drawer-quantity').should('not.exist');
-      user.findByTestId('cart-drawer-close').click().wait(10);
-      user.findByTestId('cart-drawer-close').should('not.exist');
+      cy.findByTestId('cart-drawer-quantity').should('not.exist');
+      cy.findByTestId('cart-drawer-close').click().wait(10);
+      cy.findByTestId('cart-drawer-close').should('not.exist');
    });
 
    it('Clicking Add To Cart Adds Items To Cart', () => {
       var genArr = Array.from({ length: 5 }, (v, k) => k + 1);
-      user.wrap(genArr).each((index) => {
-         user.findByTestId('product-add-to-cart-button').click().wait(10);
-         user
+      cy.wrap(genArr).each((index) => {
+         cy.findByTestId('product-add-to-cart-button').click().wait(10);
+         cy
             .findAllByTestId('cart-drawer-quantity')
             .invoke('text')
             .then(parseInt)
             .should('eq', index);
-         user.findByTestId('cart-drawer-close').click().wait(10);
+         cy.findByTestId('cart-drawer-close').click().wait(10);
       });
 
-      user.findByTestId('cart-drawer-open').click().wait(10);
-      user.removeItemsFromCartAndCloseDrawer();
+      cy.findByTestId('cart-drawer-open').click().wait(10);
+      cy.removeItemsFromCartAndCloseDrawer();
    });
 
    it('Clicking + and - Buttons Changes Item Quantity in Cart', () => {
-      user.findByTestId('product-add-to-cart-button').click().wait(10);
-      user
+      cy.findByTestId('product-add-to-cart-button').click().wait(10);
+      cy
          .findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
       const genArr = Array.from({ length: 5 }, (v, k) => k + 2);
-      user.wrap(genArr).each((index) => {
-         user.findByTestId('cart-drawer-increase-quantity').click().wait(10);
-         user
+      cy.wrap(genArr).each((index) => {
+         cy.findByTestId('cart-drawer-increase-quantity').click().wait(10);
+         cy
             .findAllByTestId('cart-drawer-quantity')
             .invoke('text')
             .then(parseInt)
             .should('eq', index);
       });
-      user.findByTestId('cart-drawer-close').click().wait(10);
+      cy.findByTestId('cart-drawer-close').click().wait(10);
 
-      user.findByTestId('cart-drawer-open').click().wait(10);
+      cy.findByTestId('cart-drawer-open').click().wait(10);
       const genArrRev = Array.from({ length: 5 }, (v, k) => k + 1).reverse();
-      user.wrap(genArrRev).each((index) => {
-         user.findByTestId('cart-drawer-decrease-quantity').click().wait(10);
-         user
+      cy.wrap(genArrRev).each((index) => {
+         cy.findByTestId('cart-drawer-decrease-quantity').click().wait(10);
+         cy
             .findAllByTestId('cart-drawer-quantity')
             .invoke('text')
             .then(parseInt)
             .should('eq', index);
       });
-      user
+      cy
          .findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
-      user
+      cy
          .findAllByTestId('cart-drawer-quantity')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
-      user.removeItemsFromCartAndCloseDrawer();
+      cy.removeItemsFromCartAndCloseDrawer();
    });
 
    it('Item Can Be Added Again After Removing The Item', () => {
-      user.findByTestId('product-add-to-cart-button').click().wait(10);
-      user
+      cy.findByTestId('product-add-to-cart-button').click().wait(10);
+      cy
          .findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
-      user
+      cy
          .findAllByTestId('cart-drawer-remove-item')
          .wait(10)
          .then((items) => items.trigger('click'))
          .wait(10);
-      user
+      cy
          .findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 0);
-      user.findAllByTestId('cart-drawer-quantity').should('not.exist');
-      user.findByTestId('cart-drawer-close').click().wait(10);
-      user.findByTestId('product-add-to-cart-button').click().wait(10);
-      user
+      cy.findAllByTestId('cart-drawer-quantity').should('not.exist');
+      cy.findByTestId('cart-drawer-close').click().wait(10);
+      cy.findByTestId('product-add-to-cart-button').click().wait(10);
+      cy
          .findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
-      user.removeItemsFromCartAndCloseDrawer();
+      cy.removeItemsFromCartAndCloseDrawer();
    });
 
    it('Back to Shopping Button Works', () => {
-      user.findByTestId('cart-drawer-open').click().wait(10);
-      user
+      cy.findByTestId('cart-drawer-open').click().wait(10);
+      cy
          .findAllByTestId('cart-drawer-item-count')
          .should('exist')
          .invoke('text')
          .should('eq', '0');
-      user.findAllByTestId('cart-drawer-quantity').should('not.exist');
-      user.findByTestId('back-to-shopping').click().wait(10);
-      user.findAllByTestId('cart-drawer-item-count').should('not.exist');
+      cy.findAllByTestId('cart-drawer-quantity').should('not.exist');
+      cy.findByTestId('back-to-shopping').click().wait(10);
+      cy.findAllByTestId('cart-drawer-item-count').should('not.exist');
 
-      user.findByTestId('product-add-to-cart-button').click().wait(10);
-      user
+      cy.findByTestId('product-add-to-cart-button').click().wait(10);
+      cy
          .findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 1);
-      user.findByTestId('back-to-shopping').should('not.be.visible');
-      user
+      cy.findByTestId('back-to-shopping').should('not.be.visible');
+      cy
          .findAllByTestId('cart-drawer-remove-item')
          .wait(10)
          .then((items) => items.trigger('click'))
          .wait(10);
-      user
+      cy
          .findAllByTestId('cart-drawer-item-count')
          .invoke('text')
          .then(parseInt)
          .should('eq', 0);
-      user.findByTestId('cart-drawer-quantity').should('not.exist');
-      user.findByTestId('back-to-shopping').click().wait(10);
-      user.findAllByTestId('cart-drawer-item-count').should('not.exist');
-      user.findByTestId('cart-drawer-close').should('not.exist');
+      cy.findByTestId('cart-drawer-quantity').should('not.exist');
+      cy.findByTestId('back-to-shopping').click().wait(10);
+      cy.findAllByTestId('cart-drawer-item-count').should('not.exist');
+      cy.findByTestId('cart-drawer-close').should('not.exist');
    });
 });
 
