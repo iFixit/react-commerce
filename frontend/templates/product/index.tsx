1505685d3addbf639f86b451f8a504b7d5387f26
danielcliu-ifixit <daniell@ifixit.com>
2022-11-01 22:24:46 -0700

    Re-hit the api if the iFixitProductId changes.

--- frontend/templates/product/index.tsx
+++ frontend/templates/product/index.tsx
@@ -54,7 +54,7 @@ export const ProductTemplate: NextPageWithLayout<ProductTemplateProps> = () => {
          .catch(() => {
             setReviewsData(null);
          });
-   }, []);
+   }, [product.iFixitProductId]);
 
    const isProductForSale = useIsProductForSale(product);
 
