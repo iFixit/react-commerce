51d210db4adb252a0bd9c87c6d95394a5a543aab
Federico Badini <federico@inkofpixel.com>
2022-11-16 16:51:51 +0100

    fix: style

--- frontend/templates/product/sections/ProductSection/ProductGallery.tsx
+++ frontend/templates/product/sections/ProductSection/ProductGallery.tsx
@@ -340,6 +340,8 @@ function ImageWithZoom({ index, image, enableZoom }: ImageProps) {
                loading={index === 0 ? 'eager' : 'lazy'}
                decoding={index === 0 ? 'sync' : 'async'}
                preload={index === 0}
+               width="100%"
+               height="100%"
                {...eventHandlers}
             />
          </Flex>
