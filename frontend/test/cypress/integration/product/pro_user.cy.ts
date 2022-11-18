5d3fd0af93406ec51159c34a033b10c77cd39b8d
batbattur <bat@ifixit.com>
2022-11-18 15:41:17 -0800

    Create separate test file for product page title price and sku

--- frontend/test/cypress/integration/product/pro_user.cy.ts
+++ frontend/test/cypress/integration/product/pro_user.cy.ts
@@ -2,31 +2,11 @@ describe('Pro user test', () => {
    it('will give pro users a discount', () => {
       cy.loadProductPageByPath('/products/repair-business-toolkit');
 
-      // Assert product title is visible
-      cy.findByTestId('product-title')
-         .should('be.visible')
-         .invoke('text')
-         .then((productTitle) => {
-            expect(productTitle).to.equal('Repair Business Toolkit');
-         });
-
-      // Assert product sku is visible
-      cy.findByTestId('product-sku')
-         .should('be.visible')
-         .invoke('text')
-         .then((productSku) => {
-            expect(productSku).to.match(/IF\d*\-\d*/g);
-         });
-
       // Get price from page
       cy.findAllByTestId('product-price')
          .first()
-         .should('be.visible')
          .invoke('text')
          .then((originalPriceString) => {
-            // regex to match $299.99 with dollar sign
-            expect(originalPriceString).to.match(/\$\d*\.\d*/g);
-
             // Login as pro
             cy.interceptLogin({ discount_tier: 'pro_4' });
             cy.reload();
