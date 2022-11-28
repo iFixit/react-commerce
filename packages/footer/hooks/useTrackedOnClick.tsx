db87186e06eb9ceb3431d7e9e8dfb59a99372f6e
Alex Burke <aburke07@calpoly.edu>
2022-11-28 09:24:13 -0800

    Add name to tracking function
    1. We want to be able to report what was clicked on, and not just the url.
    2. This adds an optional param for us to prefer over the href and changes the
        formatting of the eventName to reflect the spec.

--- packages/footer/hooks/useTrackedOnClick.tsx
+++ packages/footer/hooks/useTrackedOnClick.tsx
@@ -4,18 +4,20 @@ import { TrackingContext } from './TrackingContext';
 type UseFooterLinkTrackingProps<T> = {
    href?: string;
    onClick?: React.MouseEventHandler<T>;
+   clickName?: string;
 };
 
 export function useTrackedOnClick<T>({
    href,
    onClick,
+   clickName,
 }: UseFooterLinkTrackingProps<T>) {
    const { trackClick } = React.useContext(TrackingContext);
    const trackedOnClick: React.MouseEventHandler<T> = React.useCallback(
       (e) => {
          trackClick({
             eventCategory: 'Footer',
-            eventAction: `Link Click - ${href}`,
+            eventAction: `Footer Link - ${clickName || href || 'undefined'} - Click`,
          });
          onClick?.(e);
       },
