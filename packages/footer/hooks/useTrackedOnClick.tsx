c9551246b8b093b926eb20f4fc04dc5d373657a8
Alex Burke <aburke07@calpoly.edu>
2022-11-28 11:11:59 -0800

    Add Link type to tracking action
    1. Updated the event action to more closely match the spec.
    2. This includes differentiating the routing links from the link to change the store

--- packages/footer/hooks/useTrackedOnClick.tsx
+++ packages/footer/hooks/useTrackedOnClick.tsx
@@ -5,19 +5,22 @@ type UseFooterLinkTrackingProps<T> = {
    href?: string;
    onClick?: React.MouseEventHandler<T>;
    clickName?: string;
+   isStoreLink?: boolean;
 };
 
 export function useTrackedOnClick<T>({
    href,
    onClick,
    clickName,
+   isStoreLink,
 }: UseFooterLinkTrackingProps<T>) {
+   const LinkType = isStoreLink ? 'Stores' : 'iFixit';
    const { trackClick } = React.useContext(TrackingContext);
    const trackedOnClick: React.MouseEventHandler<T> = React.useCallback(
       (e) => {
          trackClick({
             eventCategory: 'Footer',
-            eventAction: `Footer Link - ${clickName || href || 'undefined'} - Click`,
+            eventAction: `Footer ${LinkType} Link - \"${clickName || href || 'undefined'}\" - Click`,
          });
          onClick?.(e);
       },
