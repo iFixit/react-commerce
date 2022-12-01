53a4f2413529db7762b3d13bf4b2d891b08bfbce
Daniel Beardsley <daniel@ifixit.com>
2022-12-01 13:08:29 -0800

    Review Stars: defer rendering to client-side
    Stars are individual svg elements and the "featured products"
    section has quite a number of them. Deferring this below-the-fold
    content to client-side rendering saves ~5-6kb network KB and ~60KB of
    html parsing.

--- frontend/components/common/ProductRating.tsx
+++ frontend/components/common/ProductRating.tsx
@@ -1,5 +1,6 @@
 import { HStack, StackProps, Text } from '@chakra-ui/react';
 import { Rating } from '@components/ui';
+import { useIsClientSide } from '@ifixit/ui/hooks';
 
 export type ProductRatingProps = StackProps & {
    rating: number;
@@ -11,13 +12,18 @@ export const ProductRating = ({
    count,
    ...stackProps
 }: ProductRatingProps) => {
+   const isClientSide = useIsClientSide();
    if (rating < 4 && count <= 10) {
       return null;
    }
    return (
       <HStack {...stackProps} align="flex-end">
-         <Rating value={rating} />
-         <Text lineHeight="1em">{count}</Text>
+         {isClientSide && (
+            <>
+               <Rating value={rating} />
+               <Text lineHeight="1em">{count}</Text>
+            </>
+         )}
       </HStack>
    );
 };
