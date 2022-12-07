372a4ec95ae500f532e1e2ba73d6a22bdcef21f0
danielcliu-ifixit <daniell@ifixit.com>
2022-12-06 16:00:43 -0800

    Run prettier

--- frontend/templates/product/sections/CrossSellSection/index.tsx
+++ frontend/templates/product/sections/CrossSellSection/index.tsx
@@ -438,11 +438,13 @@ function CrossSellItem({
                         >
                            <FaIcon
                               icon={faCircleCheck}
-                              color={isSelected ? 'brand.500' : 'gray.300'}                        
+                              color={isSelected ? 'brand.500' : 'gray.300'}
                               h="6"
                               transition="color 300ms"
-                              _hover={ isSelected ? {color: 'gray.300'} :
-                                 {color: 'brand.500'}
+                              _hover={
+                                 isSelected
+                                    ? { color: 'gray.300' }
+                                    : { color: 'brand.500' }
                               }
                            />
                         </Center>
