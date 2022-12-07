1f469a6af9661d70ebcd3450d26f17f55b7f7153
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-07 11:51:48 -0800

    jest: ignore playwright tests
    Ensure that the playwright tests are not run by jest.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -20,7 +20,7 @@ const customJestConfig = {
       '@public(.+)': '<rootDir>/public/$1',
    },
    setupFilesAfterEnv: ['<rootDir>/tests/jest/jest-setup.ts'],
-   testPathIgnorePatterns: ['<rootDir>/tests/cypress'],
+   testPathIgnorePatterns: ['<rootDir>/tests/(cypress|playwright)'],
    testEnvironment: 'jsdom',
    transform: {
       '.+\\.(css|style|less|sass|scss)$': 'jest-css-modules-transform',
