ff0c137085be84b399808c3faa854d86aa7109f7
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-07 16:07:44 -0800

    create skeleton test for ProductSection

---
+++ frontend/test/jest/tests/ProductSection.test.tsx
@@ -0,0 +1,5 @@
+import { screen } from '@testing-library/react';
+import { renderWithAppContext } from '../utils';
+import { ProductSection } from '@templates/product/sections/ProductSection/index';
+
+describe('ProductSection Tests', () => {});
