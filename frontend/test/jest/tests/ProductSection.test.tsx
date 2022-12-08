6530f74b675fd7ef41cc70491836404407aef0e3
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-07 17:46:14 -0800

    write intial test for product section description
    This adds additional setup steps to be able test Product Section and
    tests that the description is rendered. We need to mock a custom
    react hook to be able to test the component and mock the matchMedia
    object before we can run the tests.

--- frontend/test/jest/tests/ProductSection.test.tsx
+++ frontend/test/jest/tests/ProductSection.test.tsx
@@ -1,5 +1,32 @@
 import { screen } from '@testing-library/react';
-import { renderWithAppContext } from '../utils';
+import { mockMatchMedia, renderWithAppContext } from '../utils';
 import { ProductSection } from '@templates/product/sections/ProductSection/index';
+import { mockedProduct, mockedProductVariant } from '../__mocks__/products';
 
-describe('ProductSection Tests', () => {});
+jest.mock('@templates/product/hooks/useIsProductForSale');
+
+describe('ProductSection Tests', () => {
+   beforeAll(() => {
+      mockMatchMedia();
+   });
+
+   test('renders product description', async () => {
+      // @ts-ignore
+      renderWithAppContext(
+         <ProductSection
+            product={mockedProduct}
+            selectedVariant={{
+               ...mockedProductVariant,
+               description: 'Mocked Product Description',
+            }}
+            onVariantChange={jest.fn()}
+            internationalBuyBox={null}
+         />
+      );
+
+      const description = await screen.findByText(
+         /mocked product description/i
+      );
+      (expect(description) as any).toBeInTheDocument();
+   });
+});
