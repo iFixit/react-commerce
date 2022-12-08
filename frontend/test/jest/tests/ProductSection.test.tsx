29e17f1efd9d54a46c4f186e06c69196078e6fef
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-07 17:53:40 -0800

    test rest of the production description section
    Adding tests to confirm that the rest of the production description
    section is rendering as expected. In order to make it easier to target
    these subsections of the description section, I've added a data-testid
    attributes to the subsections.

--- frontend/test/jest/tests/ProductSection.test.tsx
+++ frontend/test/jest/tests/ProductSection.test.tsx
@@ -29,4 +29,87 @@ describe('ProductSection Tests', () => {
       );
       (expect(description) as any).toBeInTheDocument();
    });
+
+   test('note renders', async () => {
+      const productVariant = { ...mockedProductVariant, note: 'Mocked Note' };
+
+      renderWithAppContext(
+         <ProductSection
+            product={mockedProduct}
+            selectedVariant={productVariant}
+            onVariantChange={jest.fn()}
+            internationalBuyBox={null}
+         />
+      );
+
+      const note = await screen.findByTestId('product-note');
+      (expect(note) as any).toBeInTheDocument();
+      (expect(note.textContent) as any).toContain('Mocked Note');
+   });
+
+   test('disclaimer renders', async () => {
+      const productVariant = {
+         ...mockedProductVariant,
+         disclaimer: 'Mocked Disclaimer',
+      };
+
+      renderWithAppContext(
+         <ProductSection
+            product={mockedProduct}
+            selectedVariant={productVariant}
+            onVariantChange={jest.fn()}
+            internationalBuyBox={null}
+         />
+      );
+
+      const disclaimer = await screen.findByTestId('product-disclaimer');
+      (expect(disclaimer) as any).toBeInTheDocument();
+      (expect(disclaimer.textContent) as any).toContain('Mocked Disclaimer');
+   });
+
+   test('warning renders', async () => {
+      const productVariant = {
+         ...mockedProductVariant,
+         warning: 'Mocked Warning',
+      };
+
+      renderWithAppContext(
+         <ProductSection
+            product={mockedProduct}
+            selectedVariant={productVariant}
+            onVariantChange={jest.fn()}
+            internationalBuyBox={null}
+         />
+      );
+
+      const warning = await screen.findByTestId('product-warning');
+      (expect(warning) as any).toBeInTheDocument();
+      (expect(warning.textContent) as any).toContain('Mocked Warning');
+   });
+
+   test('descriptors do not render', async () => {
+      const productVariant = {
+         ...mockedProductVariant,
+         note: '',
+         warning: '',
+         disclaimer: '',
+      };
+
+      renderWithAppContext(
+         <ProductSection
+            product={mockedProduct}
+            selectedVariant={productVariant}
+            onVariantChange={jest.fn()}
+            internationalBuyBox={null}
+         />
+      );
+
+      const warning = screen.queryByTestId('product-warning');
+      const note = screen.queryByTestId('product-note');
+      const disclaimer = screen.queryByTestId('product-disclaimer');
+
+      (expect(warning) as any).not.toBeInTheDocument();
+      (expect(note) as any).not.toBeInTheDocument();
+      (expect(disclaimer) as any).not.toBeInTheDocument();
+   });
 });
