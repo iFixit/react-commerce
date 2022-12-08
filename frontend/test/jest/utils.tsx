035640b1685eb0c2a63b0a0e1d38251c461c48fa
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-08 09:17:09 -0800

    create utility functions to mock product data for jest
    This mimics the API we have in our ProductExtensions class within
    the iFixit/ifixit repo. This will allow us to easily get a Product
    like object for testing purposes and give us the ability to override
    any of the properties we want to test. In addition, since we are using
    the `Partial` type, we can make the properties optional but still
    retain the type safety when trying to override default values.

--- frontend/test/jest/utils.tsx
+++ frontend/test/jest/utils.tsx
@@ -1,5 +1,12 @@
 import { render, RenderOptions, RenderResult } from '@testing-library/react';
 import { AppProviders } from '@components/common';
+import { Product, ProductVariant } from '@models/product';
+import {
+   mockedProduct,
+   mockedProductSearchHit,
+   mockedProductVariant,
+} from './__mocks__/products';
+import { ProductSearchHit } from '@models/product-list';
 
 /* This is needed if there is some code that uses a method which is not available in the jsdom environment yet
  * https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
@@ -32,3 +39,31 @@ export const renderWithAppContext = (
    ui: any,
    options?: RenderOptions
 ): RenderResult => render(ui, { wrapper: GlobalContextProvider, ...options });
+
+/*
+ * Utility functions for mocking product data
+ */
+export const getMockProduct = (overrides: Partial<Product> = {}): Product => {
+   return {
+      ...mockedProduct,
+      ...overrides,
+   };
+};
+
+export const getMockProductVariant = (
+   overrides: Partial<ProductVariant> = {}
+): ProductVariant => {
+   return {
+      ...mockedProductVariant,
+      ...overrides,
+   };
+};
+
+export const getMockProductSearchHit = (
+   overrides: Partial<ProductSearchHit> = {}
+): ProductSearchHit => {
+   return {
+      ...mockedProductSearchHit,
+      ...overrides,
+   };
+};
