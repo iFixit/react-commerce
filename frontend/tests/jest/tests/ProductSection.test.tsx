b0b60d9b7cb03069029ef7c6158b930a9bc0d953
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-09 13:34:28 -0800

    move product section description tests into sub describe block
    We will be adding more product section tests, so we should move the
    description tests into a sub describe block to keep things organized.

--- frontend/tests/jest/tests/ProductSection.test.tsx
+++ frontend/tests/jest/tests/ProductSection.test.tsx
@@ -14,102 +14,104 @@ describe('ProductSection Tests', () => {
       mockMatchMedia();
    });
 
-   test('renders product description', async () => {
-      // @ts-ignore
-      renderWithAppContext(
-         <ProductSection
-            product={getMockProduct()}
-            selectedVariant={getMockProductVariant({
-               description: 'Mocked Product Description',
-            })}
-            onVariantChange={jest.fn()}
-            internationalBuyBox={null}
-         />
-      );
+   describe('Product Description Tests', () => {
+      test('renders product description', async () => {
+         // @ts-ignore
+         renderWithAppContext(
+            <ProductSection
+               product={getMockProduct()}
+               selectedVariant={getMockProductVariant({
+                  description: 'Mocked Product Description',
+               })}
+               onVariantChange={jest.fn()}
+               internationalBuyBox={null}
+            />
+         );
 
-      const description = await screen.findByText(
-         /mocked product description/i
-      );
-      (expect(description) as any).toBeInTheDocument();
-   });
-
-   test('note renders', async () => {
-      const productVariant = getMockProductVariant({ note: 'Mocked Note' });
-
-      renderWithAppContext(
-         <ProductSection
-            product={getMockProduct()}
-            selectedVariant={productVariant}
-            onVariantChange={jest.fn()}
-            internationalBuyBox={null}
-         />
-      );
-
-      const note = await screen.findByTestId('product-note');
-      (expect(note) as any).toBeInTheDocument();
-      (expect(note.textContent) as any).toContain('Mocked Note');
-   });
-
-   test('disclaimer renders', async () => {
-      const productVariant = getMockProductVariant({
-         disclaimer: 'Mocked Disclaimer',
+         const description = await screen.findByText(
+            /mocked product description/i
+         );
+         (expect(description) as any).toBeInTheDocument();
       });
 
-      renderWithAppContext(
-         <ProductSection
-            product={getMockProduct()}
-            selectedVariant={productVariant}
-            onVariantChange={jest.fn()}
-            internationalBuyBox={null}
-         />
-      );
+      test('note renders', async () => {
+         const productVariant = getMockProductVariant({ note: 'Mocked Note' });
 
-      const disclaimer = await screen.findByTestId('product-disclaimer');
-      (expect(disclaimer) as any).toBeInTheDocument();
-      (expect(disclaimer.textContent) as any).toContain('Mocked Disclaimer');
-   });
+         renderWithAppContext(
+            <ProductSection
+               product={getMockProduct()}
+               selectedVariant={productVariant}
+               onVariantChange={jest.fn()}
+               internationalBuyBox={null}
+            />
+         );
 
-   test('warning renders', async () => {
-      const productVariant = getMockProductVariant({
-         warning: 'Mocked Warning',
+         const note = await screen.findByTestId('product-note');
+         (expect(note) as any).toBeInTheDocument();
+         (expect(note.textContent) as any).toContain('Mocked Note');
       });
 
-      renderWithAppContext(
-         <ProductSection
-            product={getMockProduct()}
-            selectedVariant={productVariant}
-            onVariantChange={jest.fn()}
-            internationalBuyBox={null}
-         />
-      );
+      test('disclaimer renders', async () => {
+         const productVariant = getMockProductVariant({
+            disclaimer: 'Mocked Disclaimer',
+         });
 
-      const warning = await screen.findByTestId('product-warning');
-      (expect(warning) as any).toBeInTheDocument();
-      (expect(warning.textContent) as any).toContain('Mocked Warning');
-   });
+         renderWithAppContext(
+            <ProductSection
+               product={getMockProduct()}
+               selectedVariant={productVariant}
+               onVariantChange={jest.fn()}
+               internationalBuyBox={null}
+            />
+         );
 
-   test('descriptors do not render', async () => {
-      const productVariant = getMockProductVariant({
-         note: '',
-         warning: '',
-         disclaimer: '',
+         const disclaimer = await screen.findByTestId('product-disclaimer');
+         (expect(disclaimer) as any).toBeInTheDocument();
+         (expect(disclaimer.textContent) as any).toContain('Mocked Disclaimer');
       });
 
-      renderWithAppContext(
-         <ProductSection
-            product={getMockProduct()}
-            selectedVariant={productVariant}
-            onVariantChange={jest.fn()}
-            internationalBuyBox={null}
-         />
-      );
+      test('warning renders', async () => {
+         const productVariant = getMockProductVariant({
+            warning: 'Mocked Warning',
+         });
 
-      const warning = screen.queryByTestId('product-warning');
-      const note = screen.queryByTestId('product-note');
-      const disclaimer = screen.queryByTestId('product-disclaimer');
+         renderWithAppContext(
+            <ProductSection
+               product={getMockProduct()}
+               selectedVariant={productVariant}
+               onVariantChange={jest.fn()}
+               internationalBuyBox={null}
+            />
+         );
 
-      (expect(warning) as any).not.toBeInTheDocument();
-      (expect(note) as any).not.toBeInTheDocument();
-      (expect(disclaimer) as any).not.toBeInTheDocument();
+         const warning = await screen.findByTestId('product-warning');
+         (expect(warning) as any).toBeInTheDocument();
+         (expect(warning.textContent) as any).toContain('Mocked Warning');
+      });
+
+      test('descriptors do not render', async () => {
+         const productVariant = getMockProductVariant({
+            note: '',
+            warning: '',
+            disclaimer: '',
+         });
+
+         renderWithAppContext(
+            <ProductSection
+               product={getMockProduct()}
+               selectedVariant={productVariant}
+               onVariantChange={jest.fn()}
+               internationalBuyBox={null}
+            />
+         );
+
+         const warning = screen.queryByTestId('product-warning');
+         const note = screen.queryByTestId('product-note');
+         const disclaimer = screen.queryByTestId('product-disclaimer');
+
+         (expect(warning) as any).not.toBeInTheDocument();
+         (expect(note) as any).not.toBeInTheDocument();
+         (expect(disclaimer) as any).not.toBeInTheDocument();
+      });
    });
 });
