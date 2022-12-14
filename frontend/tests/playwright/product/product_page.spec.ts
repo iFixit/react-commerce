c835deb0038149de1097ca00372716865a5b0bcf
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-14 12:27:33 -0600

    port product_page to playwright

--- frontend/tests/playwright/product/product_page.spec.ts
+++ frontend/tests/playwright/product/product_page.spec.ts
@@ -1,32 +1,23 @@
-describe('Product page test', () => {
-   it('verify product title, price and sku are visible', () => {
-      cy.loadProductPageByPath('/products/repair-business-toolkit');
+import { test, expect } from '@playwright/test';
+
+test.describe('Product page test', () => {
+   test('verify product title, price and sku are visible', async ({ page }) => {
+      await page.goto('/products/repair-business-toolkit');
 
       // Assert product title is visible
-      cy.findByTestId('product-title')
-         .should('be.visible')
-         .invoke('text')
-         .then((productTitle) => {
-            expect(productTitle).to.equal('Repair Business Toolkit');
-         });
+      await expect(page.getByTestId('product-title')).toBeVisible();
+      await expect(page.getByTestId('product-title')).toHaveText(
+         'Repair Business Toolkit'
+      );
 
       // Assert product sku is visible
-      cy.findByTestId('product-sku')
-         .should('be.visible')
-         .invoke('text')
-         .then((productSku) => {
-            expect(productSku).to.match(/IF\d*\-\d*/g);
-         });
+      await expect(page.getByTestId('product-sku')).toBeVisible();
+      await expect(page.getByTestId('product-sku')).toHaveText(/IF\d*\-\d*/g);
 
       // Get price from page
-      cy.findAllByTestId('product-price')
-         .first()
-         .should('be.visible')
-         .invoke('text')
-         .then((originalPriceString) => {
-            // regex to match $299.99 with dollar sign
-            expect(originalPriceString).to.match(/\$\d*\.\d*/g);
-         });
+      await expect(page.getByTestId('product-price').first()).toBeVisible();
+      await expect(page.getByTestId('product-price').first()).toHaveText(
+         /\$\d*\.\d*/g
+      );
    });
 });
-export {};
