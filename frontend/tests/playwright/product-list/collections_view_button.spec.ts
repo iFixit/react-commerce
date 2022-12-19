0773708e58a4f2f9dc02c8941128a9090573ccf4
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-19 13:16:41 -0600

    port collections_view_button test to playwright

--- frontend/tests/playwright/product-list/collections_view_button.spec.ts
+++ frontend/tests/playwright/product-list/collections_view_button.spec.ts
@@ -1,30 +1,28 @@
-describe('collection display', () => {
-   beforeEach(() => {
-      cy.loadCollectionPageByPath('/Tools');
+import { test, expect } from '@playwright/test';
+
+test.describe('collection display', () => {
+   test.beforeEach(async ({ page }) => {
+      await page.goto('/Tools');
    });
 
-   it('should display grid view when selected', () => {
-      cy.findByTestId('grid-view-button').click();
-      cy.findByTestId('list-view-products').should('not.exist');
+   test('Should display grid view when selected', async ({ page }) => {
+      await page.getByTestId('grid-view-button').click();
+      await expect(page.getByTestId('list-view-products')).not.toBeVisible();
 
       // Make sure the display property equals to grid
-      cy.findByTestId('grid-view-products')
-         .invoke('css', 'display')
-         .should('equal', 'grid');
+      const grid = page.getByTestId('grid-view-products');
+      await expect(grid).toHaveCSS('display', 'grid');
    });
 
-   it('should display list view when selected', () => {
+   test('Should display list view when selected', async ({ page }) => {
       // First click on grid view button
-      cy.findByTestId('grid-view-button').click();
+      await page.getByTestId('grid-view-button').click();
       // Then switch back to list view button
-      cy.findByTestId('list-view-button').click();
-      cy.findByTestId('grid-view-products').should('not.exist');
+      await page.getByTestId('list-view-button').click();
+      await expect(page.getByTestId('grid-view-products')).not.toBeVisible();
 
       // Make sure the display property equals to flex
-      cy.findByTestId('list-view-products')
-         .invoke('css', 'display')
-         .should('equal', 'flex');
+      const list = page.getByTestId('list-view-products');
+      await expect(list).toHaveCSS('display', 'flex');
    });
 });
-
-export {};
