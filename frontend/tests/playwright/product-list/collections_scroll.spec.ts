fd6b3237fbae011d7ea95782cc03f3bd2ddbce8a
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-19 13:06:45 -0600

    port collections_scroll to playwright
    In order to port this test I needed to extend the expect
    functionality to support Cypress's `isWithinViewport` assertion.
    Playwright's `isVisible` assertion simply checks if the element is
    is in the DOM and not hidden. In order to check if an element is
    within the viewport, I needed to use the `boundingBox` property
    of the element. This returns the element's position and size
    relative to the viewport. Therefore, using the page viewport size
    we can check if the element is within the viewport.
    
    I also needed to add an additional wait before validating the
    element is within the viewport. This is because we are still
    in the process of scrolling to the top of the page when we
    are validating the element is within the viewport. This will
    wait until all network requests have finished before validating
    the element is within the viewport.

--- frontend/tests/playwright/product-list/collections_scroll.spec.ts
+++ frontend/tests/playwright/product-list/collections_scroll.spec.ts
@@ -1,23 +1,30 @@
+import { test, expect } from '@playwright/test';
 // Assert that when a user clicks the pagination buttons,
 // the page scrolls back up.
 
-describe('collections scroll', () => {
-   beforeEach(() => {
-      cy.loadCollectionPageByPath('/Tools');
+test.describe('collections scroll', () => {
+   test.beforeEach(async ({ page }) => {
+      await page.goto('/Tools');
    });
 
-   it('should scroll to the top of the page after clicking next page', () => {
-      cy.findByTestId('collections-search-box').should('be.visible');
-      cy.findByTestId('next-page').click();
+   test('Should scroll to the top of the page after clicking next page', async ({
+      page,
+   }) => {
+      await (
+         expect(page.getByTestId('collections-search-box')) as any
+      ).toBeVisible();
+
+      await page.getByTestId('next-page').click();
+
+      // Wait for the page to scroll to the top
+      await page.waitForLoadState('networkidle');
 
       // When it scrolls to the top, the search bar should be visible
-      cy.findByTestId('collections-search-box').isWithinViewport();
+      await (
+         expect(page.getByTestId('collections-search-box')) as any
+      ).toBeWithinViewport(page.viewportSize());
 
       // Check that url parameter contains ?p after clicking next page
-      cy.location().should((loc) => {
-         expect(loc.search).to.have.string('?p=');
-      });
+      await (expect(page.url()) as any).toContain('?p=');
    });
 });
-
-export {};
