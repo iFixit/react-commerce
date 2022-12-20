7a6f23ec5cdc5406b81cb7e9d6703645e662b20a
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-20 13:52:52 -0600

    port filters test to playwright
    This test was a little tricky to port because Playwright's locators
    do not bind the element of the pages. This means everytime we
    want to interact with a Locator, it will requery the page with the
    specified selector. If the element has changed such as when we expand
    a collapsed facet, then the Locator will not be able to find it anymore
    when we try to continue interacting with it. To solve this, we need to
    use the actual ElementHandle that the Locator returns and use that to
    interact with the elements.
    
    Additionally, in Cypress we have a custom command to append a
    `filteredProducts` property to the window object in
    https://github.com/iFixit/react-commerce/blob/main/frontend/templates/product-list/sections/FilterableProductsSection/index.tsx#L56.
    This property is later used to validate that products we got back
    from the `search` query include the same `facet tag` as the one we
    selected in the filter. In order to test this, we need to return
    the response from the `search` query and then use that to validate the
    products include the correct `facet tag` as the one we selected. There
    was an additional problem with this as Playwright does not have an
    `its` command like Cypress does. To resolve this, I added a utility
    function that wraps around `lodash.get()` to get the nested property
    value of an object with a string path.

--- frontend/tests/playwright/product-list/filters.spec.ts
+++ frontend/tests/playwright/product-list/filters.spec.ts
@@ -1,114 +1,117 @@
-describe('product list filters', () => {
-   beforeEach(() => {
-      cy.loadCollectionPageByPath('/Parts');
+import { test, expect } from '@playwright/test';
+import { resolvePath } from '../utils';
+
+test.describe('product list filters', () => {
+   test.beforeEach(async ({ page }) => {
+      await page.goto('/Parts');
    });
 
-   it('should help user filter', () => {
-      cy.findAllByTestId(/collapsed-facet-accordion-item-.*/i)
-         .first()
-         .as('first-facet-accordion-item')
-         .next()
-         .as('second-facet-accordion-item');
+   test('Should help user filter', async ({ page }) => {
+      const facetList = page
+         .getByTestId('facets-accordion')
+         .locator('[data-testid^=collapsed-facet-accordion-item-]');
 
-      cy.get('@first-facet-accordion-item')
-         .invoke('attr', 'data-facet-name')
-         .as('first-facet-name');
+      const firstCollapsedAccordionItem = await facetList
+         .nth(0)
+         .elementHandle();
+      const secondCollapsedAccordionItem = await facetList
+         .nth(1)
+         .elementHandle();
 
       // Click the first facet accordion item.
-      cy.get('@first-facet-accordion-item')
-         .findByRole('button', { name: /expand/i, expanded: false })
-         .click();
+      if (!firstCollapsedAccordionItem) {
+         throw new Error('Could not find first collapsed accordion item');
+      }
+      await firstCollapsedAccordionItem.click();
+
+      // Define a Promise to wait for the search to be triggered and let the UI update.
+      const queryResponse = page.waitForResponse(async (response) => {
+         return (
+            response.url().includes('algolia') &&
+            response.url().includes('queries?') &&
+            response.status() === 200
+         );
+      });
 
       // Click the first facet item
-      cy.get('@first-facet-accordion-item')
-         .findAllByRole('option')
-         .first()
-         .click()
-         .invoke('attr', 'data-value')
-         .as('first-facet-option-value');
+      const firstFacetOption = await firstCollapsedAccordionItem.$(
+         'button[role="option"]'
+      );
+      await firstFacetOption?.click();
+      const { results } = await (await queryResponse).json();
 
-      // Wait for the search to be triggered and let the UI update.
-      cy.wait('@search');
+      // Check that the refinement value is in the current refinements.
+      const firstFacetOptionValue = await firstFacetOption?.getAttribute(
+         'data-value'
+      );
+      if (!firstFacetOptionValue) {
+         throw new Error('Could not find first facet option value');
+      }
+      await expect(
+         page.getByTestId(`current-refinement-${firstFacetOptionValue}`)
+      ).toBeVisible();
 
-      cy.get('@first-facet-option-value').then((refinementValue) => {
-         const lowercaseRefinementValue: string = (
-            refinementValue as any
-         ).toLowerCase();
+      // Reduce the search results to a single array of products.
+      const filteredProducts = results.reduce(
+         (products: [], searchResults: { hits: [] }) => {
+            return [...products, ...searchResults.hits];
+         },
+         []
+      );
 
-         // Check that the refinement value is in the current refinements.
-         cy.findAllByTestId(`current-refinement-${refinementValue}`).should(
-            'exist'
+      // Check that the refinement value is in the search results.
+      const firstFacetName = await firstCollapsedAccordionItem.getAttribute(
+         'data-facet-name'
+      );
+      if (!firstFacetName) {
+         throw new Error('Could not find first facet name');
+      }
+      filteredProducts.forEach((product: any) => {
+         expect(resolvePath(product, firstFacetName)).toContain(
+            firstFacetOptionValue
          );
-
-         // Check that the refinement value is in the search results.
-         cy.get('@first-facet-name').then((facetName) => {
-            cy.window()
-               .its('filteredProducts')
-               .each((product) => {
-                  cy.wrap(product)
-                     .its(facetName as any)
-                     .should(
-                        'satisfy',
-                        function isRefinedFacetValue(value: any) {
-                           return (
-                              (typeof value === 'string' &&
-                                 value.toLowerCase() ===
-                                    lowercaseRefinementValue) ||
-                              (Array.isArray(value) &&
-                                 value.some(
-                                    (t) =>
-                                       t.toLowerCase() ===
-                                       lowercaseRefinementValue
-                                 ))
-                           );
-                        }
-                     );
-               });
-         });
       });
-
-      cy.get('@second-facet-accordion-item')
-         .invoke('attr', 'data-facet-name')
-         .as('second-facet-name');
 
       // Click the second facet accordion item.
-      cy.get('@second-facet-accordion-item')
-         .findByRole('button', { name: /expand/i, expanded: false })
-         .click();
+      if (!secondCollapsedAccordionItem) {
+         throw new Error('Could not find second collapsed accordion item');
+      }
+      await secondCollapsedAccordionItem.click();
 
       // Click the second facet item
-      cy.get('@second-facet-accordion-item')
-         .findAllByRole('option')
-         .first()
-         .click()
-         .invoke('attr', 'data-value')
-         .as('second-facet-option-value');
+      const secondFacetOption = await secondCollapsedAccordionItem.$(
+         '[role="option"]'
+      );
+      await secondFacetOption?.click();
 
-      cy.get('@second-facet-option-value').then((refinementValue) => {
-         // Check that the refinement value is in the current refinements.
-         cy.findAllByTestId(`current-refinement-${refinementValue}`).should(
-            'exist'
-         );
-      });
+      // Check that the refinement value is in the current refinements.
+      const secondFacetOptionValue = await secondFacetOption?.getAttribute(
+         'data-value'
+      );
+      if (!secondFacetOptionValue) {
+         throw new Error('Could not find second facet option value');
+      }
+      await expect(
+         page.getByTestId(`current-refinement-${secondFacetOptionValue}`)
+      ).toBeVisible();
 
-      cy.findAllByTestId(/current-refinement-/i)
-         .first()
-         .invoke('attr', 'data-testid')
-         .then((testId) => {
-            // Remove the refinement
-            cy.findByTestId(testId!)
-               .findByRole('button', { name: /remove/i })
-               .click();
+      // Remove the newest refinement.
+      await page
+         .getByTestId(`current-refinement-${secondFacetOptionValue}`)
+         .getByRole('button', { name: /remove/i })
+         .click();
 
-            // Check that the refinement is no longer in the current refinements.
-            cy.findByTestId(testId!).should('not.exist');
-         });
+      // Check that the refinement value is not in the current refinements.
+      await expect(
+         page.getByTestId(`current-refinement-${secondFacetOptionValue}`)
+      ).not.toBeVisible();
 
-      // Reset the filters
-      cy.findByRole('button', { name: /clear all filters/i }).click();
+      // Reset the filters.
+      await page.getByRole('button', { name: /clear all filters/i }).click();
+
       // Check that the current refinements are empty
-      cy.findByTestId(/current-refinement-/i).should('not.exist');
+      expect(
+         await page.locator('[data-testid^=current-refinement-]').count()
+      ).toBe(0);
    });
 });
-
-export {};
