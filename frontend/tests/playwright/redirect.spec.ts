017a2b65ea562add5eb68dcfa9cd42afed8f04c7
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-12-20 14:06:35 -0600

    copy over redirect cypress test to playwright

---
+++ frontend/tests/playwright/redirect.spec.ts
@@ -0,0 +1,41 @@
+describe('/parts page', () => {
+   it('redirects to correct sitemap', () => {
+      cy.request({ url: '/Parts/sitemap.xml', followRedirect: false }).then(
+         (response) => {
+            expect(response.status).to.eq(308);
+            expect(response.headers.location).to.match(
+               /\/sitemap\/parts\.xml$/
+            );
+         }
+      );
+
+      cy.request({ url: '/Tools/sitemap.xml', followRedirect: false }).then(
+         (response) => {
+            expect(response.status).to.eq(308);
+            expect(response.headers.location).to.match(
+               /\/sitemap\/tools\.xml$/
+            );
+         }
+      );
+
+      cy.request({ url: '/Shop/sitemap.xml', followRedirect: false }).then(
+         (response) => {
+            expect(response.status).to.eq(308);
+            expect(response.headers.location).to.match(
+               /\/sitemap\/marketing\.xml$/
+            );
+         }
+      );
+
+      cy.request({ url: '/products/sitemap.xml', followRedirect: false }).then(
+         (response) => {
+            expect(response.status).to.eq(308);
+            expect(response.headers.location).to.match(
+               /\/sitemap\/products\.xml$/
+            );
+         }
+      );
+   });
+});
+
+export {};
