e95288fad6ea59bbd2443742f237eebadba66a60
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-01-03 16:22:46 -0800

    add msw servers and handlers for playwright tests
    This just adds the base files necessary to set up the msw server, worker,
    and handlers for the playwright tests. The handlers are not yet used in
    the tests, but they will be in a future commit.
    
    In addition, we will only setup the msw server as there is a compatibility
    issue between `Playwright`'s `page.route` and `msw`'s interceptors. Until,
    we can dynamically modify the handlers, we will only setup the mocks
    for the server.

---
+++ frontend/tests/playwright/msw/index.ts
@@ -0,0 +1,21 @@
+export const setupMocks = async () => {
+   // Only setup mocks for the server and not the browser
+
+   /* We currently don't have a way to dynamically set up the handlers for MSW.
+    * This wouldn't be a problem, because we can just pass through any requests
+    * that aren't handled. However, we're using Playwright's `page.route` method
+    * to intercept and mock Browser requests, and it seems that this method is
+    * incompatible with MSW's interceptor.
+    *
+    * As a result, it always defaults to using MSW's interceptor. This means we
+    * can't handle the current request, because the test that's using page.route
+    * is intentionally returning a failed response. We don't want to set this
+    * failed response as the default for all requests.
+    *
+    * Therefore, we only set up MSW for the server, and not the browser.
+    */
+   if (typeof window === 'undefined') {
+      const { mswServer } = await import('./server');
+      mswServer.listen({ onUnhandledRequest: 'warn' });
+   }
+};
