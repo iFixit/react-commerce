8d89f8f8b1e5fbcc984f4580de2d5d65038b92cb
batbattur <bat@ifixit.com>
2023-01-04 15:12:44 -0800

    Add test for a product with multiple images
    This test grabs the src of the currently displayed image on the
    product page using the helper function, then clicks on the next image.
    
    Then when the next image becomes the active image, it also grabs the
    src and compares it with the first image src to make sure different
    images are displayed.

--- frontend/tests/playwright/product/product_image.spec.ts
+++ frontend/tests/playwright/product/product_image.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
 test.describe('Product image test', () => {
    test('product with a single image ', async ({ page }) => {
@@ -9,4 +9,33 @@ test.describe('Product image test', () => {
          .first();
       expect(image).toBeVisible;
    });
+
+   test('product with multiple images ', async ({ page }) => {
+      await page.goto('/products/repair-business-toolkit');
+
+      const firstImageSrc = await getCurrentActiveImgSrc(page);
+      const firstImage = page.locator(`img[src="${firstImageSrc}"]`).first();
+      await expect(firstImage).toBeVisible();
+
+      // Click on the arrow for the next image
+      await page.getByTestId('swiper-next-image').first().click();
+
+      const secondImageSrc = await getCurrentActiveImgSrc(page);
+      const secondImage = page.locator(`img[src="${secondImageSrc}"]`).first();
+      await expect(secondImage).toBeVisible();
+
+      // Assert that currently active image has different src than the first one
+      expect(firstImageSrc).not.toEqual(secondImageSrc);
+   });
+
+   /*
+    * Returns the src of the currently displayed active image
+    * Applies only for product variants with multiple images
+    */
+   async function getCurrentActiveImgSrc(page: Page) {
+      const divElement = await page.$('.swiper-slide.swiper-slide-active');
+      const imgElement = await divElement!.$$('img');
+      const src = await imgElement[0].getAttribute('src');
+      return src;
+   }
 });
