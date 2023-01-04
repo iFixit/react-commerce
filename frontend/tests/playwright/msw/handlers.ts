a52edc8514472a5fe7546649b65c8590fab40c88
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-01-03 16:54:48 -0800

    conditionally intercept findProduct request
    We are still in the process of trying to figure out how to dynamically
    add handlers. However, that will block us from moving forward and
    writing more tests. So, we are going to conditionally intercept the
    request based on the product handle we are looking for. For now, if
    we are looking for the product with the handle 'caig-electronic-cleaner',
    which is the same product we currently have mocked in our mocked data,
    then we will intercept the request and return the mocked data. Otherwise,
    we will request the original data and return it.

--- frontend/tests/playwright/msw/handlers.ts
+++ frontend/tests/playwright/msw/handlers.ts
@@ -3,6 +3,20 @@ import { mockedProductQuery } from '@tests/jest/__mocks__/products';
 
 export const handlers: RequestHandler[] = [
    graphql.query('findProduct', async (req, res, ctx) => {
-      return res(ctx.data(mockedProductQuery));
+      if (req.variables.handle === 'caig-electronic-cleaner') {
+         return res(ctx.data(mockedProductQuery));
+      } else {
+         // Perform an original request to the intercepted request URL
+         const originalResponse = await ctx.fetch(req);
+         const originalResponseData = await originalResponse.json();
+
+         /*
+          * A `data` property will be added to the original response so in order
+          * to properly return the correct response structure we need to remove
+          * the `data` property so we don't end up with a nested `data` property.
+          */
+         // Return the original response product
+         return res(ctx.data(originalResponseData.data));
+      }
    }),
 ];
