234f72478398f39662f6339e60901acd9ac1fafa
Andrew Gilbert <andrewg@ifixit.com>
2023-01-04 13:19:16 -0800

    Troubleshooting: Load wiki text on page
    This is using the pre-rendered text, which may not be the best long-term
    plan. It's just an initial proof-of-concept to show that we can load
    a wiki from the API and render it on the page.

--- frontend/templates/troubleshooting/index.tsx
+++ frontend/templates/troubleshooting/index.tsx
@@ -2,15 +2,35 @@ import { useRouter } from 'next/router';
 import { DefaultLayout } from '@layouts/default';
 import { DefaultLayoutProps } from '@layouts/default/server';
 import Head from 'next/head';
+import { IFixitAPIClient } from '@ifixit/ifixit-api-client';
+import React from 'react';
+import { useAppContext } from '@ifixit/app';
 
 const Wiki: NextPageWithLayout<{ layoutProps: DefaultLayoutProps }> = () => {
+   const appContext = useAppContext();
+   const client = new IFixitAPIClient({ origin: appContext.ifixitOrigin });
+
    const { wikiname } = useRouter().query;
+   const [wikiData, setWikiData] = React.useState(null);
+   React.useEffect(() => {
+      client
+         .get(`wikis/WIKI/${wikiname}`, {
+            credentials: 'include',
+         })
+         .then((res: any) => {
+            setWikiData(res);
+         });
+   }, [wikiname]);
+   if (!wikiData) {
+      return <p>waiting</p>
+   }
    return (
       <div>
          <Head>
             <meta name="robots" content="noindex" />
          </Head>
-         Wiki {wikiname}
+         <h1>Wiki {wikiname}</h1>
+         <p dangerouslySetInnerHTML={{ __html: wikiData.contents_rendered }} />
       </div>
    );
 };
