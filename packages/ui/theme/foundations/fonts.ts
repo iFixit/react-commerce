e1f97d30cab74996f13f8d0dfb858aa4a9846d87
David Rans <david@ifixit.com>
2023-01-05 10:41:03 -0700

    Use current core-primitives sans font stack
    This is what we are now using on the rest of ifixit.com. We can adjust
    the core primitives stack later if we want to tweak it.

--- packages/ui/theme/foundations/fonts.ts
+++ packages/ui/theme/foundations/fonts.ts
@@ -1,8 +1,8 @@
 import type { ThemeOverride } from '@chakra-ui/react';
-// import primitives from '@core-ds/primitives';
+import primitives from '@core-ds/primitives';
 
 export const fonts: ThemeOverride['fonts'] = {
-   body: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"`,
-   heading: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"`,
-   mono: `ui-monospace, SFMono-Regular, SF Mono, Menlo,Consolas, Liberation Mono, monospace`,
+   body: primitives.fontFamily.sansSystem,
+   heading: primitives.fontFamily.sansSystem,
+   mono: primitives.fontFamily.monoSystem,
 };
