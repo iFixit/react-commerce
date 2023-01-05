2d80df6e098b24322e27cbde0d80eb16faeb0c8c
Andrew Gilbert <andrewg@ifixit.com>
2023-01-05 11:23:11 -0800

    Troubleshooting: Create own `getServerSideProps`
    This is based on the `server.tsx` file for `page`, which  we were
    previously using here.

---
+++ frontend/templates/troubleshooting/hooks/useTroubleshootingProps.tsx
@@ -0,0 +1,4 @@
+import { WithProvidersProps } from '@components/common';
+import type { WithLayoutProps } from '@layouts/default/server';
+
+export type TroubleshootingProps = WithProvidersProps<WithLayoutProps<{}>>;
