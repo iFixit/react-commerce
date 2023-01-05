747cac44eff651c3b32df7dec815e0fb5494ea3d
Andrew Gilbert <andrewg@ifixit.com>
2023-01-05 12:32:18 -0800

    Troubleshooting: `noindex` page
    This also adds a test to check that the page is `noindex`ed. We'll need
    to delete it later, but for now, it's nice to be sure we're not
    accidentally making the page indexable.

--- frontend/templates/troubleshooting/index.tsx
+++ frontend/templates/troubleshooting/index.tsx
@@ -1,10 +1,18 @@
 import { useRouter } from 'next/router';
 import { DefaultLayout } from '@layouts/default';
 import { DefaultLayoutProps } from '@layouts/default/server';
+import Head from 'next/head';
 
 const Wiki: NextPageWithLayout<{ layoutProps: DefaultLayoutProps }> = () => {
    const { wikiname } = useRouter().query;
-   return <div>Wiki {wikiname}</div>;
+   return (
+      <div>
+         <Head>
+            <meta name="robots" content="noindex" />
+         </Head>
+         Wiki {wikiname}
+      </div>
+   );
 };
 
 Wiki.getLayout = function getLayout(page, pageProps) {
--- frontend/tests/playwright/troubleshooting/loading.spec.ts
+++ frontend/tests/playwright/troubleshooting/loading.spec.ts
@@ -5,4 +5,12 @@ test.describe('Vulcan page', () => {
       await page.goto('/Vulcan/iPhone');
       await expect(page.getByText('iPhone')).toBeVisible();
    });
+
+   test('it should not be indexed', async ({ page }) => {
+      await page.goto('/Vulcan/iPhone');
+      // check that the meta robots tag is set to noindex
+      expect(
+         page.locator('meta[name="robots"][content="noindex"]')
+      ).toBeTruthy();
+   });
 });
