2e8d5a7463604ff016dda6fa99f04fc71d192206
Andrew Gilbert <andrewg@ifixit.com>
2023-01-06 22:13:22 -0800

    Troubleshooting: Update test to reflect rendered wiki

--- frontend/tests/playwright/troubleshooting/loading.spec.ts
+++ frontend/tests/playwright/troubleshooting/loading.spec.ts
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Vulcan page', () => {
    test('it loads', async ({ page }) => {
-      await page.goto('/Vulcan/iPhone');
-      await expect(page.getByText('iPhone')).toBeVisible();
+      await page.goto('/Vulcan/Dryer_Not_Spinning');
+      await expect(page.getByText('Dryer Not Spinning')).toBeVisible();
    });
 
    test('it should not be indexed', async ({ page }) => {
