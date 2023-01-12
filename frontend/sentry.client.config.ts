675309cded3b3b9bce1efecbd505785f06ba0c3e
Daniel Beardsley <daniel@ifixit.com>
2023-01-12 11:12:05 -0800

    Sentr Tracing: increase sample rate significantly
    We're already paid for this much data collection in sentry, so let's use
    it.

--- frontend/sentry.client.config.ts
+++ frontend/sentry.client.config.ts
@@ -18,7 +18,7 @@ Sentry.init({
    integrations: [new BrowserTracing()],
    sampleRate: 1.0,
    normalizeDepth: 5,
-   tracesSampleRate: 0.005,
+   tracesSampleRate: 0.05,
    // ...
    // Note: if you want to override the automatic release value, do not set a
    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
