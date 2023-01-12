e70d716a99db6429bc51503a68b709edc351d305
Andrew Gilbert <andrewg@ifixit.com>
2023-01-12 15:16:58 -0800

    Troubleshooting: Load page which exists
    `iPhone` doesn't actually exist, thus, the test doesn't pass. This
    switches it to use `Dryer_Not_Spinning` like its sibling test.
    
    I think this might have only just come up because previously we weren't
    returning a 404 for wikis which don't exist, so the erroring-out
    client-side page still had the meta tag on it, satistfying the test.

--- frontend/tests/playwright/troubleshooting/loading.spec.ts
+++ frontend/tests/playwright/troubleshooting/loading.spec.ts
@@ -7,7 +7,7 @@ test.describe('Vulcan page', () => {
    });
 
    test('it should not be indexed', async ({ page }) => {
-      await page.goto('/Vulcan/iPhone');
+      await page.goto('/Vulcan/Dryer_Not_Spinning');
       // check that the meta robots tag is set to noindex
       const meta = page.locator('meta[name="robots"]');
       await expect(meta).toHaveAttribute('content', 'noindex');
