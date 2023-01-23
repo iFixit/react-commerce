3e3043bfaf70605c17d44ec9a9f66b5c91918d2f
Mason McElvain (he/him) <52104630+masonmcelvain@users.noreply.github.com>
2023-01-23 05:46:08 -0800

    fix(jest): drop unnecessary export

--- frontend/tests/jest/__mocks__/svg.tsx
+++ frontend/tests/jest/__mocks__/svg.tsx
@@ -4,5 +4,4 @@ const SvgrMock = React.forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
    (props, ref) => <svg ref={ref} {...props} />
 );
 
-export const ReactComponent = SvgrMock;
 export default SvgrMock;
