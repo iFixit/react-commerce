01dfc6974c1f134202aaacae4a5aaf5ed0be0f8a
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-01-24 15:37:19 -0800

    update comment about starting msw for the browser
    We will now be able to dynamically add handlers to msw, so this comment
    is partially incorrect. We will will not be using `page.route` in the
    future to intercept client-side requests but we will still not use an
    `msw` browser side serivce worker to intercept requests -- at least in
    Playwright. We will only use the `msw` browser side service worker
    when running the dev server with API mocking enabled.

--- frontend/tests/playwright/msw/index.ts
+++ frontend/tests/playwright/msw/index.ts
@@ -3,18 +3,9 @@ import bootstrapMockServer from './server';
 export const setupMocks = async () => {
    // Only setup mocks for the server and not the browser
 
-   /* We currently don't have a way to dynamically set up the handlers for MSW.
-    * This wouldn't be a problem, because we can just pass through any requests
-    * that aren't handled. However, we're using Playwright's `page.route` method
-    * to intercept and mock Browser requests, and it seems that this method is
-    * incompatible with MSW's interceptor.
-    *
-    * As a result, it always defaults to using MSW's interceptor. This means we
-    * can't handle the current request, because the test that's using page.route
-    * is intentionally returning a failed response. We don't want to set this
-    * failed response as the default for all requests.
-    *
-    * Therefore, we only set up MSW for the server, and not the browser.
+   /**
+    * To Do -- Integrate the browser service worker to be able to use static
+    * handlers when debugging or testing handlers in development mode.
     */
    if (typeof window === 'undefined') {
       const mswServer = bootstrapMockServer();
