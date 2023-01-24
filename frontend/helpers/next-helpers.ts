a73e16d27c91d7e4573ec212cf9f4bb18455f297
Daniel Beardsley <daniel@ifixit.com>
2023-01-24 09:30:23 -0800

    withLogging: drop pageName parameter
    Since we're mostly using these timing logs for DataDog, let's cater to
    their style. They already process the whole log output for one request
    at a time. This means their system already knows which nextjs "page" the
    response came from and it's better to have common metric names like
    "server_side_props" than metric names that are different for each page.
    
    So, let's just use `server_side_props` as the metric name and stop
    taking in a pageName parameter.

--- frontend/helpers/next-helpers.ts
+++ frontend/helpers/next-helpers.ts
@@ -7,53 +7,40 @@ import type { GetServerSidePropsMiddleware } from '@lib/next-middleware';
 import * as Sentry from '@sentry/nextjs';
 import { GetServerSidePropsContext } from 'next';
 
-interface LoggingMiddlewareOptions {
-   pageName: string;
-}
-
-interface WithLoggingMiddleware {
-   (options: LoggingMiddlewareOptions): GetServerSidePropsMiddleware;
-}
-
-export const withLogging: WithLoggingMiddleware =
-   ({ pageName }) =>
-   (next) => {
-      return withTiming(
-         `page.${pageName}.server_side_props`,
-         async (context) => {
-            console.log('context.resolvedUrl', context.resolvedUrl);
-            console.log('context.req.url', context.req.url);
-            Sentry.setContext('Extra Info', {
-               headers: context.req.headers,
-               url: context.req.url,
-               method: context.req.method,
-               locale: context.locale,
-               ...context.params,
-               ...context.query,
-            });
-            const isCacheDisabled =
-               CACHE_DISABLED || context.query._vercel_no_cache === '1';
+export const withLogging: GetServerSidePropsMiddleware = (next) => {
+   return withTiming(`server_side_props`, async (context) => {
+      console.log('context.resolvedUrl', context.resolvedUrl);
+      console.log('context.req.url', context.req.url);
+      Sentry.setContext('Extra Info', {
+         headers: context.req.headers,
+         url: context.req.url,
+         method: context.req.method,
+         locale: context.locale,
+         ...context.params,
+         ...context.query,
+      });
+      const isCacheDisabled =
+         CACHE_DISABLED || context.query._vercel_no_cache === '1';
+      if (isCacheDisabled) {
+         clearCache();
+      }
+      return next(context)
+         .then((result) => {
             if (isCacheDisabled) {
+               context.res.setHeader(
+                  'Cache-Control',
+                  'no-store, no-cache, must-revalidate, stale-if-error=0'
+               );
                clearCache();
             }
-            return next(context)
-               .then((result) => {
-                  if (isCacheDisabled) {
-                     context.res.setHeader(
-                        'Cache-Control',
-                        'no-store, no-cache, must-revalidate, stale-if-error=0'
-                     );
-                     clearCache();
-                  }
-                  return result;
-               })
-               .catch((err) => {
-                  setSentryPageContext(context);
-                  throw err;
-               });
-         }
-      );
-   };
+            return result;
+         })
+         .catch((err) => {
+            setSentryPageContext(context);
+            throw err;
+         });
+   });
+};
 
 export function noindexDevDomains(context: GetServerSidePropsContext) {
    if (context.req.headers['user-agent'] !== PROD_USER_AGENT) {
--- frontend/pages/Shop/[handle].tsx
+++ frontend/pages/Shop/[handle].tsx
@@ -4,9 +4,7 @@ import { getProductListServerSideProps } from '@templates/product-list/server';
 
 export { default } from '@templates/product-list';
 
-export const getServerSideProps = withLogging({
-   pageName: 'shop',
-})(
+export const getServerSideProps = withLogging(
    getProductListServerSideProps({
       productListType: ProductListType.Marketing,
    })
