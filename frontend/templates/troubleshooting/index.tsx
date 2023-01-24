7356de39c77c814ca6961f3c5487ea64eb6e8a7d
Andrew Gilbert <andrewg@ifixit.com>
2023-01-24 15:14:25 -0800

    Troubleshooting: Update containers to match mockup better

--- frontend/templates/troubleshooting/index.tsx
+++ frontend/templates/troubleshooting/index.tsx
@@ -2,43 +2,47 @@ import { DefaultLayout } from '@layouts/default';
 import { DefaultLayoutProps } from '@layouts/default/server';
 import Head from 'next/head';
 import React from 'react';
-import { Box, Container, Heading } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading } from '@chakra-ui/react';
 import Prerendered from './prerendered';
 import { Problem, TroubleshootingData } from './hooks/useTroubleshootingProps';
 import SolutionCard from './solution';
-
-const renderStyles = {
-   a: {
-      color: 'blue.500',
-   },
-   'ul, ol': {
-      paddingLeft: 4,
-   },
-};
 
 const Wiki: NextPageWithLayout<{
    wikiData: TroubleshootingData;
    layoutProps: DefaultLayoutProps;
 }> = ({ wikiData }) => {
    return (
-      <Container sx={renderStyles}>
-         <Head>
-            <meta name="robots" content="noindex" />
-         </Head>
-         <Heading as="h1">{wikiData.title}</Heading>
-         {wikiData.introduction.map((intro) => (
-            <IntroductionSection key={intro.heading} intro={intro} />
-         ))}
-         {wikiData.solutions.map((solution) => (
-            <SolutionCard key={solution.heading} solution={solution} />
-         ))}
-         {wikiData.conclusion.map((conclusion) => (
-            <ConclusionSection
-               key={conclusion.heading}
-               conclusion={conclusion}
-            />
-         ))}
-      </Container>
+      <Flex
+         direction="column"
+         justifyContent="center"
+         width="100%"
+         fontSize="16px"
+      >
+         <Flex padding="0px 32px 32px" gap="48px" maxW="1280px" id="wrapper">
+            <Flex gap="16px" direction="column" id="main">
+               <Head>
+                  <meta name="robots" content="noindex" />
+               </Head>
+               <Heading as="h1">{wikiData.title}</Heading>
+               {wikiData.introduction.map((intro) => (
+                  <IntroductionSection key={intro.heading} intro={intro} />
+               ))}
+               {wikiData.solutions.map((solution, index) => (
+                  <SolutionCard
+                     key={solution.heading}
+                     index={index + 1}
+                     solution={solution}
+                  />
+               ))}
+               {wikiData.conclusion.map((conclusion) => (
+                  <ConclusionSection
+                     key={conclusion.heading}
+                     conclusion={conclusion}
+                  />
+               ))}
+            </Flex>
+         </Flex>
+      </Flex>
    );
 };
 
