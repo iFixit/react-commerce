8e82823dcce488301bdc40f3c1288ae7ae1e2249
Daniel Beardsley <daniel@ifixit.com>
2023-01-30 12:58:47 -0800

    Redis: tolerate disconnection
    If redis is unavailable, we want our app to treat every request like a
    miss.
    * Change "maxRetriesPerRequest" to 0 so individual commands aren't retried at all (fail fast)
    * Change retryStrategy to retry less often so on failure our logs arent'
      swamped with endless retries
    * Change reconnectOnError to always try reconnecting since next.js is a
      long lived process and we want it to auto-recover from temporary
      problems
    * Disable enableOfflineQueue so commands will fail if redis is
      unavailable instead of hanging until it reconnects
    * Lower the connect timeout because we expect connection to take in the
      10ms or so, not 10s.

--- frontend/lib/swr-cache/adapters/index.ts
+++ frontend/lib/swr-cache/adapters/index.ts
@@ -7,6 +7,18 @@ export const getCache = () => {
    if (REDIS_URL === undefined) {
       return nullAdapter();
    }
-   const client = new Redis(REDIS_URL);
+
+   const client = new Redis(REDIS_URL, {
+      connectTimeout: 500,
+      // Retry, connect every once in a while as cache misses / failures are OK
+      retryStrategy: (times) => Math.min(times * 5000, 10 * 60 * 1000),
+      maxRetriesPerRequest: 0,
+      // Always try re-connecting since one instance is shared across the whole
+      // process
+      reconnectOnError: (err) => 1,
+      // When not connected to redis, return error, don't add operations to a
+      // queue
+      enableOfflineQueue: false,
+   });
    return redisAdapter(client);
 };
