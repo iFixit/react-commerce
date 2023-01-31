04082ef9368a37d33249c54ca7ef8692f99dc7ad
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-01-30 16:33:49 -0800

    init browser side worker for msw
    Similar to the server side msw instance, we will need to export a
    boostrap function that will be used to initialize the worker when
    and where we need it.

--- frontend/tests/playwright/msw/index.ts
+++ frontend/tests/playwright/msw/index.ts
@@ -1,16 +1,17 @@
 import bootstrapMockServer from './server';
+import bootstrapMockWorker from './browser';
 
 export const setupMocks = async () => {
-   // Only setup mocks for the server and not the browser
-
-   /**
-    * To Do -- Integrate the browser service worker to be able to use static
-    * handlers when debugging or testing handlers in development mode.
-    */
    if (typeof window === 'undefined') {
       const mswServer = bootstrapMockServer();
 
       mswServer.listen({
+         onUnhandledRequest: process.env.CI ? 'bypass' : 'warn',
+      });
+   } else {
+      const mswWorker = bootstrapMockWorker();
+
+      mswWorker.start({
          onUnhandledRequest: process.env.CI ? 'bypass' : 'warn',
       });
    }
