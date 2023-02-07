76474edc243ad59d1a0c4c356f19bd5e5198de86
Daniel Beardsley <daniel.beardsley@gmail.com>
2023-02-07 11:52:38 -0800

    Cache: use exponent operator, *not* XOR
    A little surprised typescript didn't complain. I guess cause javascript only has `number` and not `int / float`

--- frontend/lib/swr-cache/adapters/index.ts
+++ frontend/lib/swr-cache/adapters/index.ts
@@ -13,7 +13,7 @@ export const getCache = () => {
       // Retry, connect every once in a while as cache misses / failures are OK
       retryStrategy: (times) => {
          times = Math.min(20, times);
-         return Math.min((50 + 2) ^ (times + 3), 10 * 60 * 1000);
+         return Math.min((50 + 2) ** (times + 3), 10 * 60 * 1000);
       },
       maxRetriesPerRequest: 0,
       // Always try re-connecting since one instance is shared across the whole
