59d50684f683bc0d81ddfee859c0baa06bd01626
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-02-08 12:17:29 -0800

    define methods types for each request to check API type
    Since we are using the same request handler for all request types,
    we need to define the types for each request type to check the API type.
    This will help us in determining the type of handler to create and
    return.
    
    That said, the methods availabe for each request are only a subset of
    all methods available for each api type. This is because we are using
    `msw` to mock the requests and `msw` only supports a subset of all
    methods available for each api type.
    
    https://mswjs.io/docs/api/graphql#operation-kind
    https://mswjs.io/docs/api/rest#methods
    
    Lastly, in order to check the API type, we will need to check which
    method of the subsets availabe is being used. For example, if the
    method is one of GraphQLMethods then the API type is GraphQL. Then
    by using a predicate return type we can ensure that Typescript
    safe guards the methods availabe for each API type and helps with type
    narrowing.

--- frontend/tests/playwright/msw/request-handler.ts
+++ frontend/tests/playwright/msw/request-handler.ts
@@ -1,12 +1,65 @@
 import { RequestHandler } from 'msw';
 
+/**
+ * These methods are a subset of the GraphQL operational methods that are
+ * supported by MSW's `graphql` handler. There are of course other methods
+ * `MSW` supports, but we are only interested in these two for now.
+ *
+ * @see https://mswjs.io/docs/api/graphql#operation-kind
+ */
+export type GraphQLMethods = 'mutation' | 'query';
+
+/**
+ * These methods are a subset of the REST methods that are
+ * supported by MSW's `rest` handler - _with the exception of
+ * `options`_.
+ *
+ * @see https://mswjs.io/docs/api/rest#methods
+ */
+export type RestMethods = 'all' | 'get' | 'post' | 'put' | 'patch' | 'delete';
+
 export type RequestInfo = {
    endpoint: string;
-   method: string;
+   method: GraphQLMethods | RestMethods;
 };
 
 export default class Handler {
    static create(request: RequestInfo): RequestHandler {
       const { endpoint, method } = request;
+
+      if (isGraphQLMethod(method)) {
+         // return GraphQLHandler
+      } else if (isRestMethod(method)) {
+         // return RestHandler
+      } else {
+         throw new Error(
+            `Invalid method ${method}. Must be 'mutation', 'query', 'all', 'get', 'post', 'put', 'patch', or 'delete'.`
+         );
+      }
    }
+}
+
+/**
+ * This is a type guard to ensure that the `method` is a valid GraphQLMethod
+ * by using Typescript Predicate Return Types.
+ * @see https://www.typescriptlang.org/docs/handbook/2/classes.html#this-based-type-guards
+ */
+function isGraphQLMethod(method: string): method is GraphQLMethods {
+   return method === 'mutation' || method === 'query';
+}
+
+/**
+ * This is a type guard to ensure that the `method` is a valid RestMethod
+ * by using Typescript Predicate Return Types.
+ * @see https://www.typescriptlang.org/docs/handbook/2/classes.html#this-based-type-guards
+ */
+function isRestMethod(method: string): method is RestMethods {
+   return (
+      method === 'all' ||
+      method === 'get' ||
+      method === 'post' ||
+      method === 'put' ||
+      method === 'patch' ||
+      method === 'delete'
+   );
 }
