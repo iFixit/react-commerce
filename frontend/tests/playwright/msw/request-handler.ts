f29408ab7f8ec798c258ae948368c3c2712b6b17
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-02-08 12:07:30 -0800

    define the type for the request of the handler
    For both a graphql and a rest handler, we will need to pass the
    endpoint we are trying to mock and the method for the api call.
    Therefore, we can define a type that takes in these two parameters
    and can be used for both handlers.

--- frontend/tests/playwright/msw/request-handler.ts
+++ frontend/tests/playwright/msw/request-handler.ts
@@ -1,1 +1,12 @@
-export default class Handler {}
+import { RequestHandler } from 'msw';
+
+export type RequestInfo = {
+   endpoint: string;
+   method: string;
+};
+
+export default class Handler {
+   static create(request: RequestInfo): RequestHandler {
+      const { endpoint, method } = request;
+   }
+}
