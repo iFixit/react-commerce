f76afc0e9d233e57a85f3970c224cbd4482e8143
Federico Badini <federico@inkofpixel.com>
2023-03-15 16:44:07 +0100

    feat: working to hide product list facet panel when empty

--- frontend/templates/product-list/sections/ProductListChildrenSection.tsx
+++ frontend/templates/product-list/sections/ProductListChildrenSection.tsx
@@ -115,7 +115,7 @@ export function ProductListChildrenSection({
                         },
                      }}
                   >
-                     more to show
+                     more
                   </Text>
                </Button>
             )}
