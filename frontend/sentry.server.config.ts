3a75642f8ab86b3850b3fa65e3b8332a0be3949b
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-03-21 14:09:16 -0700

    debug

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -7,6 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN;
 
 Sentry.init({
+   debug: true,
    dsn: SENTRY_DSN,
    sampleRate: 1.0,
    normalizeDepth: 5,
