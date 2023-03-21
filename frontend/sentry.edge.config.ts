527a8779d73d778a3e9ebe0e02416947e7e54ace
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-03-21 13:27:32 -0700

    fix(sentry): add edge config
    Adds an edge config to initialize Sentry on Vercel (I think).
    https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#create-initialization-config-files
    
    This config file became available in Sentry version 7.31.0
    https://github.com/getsentry/sentry-javascript/blob/2738b5ff3fca3816c506eb1736a4c0a055c0fcf7/CHANGELOG.md?plain=1#L288

---
+++ frontend/sentry.edge.config.ts
@@ -0,0 +1,14 @@
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN;
+
+Sentry.init({
+   dsn: SENTRY_DSN,
+   sampleRate: 1.0,
+   normalizeDepth: 5,
+   initialScope: {
+      tags: {
+         'next.runtime': 'edge',
+      },
+   },
+});
