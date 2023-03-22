6119c7a09ed30e629cf0e70c22fffd92cf1766e5
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-03-22 14:47:28 -0700

    debug: use `context.query`, not `context.params`

--- frontend/pages/myPage.tsx
+++ frontend/pages/myPage.tsx
@@ -5,7 +5,7 @@ const MyComponent = () => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-   if (context.params?.myParam === 'two') {
+   if (context.query.myParam === 'two') {
       // only throw conditionally so that this page actually builds
       throw new Error("We don't like page two");
    }
