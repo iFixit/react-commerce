041b3b6c0524c9edb052ade96f0fc50b1038c4bf
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-03-22 14:05:42 -0700

    debug: try to reproduce failed reporting on vercel
    Same setup as https://github.com/getsentry/sentry-javascript/issues/6117
    
    style: apply prettier formatting diff

---
+++ frontend/pages/myPage.tsx
@@ -0,0 +1,16 @@
+import { GetServerSideProps } from 'next';
+
+const MyComponent = () => {
+   return <h1>Hello World!</h1>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+   if (context.params?.myParam === 'two') {
+      // only throw conditionally so that this page actually builds
+      throw new Error("We don't like page two");
+   }
+
+   return { props: {} };
+};
+
+export default MyComponent;
