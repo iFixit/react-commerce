3642a62ac5b9d10ec12e16fd94356aa9342f9e01
Massimo De Marchi <massimo@inkofpixel.com>
2023-03-28 15:44:55 +0200

    fix(product page): Add product overview section bottom margin

--- frontend/templates/product/sections/ProductOverviewSection/index.tsx
+++ frontend/templates/product/sections/ProductOverviewSection/index.tsx
@@ -66,7 +66,7 @@ export function ProductOverviewSection({
    const isForSale = useIsProductForSale(product);
 
    return (
-      <Wrapper as="section" id="product">
+      <Wrapper as="section" id="product" mb="16">
          <Flex>
             <Flex
                position="sticky"
