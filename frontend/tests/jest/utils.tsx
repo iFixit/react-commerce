0e6ad894f23dceb5baa8eb13ad760aa5944b94c0
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-03-30 11:58:22 -0700

    extract product reviews mock to a separate file
    Similar to the product list mock, this extracts the ProductReview mock
    from `products.ts` into `reviews.ts`. This should allow us to add more
    mocks for the product reviews in the future and while retaining the
    context of what the mocks in the file are for.

--- frontend/tests/jest/utils.tsx
+++ frontend/tests/jest/utils.tsx
@@ -67,30 +67,3 @@ export const renderWithAppContext = (
    ui: any,
    options?: RenderOptions
 ): RenderResult => render(ui, { wrapper: GlobalContextProvider, ...options });
-
-export const getReviewsResponse = (
-   type: 'filled' | 'empty' = 'filled'
-): ProductReviewData => {
-   let reviews: ProductReview[] = mockedReviews;
-
-   if (type === 'empty') {
-      reviews = [];
-   }
-
-   return {
-      reviews: reviews,
-      count: reviews.length,
-      average:
-         reviews.length == 0
-            ? 0
-            : reviews.reduce((total, next) => total + (next.rating || 0), 0) /
-              reviews.length,
-      groupedReviews: {
-         1: reviews.filter((review) => review.rating == 1).length,
-         2: reviews.filter((review) => review.rating == 2).length,
-         3: reviews.filter((review) => review.rating == 3).length,
-         4: reviews.filter((review) => review.rating == 4).length,
-         5: reviews.filter((review) => review.rating == 5).length,
-      },
-   };
-};
