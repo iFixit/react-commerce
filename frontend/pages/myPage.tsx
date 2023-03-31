d5ee360388eab119e39c699d3a1f1ce1d82a52f0
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-03-31 09:44:50 -0700

    debug: add a data fetcher that throws async

---
+++ frontend/pages/myPage.tsx
@@ -0,0 +1,16 @@
+import { GetServerSideProps } from 'next';
+
+const MyComponent = () => {
+   return <h1>Hello World!</h1>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+   if (context.query.myParam === 'two') {
+      // only throw conditionally so that this page actually builds
+      Promise.reject(new Error("We don't like page two"));
+   }
+
+   return { props: {} };
+};
+
+export default MyComponent;
