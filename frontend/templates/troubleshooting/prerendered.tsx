cee5ad7e594c08f21f3e506a9a319da845a379e3
Andrew Gilbert <andrewg@ifixit.com>
2023-04-18 12:51:51 -0700

    Prerendered: Use `fontSize` to control the font size of the text
    This uses a default `fontSize` value on the `Box` instead of providing
    a `fontSize` for the `p`, which means that the `p` will inherit the
    `Box`'s font size. As a result, the `chakra` `fontSize` prop on the
    `Prerendered` component will also affect the font size for the text
    within it. This allows us to configure the base size of the text in
    the component, rather than having it constantly set to 16px.

--- frontend/templates/troubleshooting/prerendered.tsx
+++ frontend/templates/troubleshooting/prerendered.tsx
@@ -31,7 +31,6 @@ const renderStyles: SystemStyleObject = {
    p: {
       lineHeight: '1.38',
       fontWeight: 'regular',
-      fontSize: '16px',
       color: 'gray.700',
       alignSelf: 'stretch',
       paddingBottom: 6,
@@ -201,23 +200,26 @@ const renderStyles: SystemStyleObject = {
    },
 };
 
-const Prerendered = chakra(function Prerendered({
-   html,
-   className,
-}: {
-   html: string;
-   className?: string;
-}) {
-   useEffect(() => {
-      import('lite-youtube-embed');
-   }, []);
-   return (
-      <Box
-         className={className}
-         sx={renderStyles}
-         dangerouslySetInnerHTML={{ __html: html }}
-      />
-   );
-});
+const Prerendered = chakra(
+   function Prerendered({
+      html,
+      className,
+   }: {
+      html: string;
+      className?: string;
+   }) {
+      useEffect(() => {
+         import('lite-youtube-embed');
+      }, []);
+      return (
+         <Box
+            className={className}
+            sx={renderStyles}
+            dangerouslySetInnerHTML={{ __html: html }}
+         />
+      );
+   },
+   { baseStyle: { fontSize: '16px' } }
+);
 
 export default Prerendered;
