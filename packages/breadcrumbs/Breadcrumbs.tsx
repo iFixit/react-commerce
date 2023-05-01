e95841ef170670506472c351787cf257ff856f4e
Ricky <ricky@ifixit.com>
2023-05-01 16:34:29 -0700

    Remove hover effect from last breadcrumb

--- packages/breadcrumbs/Breadcrumbs.tsx
+++ packages/breadcrumbs/Breadcrumbs.tsx
@@ -166,7 +166,7 @@ const IFixitBreadcrumbItem = function IFixitBreadcrumbItem({
          data-name={name}
       >
          <Text
-            as={Link}
+            as={url ? Link : undefined}
             noOfLines={1}
             href={url}
             fontWeight={isLast ? 500 : 400}
@@ -234,7 +234,7 @@ function IFixitCollapsedBreadcrumbItem({
 }: BreadcrumbItem & MenuItemProps & LinkProps) {
    return (
       <MenuItem
-         as={url ? Link : Text}
+         as={url ? Link : undefined}
          color="gray.900"
          _visited={{ color: menuItemProps.color || 'gray.900' }}
          _hover={{ textDecoration: 'none' }}
