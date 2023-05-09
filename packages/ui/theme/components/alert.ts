68983ed988d3e4f0af65148853a595cc985d1d8e
Ian Rohde <ian.rohde@gmail.com>
2023-05-09 16:13:16 -0700

    update Alert color schemes to match Figma

--- packages/ui/theme/components/alert.ts
+++ packages/ui/theme/components/alert.ts
@@ -13,19 +13,36 @@ const STATUSES: Record<string, string> = {
    loading: 'blue',
 };
 
+const BACKGROUNDS: Record<string, string> = {
+   default: '100',
+   info: '100',
+   warning: '50',
+   success: '50',
+   error: '50',
+   loading: '100',
+};
+
+const FOREGROUNDS: Record<string, string> = {
+   default: '300',
+   info: '300',
+   warning: '300',
+   success: '200',
+   error: '300',
+   loading: '300',
+};
+
 const subtle = definePartsStyle((props) => {
    const status: string = props.status ?? 'default';
    const colorScheme: string = STATUSES[status];
+   const backgroundScheme: string = BACKGROUNDS[status];
+   const foregroundScheme: string = FOREGROUNDS[status];
 
    return {
       container: {
-         bgColor: status === 'default' ? `${colorScheme}.100` : 'transparent',
-         border: '1px',
-         borderColor: `${colorScheme}.300`,
+         background: `${colorScheme}.${backgroundScheme}`,
+         borderWidth: '1px',
+         borderColor: `${colorScheme}.${foregroundScheme}`,
          borderRadius: 'base',
-         _light: {
-            bgColor: `${colorScheme}.50`,
-         },
       },
    };
 });
