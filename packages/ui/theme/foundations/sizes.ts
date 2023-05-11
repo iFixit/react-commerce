ac2da4a86fbc483bdb9da4824f35328c5bbe4e58
Ian Rohde <ian.rohde@gmail.com>
2023-05-11 16:01:05 -0700

    remove sizes `primitiveBreakpoint` values
    - sizes remain due to `header` value

--- packages/ui/theme/foundations/sizes.ts
+++ packages/ui/theme/foundations/sizes.ts
@@ -1,11 +1,5 @@
 import type { ThemeOverride } from '@chakra-ui/react';
-import { breakpoint as primitiveBreakpoint } from '@core-ds/primitives';
 
 export const sizes: ThemeOverride['sizes'] = {
    header: '68px',
-   sm: primitiveBreakpoint.sm,
-   md: primitiveBreakpoint.md,
-   lg: primitiveBreakpoint.lg,
-   xl: primitiveBreakpoint.xl,
-   '2xl': primitiveBreakpoint['2xl'],
 };
--- packages/ui/theme/index.ts
+++ packages/ui/theme/index.ts
@@ -10,6 +10,7 @@ import { colors } from './foundations/colors';
 import { fonts } from './foundations/fonts';
 import { radii } from './foundations/radii';
 import { shadow } from './foundations/shadow';
+import { sizes } from './foundations/sizes';
 import { space } from './foundations/space';
 import { zIndices } from './foundations/zIndices';
 import { styles } from './styles';
@@ -20,6 +21,7 @@ export const theme: ThemeOverride = {
    fonts,
    radii,
    shadows: shadow,
+   sizes,
    space,
    styles,
    zIndices,
