4f3192728494f1d1c227a190c90ace41722d2181
Ian Rohde <ian.rohde@gmail.com>
2023-05-17 18:05:12 -0700

    create shared HeadingSelfLink component

---
+++ frontend/components/ui/HeadingSelfLink.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Heading, HeadingProps, Link } from '@chakra-ui/react';
+import { FaIcon } from '@ifixit/icons';
+import { faLink } from '@fortawesome/pro-solid-svg-icons';
+
+export function HeadingSelfLink({
+   children,
+   ...props
+}: {
+   children: any;
+   props: HeadingProps;
+}) {
+   const id = children.toLowerCase().replace(/ /g, '-');
+   const iconStyles = {
+      height: '50%',
+      maxHeight: '4',
+      color: 'gray.500',
+      marginY: 'auto',
+      visibility: 'hidden',
+   };
+   const sharedStyles = {
+      display: 'inline-flex',
+      alignItems: 'stretch',
+   };
+
+   return (
+      <Heading
+         id={id}
+         sx={{ _hover: { '& svg': { visibility: 'visible' } } }}
+         {...sharedStyles}
+         {...props}
+      >
+         {children}
+         <Link href={`#${id}`} paddingLeft={2} minWidth={6} {...sharedStyles}>
+            <FaIcon icon={faLink} {...iconStyles} />
+         </Link>
+      </Heading>
+   );
+}
