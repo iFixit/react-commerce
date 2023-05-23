09b671eab11d499cc5d0e9015a5480e4835c950f
Ian Rohde <ian.rohde@gmail.com>
2023-05-23 11:09:41 -0700

    fix types

--- frontend/templates/troubleshooting/components/HeadingSelfLink.tsx
+++ frontend/templates/troubleshooting/components/HeadingSelfLink.tsx
@@ -5,45 +5,43 @@ import { faLink } from '@fortawesome/pro-solid-svg-icons';
 
 export interface HeadingSelfLinkProps {
    children: string;
-   props: HeadingProps;
-   selfLinked: boolean;
+   selfLinked?: boolean;
 }
 
-export const HeadingSelfLink = forwardRef<HeadingSelfLinkProps, 'h2'>(
-   ({ children, ...props }, ref) => {
-      const solutionHeading = children as string;
-      const solutionHeadingId = solutionHeading
-         .toLowerCase()
-         .replace(/ /g, '-');
+export const HeadingSelfLink = forwardRef<
+   HeadingProps & HeadingSelfLinkProps,
+   'h2'
+>(({ children, selfLinked, ...props }, ref) => {
+   const solutionHeading = children as string;
+   const solutionHeadingId = solutionHeading.toLowerCase().replace(/ /g, '-');
 
-      return (
-         <Heading
-            display="flex"
-            sx={{ _hover: { '& .heading_link-icon': { opacity: '1' } } }}
-            {...(props.selfLinked === true && { id: solutionHeadingId })}
-            {...props}
-            {...ref}
+   return (
+      <Heading
+         display="flex"
+         sx={{ _hover: { '& .heading_link-icon': { opacity: '1' } } }}
+         {...(selfLinked === true && { id: solutionHeadingId })}
+         {...props}
+         {...ref}
+      >
+         {children}
+         <Link
+            href={`#${solutionHeadingId}`}
+            display="inline-flex"
+            placeItems="center start"
+            paddingLeft={2}
+            minWidth={7}
+            aria-label={`${children} page link`}
          >
-            {children}
-            <Link
-               href={`#${solutionHeadingId}`}
-               display="inline-flex"
-               placeItems="center start"
-               paddingLeft={2}
-               minWidth={7}
-               aria-label={`${children} page link`}
-            >
-               <FaIcon
-                  className="heading_link-icon"
-                  icon={faLink}
-                  height="55%" // scale with smaller headings
-                  maxHeight={4} // cap size to 1em
-                  color="gray.500"
-                  opacity="0"
-                  transition="opacity 0.1s ease-in-out"
-               />
-            </Link>
-         </Heading>
-      );
-   }
-);
+            <FaIcon
+               className="heading_link-icon"
+               icon={faLink}
+               height="55%" // scale with smaller headings
+               maxHeight={4} // cap size to 1em
+               color="gray.500"
+               opacity="0"
+               transition="opacity 0.1s ease-in-out"
+            />
+         </Link>
+      </Heading>
+   );
+});
