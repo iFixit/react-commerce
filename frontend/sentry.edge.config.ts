02879886950b2e1eb984f91d6672ccc1029b7818
Federico Badini <federico@inkofpixel.com>
2023-06-16 16:30:06 +0200

    feat: add tag to sentry.edge.config
    Co-authored-by: Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>

--- frontend/sentry.edge.config.ts
+++ frontend/sentry.edge.config.ts
@@ -4,4 +4,9 @@ const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
    dsn: SENTRY_DSN,
+      initialScope: {
+      tags: {
+         'next.runtime': 'edge',
+      },
+   },
 });
