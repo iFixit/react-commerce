9c6f2735baeff4debbd2fec52cfc3c9ccdd269cb
Federico Badini <federico@inkofpixel.com>
2023-06-28 18:19:49 +0200

    force ipv4

--- frontend/codegen/strapi-schema-config.ts
+++ frontend/codegen/strapi-schema-config.ts
@@ -4,7 +4,7 @@ const schemaConfig: CodegenConfig = {
    overwrite: true,
    generates: {
       'lib/strapi-sdk/generated/schema.graphql': {
-         schema: 'http://localhost:1337/graphql',
+         schema: 'http://127.0.0.1:1337/graphql',
          plugins: ['schema-ast'],
       },
    },
