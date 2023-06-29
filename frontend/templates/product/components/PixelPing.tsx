4f9ed438cf6fd97fa7122cd2a95232d018f036cc
Jarred Stelfox <jarstelfox@mac.com>
2023-06-29 16:24:11 -0700

    Remove uneeded eslint line

--- frontend/templates/product/components/PixelPing.tsx
+++ frontend/templates/product/components/PixelPing.tsx
@@ -1,8 +1,5 @@
 import { PixelPing } from '@components/analytics/PixelPing';
 
 export const ProductPixelPing = ({ productcode }: { productcode: string }) => {
-   return (
-      // eslint-disable-next-line @next/next/no-img-element
-      <PixelPing id={`ifixit/product/${productcode}/en`} />
-   );
+   return <PixelPing id={`ifixit/product/${productcode}/en`} />;
 };
