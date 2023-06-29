2f0c41eca9596138d519f520c0b24c4f187633b3
Jarred Stelfox <jarstelfox@mac.com>
2023-06-29 16:23:45 -0700

    Refactor: Rename productcode pixel component
    We are going to add another useage. Let's update the general name to the
    exact scenerio

--- frontend/templates/product/components/PixelPing.tsx
+++ frontend/templates/product/components/PixelPing.tsx
@@ -1,8 +1,8 @@
-import { PixelPing as PixelPingComponent } from '@components/analytics/PixelPing';
+import { PixelPing } from '@components/analytics/PixelPing';
 
-export const PixelPing = ({ productcode }: { productcode: string }) => {
+export const ProductPixelPing = ({ productcode }: { productcode: string }) => {
    return (
       // eslint-disable-next-line @next/next/no-img-element
-      <PixelPingComponent id={`ifixit/product/${productcode}/en`} />
+      <PixelPing id={`ifixit/product/${productcode}/en`} />
    );
 };
