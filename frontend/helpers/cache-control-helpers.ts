7efe3ab9251f68334a0e69f6e7507300dc3f073b
Jarred Stelfox <jarstelfox@mac.com>
2023-08-03 14:09:36 -0700

    Refactor: Cache control easier configuration
    This commit refactors the cache control helpers to make it easier to
    configure the cache control headers.
    
    I had noticed that disabling the cache control headers was implemented in
    a way that was not very intuitive. In fact, the disabling code exists in
    another file / middleware.
    
    This commit makes it easy for us to move the disabling code to the
    this file.

--- frontend/helpers/cache-control-helpers.ts
+++ frontend/helpers/cache-control-helpers.ts
@@ -6,25 +6,29 @@ interface CacheControlOptions {
    staleWhileRevalidate: number;
 }
 
-const withCache =
-   (options: CacheControlOptions): GetServerSidePropsMiddleware =>
-   (next) =>
-   (context) => {
-      const maxAgeSeconds = options.sMaxAge / 1000;
-      const staleWhileRevalidateSeconds = options.staleWhileRevalidate / 1000;
-      context.res.setHeader(
-         'Cache-Control',
-         `public, s-maxage=${maxAgeSeconds}, max-age=${maxAgeSeconds}, stale-while-revalidate=${staleWhileRevalidateSeconds}`
-      );
+function getCacheString(options: CacheControlOptions) {
+   const maxAgeSeconds = options.sMaxAge / 1000;
+   const staleWhileRevalidateSeconds = options.staleWhileRevalidate / 1000;
+   return `public, s-maxage=${maxAgeSeconds}, max-age=${maxAgeSeconds}, stale-while-revalidate=${staleWhileRevalidateSeconds}`;
+}
+
+function withCacheValue(cacheValue: string): GetServerSidePropsMiddleware {
+   return (next) => (context) => {
+      context.res.setHeader('Cache-Control', cacheValue);
       return next(context);
    };
+}
 
-export const withCacheShort = withCacheControl({
-   sMaxAge: Duration(1).second,
-   staleWhileRevalidate: Duration(9).seconds,
-});
+export const withCacheShort = withCacheValue(
+   getCacheString({
+      sMaxAge: Duration(1).second,
+      staleWhileRevalidate: Duration(9).seconds,
+   })
+);
 
-export const withCacheLong = withCacheControl({
-   sMaxAge: Duration(5).minutes,
-   staleWhileRevalidate: Duration(1).day,
-});
+export const withCacheLong = withCacheValue(
+   getCacheString({
+      sMaxAge: Duration(5).minutes,
+      staleWhileRevalidate: Duration(1).day,
+   })
+);
