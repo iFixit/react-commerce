83d1ef24d3f64085cfd8b083b627898bd592874f
Massimo De Marchi <massimo@inkofpixel.com>
2023-08-11 15:46:47 +0200

    refactor(troubleshooting): Prevent FOUC

--- frontend/components/ui/IntlDate.tsx
+++ frontend/components/ui/IntlDate.tsx
@@ -1,7 +1,7 @@
 /**
  * This component renders a localized date in a way that is compatible with React hydration.
- * The first render will use a non localized format which is the same on the server and client.
- * The second render will use the localized format.
+ * The first render will not render the date to avoid FOUC.
+ * The second render (which happens in the client) will use the localized format.
  *
  * There are other ways to do this, for example by taking advantage of the Accept-Language header
  * to know the user's locale in the server. However this approach is the one that is more cache friendly.
@@ -21,9 +21,9 @@ export function IntlDate({
    options,
    className,
 }: IntlDateProps) {
-   const [stableFormattedDate, setStableFormattedDate] = useState(
-      value.toDateString()
-   );
+   const [stableFormattedDate, setStableFormattedDate] = useState<
+      string | null
+   >(null);
    const isoString = value.toISOString();
    const formattedDate = value.toLocaleDateString(locales, options);
 
