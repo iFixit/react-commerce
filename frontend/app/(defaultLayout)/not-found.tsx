48bb579d44ee524da6e483d6b97b5812716b654d
Jarred Stelfox <jarstelfox@mac.com>
2023-09-01 13:26:13 -0700

    App Router: Implementing a 404 Page
    This is another copy of the existing 404 page with some slight modifications. The main difference is that it uses the default layout instead of the empty layout.
    to use the default layout that includes the header and footer.

---
+++ frontend/app/(defaultLayout)/not-found.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { Center, Divider, Heading, Text, VStack } from '@chakra-ui/react';
+import DefaultLayout from './iFixitHeaderFooter';
+import { DEFAULT_STORE_CODE } from '@config/env';
+import { AppProviders } from '@components/common/AppProviders';
+import { getLayoutServerSideProps } from '@layouts/default/server';
+
+export default async function NotFound() {
+   const layoutPropsPromise = getLayoutServerSideProps({
+      storeCode: DEFAULT_STORE_CODE,
+   });
+
+   const layoutProps = await layoutPropsPromise;
+
+   return (
+      <AppProviders>
+         <DefaultLayout {...layoutProps}>
+            <Center
+               flexGrow={1}
+               paddingTop="16px"
+               paddingBottom="60px"
+               paddingLeft="20px"
+               paddingRight="20px"
+            >
+               <VStack>
+                  <Heading
+                     as="h2"
+                     fontSize={{ base: '4xl', md: '5xl' }}
+                     fontWeight="medium"
+                  >
+                     404
+                  </Heading>
+                  <Text fontWeight="medium" pb="3" textTransform="uppercase">
+                     Page not found
+                  </Text>
+                  <Divider
+                     borderWidth="1px"
+                     borderColor="red.500"
+                     width="50px"
+                  />
+                  <Text
+                     pt="3"
+                     overflowWrap="break-word"
+                     align="center"
+                     maxW="360px"
+                  >
+                     We can&apos;t find the page you&apos;re looking for.
+                     Whoops.
+                  </Text>
+               </VStack>
+            </Center>
+         </DefaultLayout>
+      </AppProviders>
+   );
+}
