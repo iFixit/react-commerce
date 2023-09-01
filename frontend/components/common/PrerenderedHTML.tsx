429215ce321df5119772fef4da93b48c57af0cb5
Ian Rohde <ian.rohde@gmail.com>
2023-09-01 10:51:54 -0700

    split styles into shared, troubleshooting, commerce
    - we need to apply styling based on location

--- frontend/components/common/PrerenderedHTML.tsx
+++ frontend/components/common/PrerenderedHTML.tsx
@@ -1,43 +1,23 @@
-import { Box, chakra } from '@chakra-ui/react';
-import 'lite-youtube-embed/src/lite-yt-embed.css';
 import { useEffect } from 'react';
-import { SystemStyleObject } from '@chakra-ui/react';
+import { Box, chakra, SystemStyleObject } from '@chakra-ui/react';
+import 'lite-youtube-embed/src/lite-yt-embed.css';
 
-const constrainStandardWidth = '282px'; // pulled from PHP app
-const chakraStyles: SystemStyleObject = {
-   '&': {
-      marginTop: { base: 4, sm: 6 },
-   },
+const constrainStandardWidth = '282px'; // pulled from PHP app for legacy image sizing
 
-   '.clearer': {
-      clear: 'both',
-      height: '0',
-      padding: '0',
-      margin: '0',
-      lineHeight: '0',
-      fontSize: '0',
-   },
+const sharedStyles: SystemStyleObject = {
+   a: {
+      color: 'brand.500',
 
-   '.headerContainer': {
-      display: 'flex',
-      alignItems: 'baseline',
-      marginBottom: 2,
-
-      '&:not(:first-of-type)': {
-         marginTop: { base: 4, sm: 6 },
-      },
-
-      '&:hover .selfLink': {
-         opacity: '1',
+      '&:hover': {
+         textDecoration: 'underline',
       },
    },
 
-   '.selfLink': {
-      color: 'gray.500',
-      order: '2', // swap the legacy order with flexbox
-      opacity: '0',
-      paddingLeft: 2,
-      transition: 'opacity var(--chakra-transition-duration-fast) ease-in-out',
+   blockquote: {
+      marginBlock: 6,
+      borderLeft: '4px solid',
+      borderLeftColor: 'gray.300',
+      paddingInline: '1em',
    },
 
    h3: {
@@ -54,36 +34,86 @@ const chakraStyles: SystemStyleObject = {
       lineHeight: '1.25',
    },
 
-   p: {
-      lineHeight: '1.38',
-      alignSelf: 'stretch',
+   pre: {
+      paddingInline: 1,
+      maxWidth: '100%',
+      overflow: 'auto',
+   },
 
-      '&:not(:first-of-type)': {
-         marginTop: '1em',
+   th: {
+      textAlign: 'left',
+   },
+
+   '.fa-svg-icon': {
+      svg: {
+         width: '1em',
+         height: '1em',
+         display: 'inline-block',
+
+         path: {
+            fill: 'currentColor',
+         },
+      },
+
+      '&.xs svg': {
+         width: '0.5em',
+         height: '0.5em',
+      },
+
+      '&.sm svg': {
+         width: '0.75em',
+         height: '0.75em',
+      },
+   },
+};
+
+const troubleshootingStyles: SystemStyleObject = {
+   ...sharedStyles,
+
+   '&': {
+      marginTop: { base: 4, sm: 6 },
+   },
+
+   '.clearer': {
+      clear: 'both',
+      height: '0',
+      padding: '0',
+      margin: '0',
+      lineHeight: '0',
+      fontSize: '0',
+   },
+
+   '.headerContainer': {
+      display: 'flex',
+      alignItems: 'baseline',
+
+      _notFirst: {
+         marginTop: { base: 4, sm: 6 },
+      },
+
+      '&:hover .selfLink': {
+         opacity: '1',
       },
    },
 
-   'ul, ol': {
-      marginBlock: { base: 4, sm: 6 },
-      marginInlineStart: '1em',
-      overflowX: 'auto', // clear child media floats
-      paddingLeft: '2em',
-
-      'li:not(:first-of-type)': {
-         marginTop: '1em',
-      },
-
-      // nested list spacing
-      ' ul, ol': {
-         marginBottom: '0',
-      },
+   '.selfLink': {
+      color: 'gray.500',
+      order: '2', // swap the legacy order with flexbox
+      opacity: '0',
+      paddingLeft: 2,
+      transition: 'opacity ease-in-out',
+      transitionDuration: 'fast',
    },
 
-   a: {
-      color: 'brand.500',
+   blockquote: {
+      '&.featured': {
+         borderColor: '#fe6f15',
+         borderTopWidth: '1px',
+         borderBottomWidth: '1px',
+      },
 
-      '&:hover': {
-         textDecoration: 'underline',
+      '& > p': {
+         marginBlock: 2,
       },
    },
 
@@ -94,28 +124,27 @@ const chakraStyles: SystemStyleObject = {
       borderStyle: 'none',
    },
 
-   pre: {
-      paddingBlock: 0.5,
-      paddingInline: 1,
-      maxWidth: '100%',
-      overflow: 'auto',
+   p: {
+      lineHeight: '1.38',
+      alignSelf: 'stretch',
    },
 
-   blockquote: {
-      marginBlock: 6,
-      borderLeft: '1px solid',
-      borderLeftColor: 'gray.300',
-      paddingBlock: '0.25em',
-      paddingInline: '1em',
+   'ul, ol': {
+      marginTop: '1em',
+      marginLeft: '1em',
+      overflowX: 'auto', // clear child media floats
+      paddingLeft: '1em',
 
-      '&.featured': {
-         borderColor: '#fe6f15',
-         borderTopWidth: '1px',
-         borderBottomWidth: '1px',
+      li: {
+         _notFirst: {
+            marginTop: '1em',
+         },
       },
 
-      '& > p': {
-         marginBlock: 2,
+      // nested list spacing
+      'ul, ol': {
+         marginTop: '1em',
+         marginBottom: 0,
       },
    },
 
@@ -129,29 +158,26 @@ const chakraStyles: SystemStyleObject = {
       overflowX: 'auto',
    },
 
-   th: {
-      textAlign: 'left',
-   },
-
    tr: {
       verticalAlign: 'top',
 
-      '&:not(:first-of-type) > td': {
-         paddingTop: 3,
+      '&:not(:last-of-type) > td': {
+         paddingBottom: 3,
       },
    },
 
-   'td:not(:last-child)': {
-      paddingRight: 3,
+   td: {
+      _notLast: {
+         paddingRight: 3,
+      },
    },
 
    'lite-youtube': {
-      marginBlock: 2,
+      marginTop: 4,
       borderRadius: 'md',
       overflow: 'hidden',
+      border: '1px solid',
       borderColor: 'gray.300',
-      borderWidth: '1px',
-      borderStyle: 'solid',
       maxWidth: '100%',
 
       // override lite-youtube inline styles on mobile
@@ -222,11 +248,11 @@ const chakraStyles: SystemStyleObject = {
 
    '.imageBox': {
       borderRadius: 'md',
-      marginTop: { base: 4, sm: 6 },
       outline: '1px solid',
       outlineColor: 'gray.300',
       overflow: 'hidden',
       position: 'relative',
+      marginTop: { base: 4, sm: 6 },
       minWidth: 'min-content', // narrow image text-wrap fix
       width: 'fit-content',
 
@@ -244,6 +270,7 @@ const chakraStyles: SystemStyleObject = {
 
       '@media only screen and (max-width: 575px)': {
          width: 'fit-content',
+         marginTop: { base: 4, sm: 6 },
          marginInline: 'auto',
       },
 
@@ -315,44 +342,37 @@ const chakraStyles: SystemStyleObject = {
          paddingInline: 3,
       },
    },
+};
 
-   '.fa-svg-icon': {
-      svg: {
-         width: '1em',
-         height: '1em',
-         display: 'inline-block',
-
-         path: {
-            fill: 'currentColor',
-         },
-      },
-
-      '&.xs svg': {
-         width: '0.5em',
-         height: '0.5em',
-      },
-
-      '&.sm svg': {
-         width: '0.75em',
-         height: '0.75em',
-      },
-   },
+const commerceStyles: SystemStyleObject = {
+   ...sharedStyles,
 };
 
 export const PrerenderedHTML = chakra(function Prerendered({
    html,
+   styles,
    className,
 }: {
    html: string;
+   styles: 'troubleshooting' | 'commerce';
    className?: string;
 }) {
    useEffect(() => {
       import('lite-youtube-embed');
    }, []);
+
+   const applyStyles = () => {
+      if (styles === 'troubleshooting') {
+         return troubleshootingStyles;
+      } else if (styles === 'commerce') {
+         return commerceStyles;
+      }
+   };
+
    return (
       <Box
          className={`prerendered ${className}`}
-         sx={chakraStyles}
+         sx={applyStyles()}
          dangerouslySetInnerHTML={{ __html: html }}
       />
    );
