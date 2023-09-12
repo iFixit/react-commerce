2ec4683543e8e560a21912fb76cba82b9865223b
Federico Badini <federico@inkofpixel.com>
2023-09-12 09:02:50 +0200

    feat: add classNames helper

--- frontend/helpers/ui-helpers.ts
+++ frontend/helpers/ui-helpers.ts
@@ -4,3 +4,7 @@ export function markdownToHTML(markdown: string): string {
    const preserveNewlines = markdown.trim().replace(/\n/g, '<br />');
    return snarkdown(preserveNewlines);
 }
+
+export function classNames(...classes: any[]) {
+   return classes.filter(Boolean).join(' ');
+}
