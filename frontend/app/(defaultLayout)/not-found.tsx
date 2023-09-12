4c2c24b6ec04a1c878610aa053b71d149149c98c
Federico Badini <federico@inkofpixel.com>
2023-09-12 09:02:21 +0200

    feat: cleanup not-found page

--- frontend/app/(defaultLayout)/not-found.tsx
+++ frontend/app/(defaultLayout)/not-found.tsx
@@ -1,10 +1,8 @@
-'use client';
-
-import { Center, Divider, Heading, Text, VStack } from '@chakra-ui/react';
 import { AppProviders } from '@components/common/AppProviders';
 import { DEFAULT_STORE_CODE } from '@config/env';
 import { getLayoutServerSideProps } from '@layouts/default/server';
 import IFixitPageFrame from './components/IFixitPageFrame';
+import { NotFoundPage } from './components/not-found-page';
 
 export default async function NotFound() {
    const layoutPropsPromise = getLayoutServerSideProps({
@@ -16,40 +14,7 @@ export default async function NotFound() {
    return (
       <AppProviders>
          <IFixitPageFrame {...layoutProps}>
-            <Center
-               flexGrow={1}
-               paddingTop="16px"
-               paddingBottom="60px"
-               paddingLeft="20px"
-               paddingRight="20px"
-            >
-               <VStack>
-                  <Heading
-                     as="h2"
-                     fontSize={{ base: '4xl', md: '5xl' }}
-                     fontWeight="medium"
-                  >
-                     404
-                  </Heading>
-                  <Text fontWeight="medium" pb="3" textTransform="uppercase">
-                     Page not found
-                  </Text>
-                  <Divider
-                     borderWidth="1px"
-                     borderColor="red.500"
-                     width="50px"
-                  />
-                  <Text
-                     pt="3"
-                     overflowWrap="break-word"
-                     align="center"
-                     maxW="360px"
-                  >
-                     We can&apos;t find the page you&apos;re looking for.
-                     Whoops.
-                  </Text>
-               </VStack>
-            </Center>
+            <NotFoundPage />
          </IFixitPageFrame>
       </AppProviders>
    );
