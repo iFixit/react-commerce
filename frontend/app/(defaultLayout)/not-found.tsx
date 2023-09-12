e8a61af16b039bb45afb4ef63758ee79e60815ff
Federico Badini <federico@inkofpixel.com>
2023-09-12 16:28:06 +0200

    feat: tailwind + server component not-found page

--- frontend/app/(defaultLayout)/not-found.tsx
+++ frontend/app/(defaultLayout)/not-found.tsx
@@ -2,7 +2,6 @@ import { AppProviders } from '@components/common/AppProviders';
 import { DEFAULT_STORE_CODE } from '@config/env';
 import { getLayoutServerSideProps } from '@layouts/default/server';
 import IFixitPageFrame from './components/IFixitPageFrame';
-import { NotFoundPage } from './components/not-found-page';
 
 export default async function NotFound() {
    const layoutPropsPromise = getLayoutServerSideProps({
@@ -14,7 +13,19 @@ export default async function NotFound() {
    return (
       <AppProviders>
          <IFixitPageFrame {...layoutProps}>
-            <NotFoundPage />
+            <div className="flex flex-col flex-grow items-center justify-center px-5 pt-4 pb-16">
+               <div className="flex flex-col items-center">
+                  <h2 className="text-4xl md:text-5xl font-medium">404</h2>
+                  <span className="pb-3 font-medium uppercase ">
+                     Page not found
+                  </span>
+                  <hr className="border border-red-500 w-[50px] opacity-60" />
+                  <span className="pt-3 break-words text-center max-w-[360px]">
+                     We can&apos;t find the page you&apos;re looking for.
+                     Whoops.
+                  </span>
+               </div>
+            </div>
          </IFixitPageFrame>
       </AppProviders>
    );
