5839f4ae902b21967017e6bc7cf0fa2a6541993b
Federico Badini <federico@inkofpixel.com>
2023-09-12 11:13:45 +0200

    feat: move classnames utility

--- frontend/helpers/ui-helpers.ts
+++ frontend/helpers/ui-helpers.ts
@@ -4,7 +4,3 @@ export function markdownToHTML(markdown: string): string {
    const preserveNewlines = markdown.trim().replace(/\n/g, '<br />');
    return snarkdown(preserveNewlines);
 }
-
-export function classNames(...classes: any[]) {
-   return classes.filter(Boolean).join(' ');
-}
