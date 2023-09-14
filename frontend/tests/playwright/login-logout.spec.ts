753fa32e92da0221a901b8dbf8526ec2db4c9bbc
batbattur <bat@ifixit.com>
2023-09-14 15:30:36 -0700

    Check session cookie to check logged in status
    Initially, when the browser opens, there is no session cookie set.
    When the user logs in through cominor, we check the session cookies
    on cominor and next.js app and make sure they're the same.
    
    After the user logs out, we check the session cookie has changed.

--- frontend/tests/playwright/login-logout.spec.ts
+++ frontend/tests/playwright/login-logout.spec.ts
@@ -4,6 +4,13 @@ test.describe('Login Logout', () => {
    test('Using User Icon Button', async ({ page }) => {
       await page.goto('/Tools');
 
+      // make sure the session cookie does not exist
+      const initialCookies = await page.context().cookies();
+      const initialSessionCookie = initialCookies.find(
+         (cookie) => cookie.name === 'session'
+      );
+      expect(initialSessionCookie).toBeUndefined();
+
       // click user icon button and login on cominor
       await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
       await page.getByRole('link', { name: 'Login' }).click();
@@ -14,16 +21,37 @@ test.describe('Login Logout', () => {
       await page.locator('#password').fill('testpassword');
       await page.getByRole('button', { name: 'Log In' }).click();
 
+      // on cominor: get session cookie
+      const cominorCookies = await page.context().cookies();
+      const cominorSession = cominorCookies.find(
+         (cookie) => cookie.name === 'session'
+      );
+      expect(cominorSession).toBeDefined();
+
       // navigate back to next.js app
       await page.goto('/Tools');
       // assert login button is not visible when logged in
       await expect(page.getByRole('link', { name: 'Login' })).not.toBeVisible();
+
+      // get existing cookies on next.js app and make sure the same session cookie exists
+      const cookies = await page.context().cookies();
+      const sessionCookie = cookies.find((cookie) => cookie.name === 'session');
+      expect(sessionCookie).toBeDefined();
+      expect(cominorSession!.value).toEqual(sessionCookie!.value);
 
       await page.getByRole('button', { name: 'Open user menu' }).click();
       await expect(
          page.getByRole('menuitem', { name: 'Log Out' })
       ).toBeVisible();
       await page.getByRole('menuitem', { name: 'Log Out' }).click();
+
+      // get session cookie value after logout and make sure it is different
+      const cookiesAfter = await page.context().cookies();
+      const sessionCookieAfter = cookiesAfter.find(
+         (cookie) => cookie.name === 'session'
+      );
+      expect(sessionCookieAfter!.value).not.toEqual(sessionCookie!.value);
+      expect(sessionCookieAfter!.value).not.toEqual(cominorSession!.value);
 
       await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
       await expect(
