47efd9d17bface778882daf863e335c65b16c468
Massimo De Marchi <massimo@inkofpixel.com>
2023-09-21 14:22:10 +0200

    wip

--- backend/src/plugins/addons/server/bootstrap.ts
+++ backend/src/plugins/addons/server/bootstrap.ts
@@ -11,20 +11,20 @@ export default async ({ strapi }: { strapi: Strapi }) => {
       try {
          const seedService = getAddonsService(strapi, 'seed');
          await seedService.createAdminUser();
-         // await seedService.downloadBackup({
-         //    strapiOrigin: FALLBACK_STRAPI_ORIGIN,
-         // });
-         // backgroundImport({ strapi })
-         //    .then((logs) => {
-         //       strapi.log.info('🌱 Seeding completed!');
-         //       if (logs.length > 0) {
-         //          strapi.log.info(`🌱 Seeding logs:\n${logs}`);
-         //       }
-         //    })
-         //    .catch((error) => {
-         //       strapi.log.error('💥 Error while importing from backup');
-         //       strapi.log.error(error.message);
-         //    });
+         await seedService.downloadBackup({
+            strapiOrigin: FALLBACK_STRAPI_ORIGIN,
+         });
+         backgroundImport({ strapi })
+            .then((logs) => {
+               strapi.log.info('🌱 Seeding completed!');
+               if (logs.length > 0) {
+                  strapi.log.info(`🌱 Seeding logs:\n${logs}`);
+               }
+            })
+            .catch((error) => {
+               strapi.log.error('💥 Error while importing from backup');
+               strapi.log.error(error.message);
+            });
       } catch (err: any) {
          strapi.log.error('💥 Error while seeding database');
          strapi.log.error(err.message);
@@ -39,7 +39,7 @@ interface BackgroundImportInput {
 async function backgroundImport({ strapi }: BackgroundImportInput) {
    // Wait for the Strapi server to start, since we need it for the Strapi import
    // tool to work.
-   await delay(50);
+   await delay(5000);
    const seedService = getAddonsService(strapi, 'seed');
    return seedService.importBackup();
 }
