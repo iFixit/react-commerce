492668f85388b519e4004c164d12fcfafd5a305a
Jarred Stelfox <jarstelfox@mac.com>
2023-10-24 13:00:03 -0700

    SentryError Fixer: Really fix
    This

--- packages/eslint/rules/no-new-error.js
+++ packages/eslint/rules/no-new-error.js
@@ -23,8 +23,25 @@ module.exports = {
                   message:
                      'Avoid using new Error; consider using SentryError instead to attach contexts, tags, and more.',
                   fix: function (fixer) {
-                     // Suggest replacing 'new Error' with 'new SentryError'
-                     return fixer.replaceText(node, 'new SentryError');
+                     // Suggest replacing 'new Error(...);' with 'new SentryError(...);'
+
+                     const isImported = context
+                        .getScope()
+                        .through.some(
+                           (ref) => ref.identifier.name === 'SentryError'
+                        );
+
+                     if (!isImported) {
+                        return [
+                           fixer.insertTextBeforeRange(
+                              [0, 0],
+                              "import { SentryError } from '@ifixit/sentry';\n"
+                           ),
+                           fixer.replaceText(node.callee, 'SentryError'),
+                        ];
+                     }
+
+                     return fixer.replaceText(node.callee, 'SentryError');
                   },
                });
             }
