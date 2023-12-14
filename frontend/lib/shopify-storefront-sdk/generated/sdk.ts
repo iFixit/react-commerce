6446e82a2b886a853c35fd4a7343a19d4541c1fe
Massimo De Marchi <massimo@inkofpixel.com>
2023-12-14 10:55:05 +0100

    chore: Update out of sync sdk

--- frontend/lib/shopify-storefront-sdk/generated/sdk.ts
+++ frontend/lib/shopify-storefront-sdk/generated/sdk.ts
@@ -1357,7 +1357,7 @@ export type Checkout = Node & {
    note?: Maybe<Scalars['String']>;
    /** The resulting order from a paid checkout. */
    order?: Maybe<Order>;
-   /** The Order Status Page for this Checkout, null when checkout isn't completed. */
+   /** The <b>Order status</b> page for this Checkout, null when checkout isn't completed. */
    orderStatusUrl?: Maybe<Scalars['URL']>;
    /** The amount left to be paid. This is equal to the cost of the line items, taxes, and shipping, minus discounts and gift cards. */
    paymentDue: MoneyV2;
