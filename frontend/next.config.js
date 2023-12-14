3d0b3392879469a2db95a803052a0d95ca7f394d
Massimo De Marchi <massimo@inkofpixel.com>
2023-12-14 10:05:27 +0100

    refactor: Turn on swcMinify

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -194,7 +194,7 @@ const moduleExports = {
          disableClientWebpackPlugin: true,
       }),
    },
-   swcMinify: false,
+   swcMinify: true,
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
