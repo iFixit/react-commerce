e1d4bc8737d1f53b65da8d7283b744e2159e1a17
Massimo De Marchi <massimo@inkofpixel.com>
2024-01-13 13:10:09 +0100

    refactor(product page): Port metadata to app router

--- frontend/app/(defaultLayout)/products/app-router/[handle]/page.tsx
+++ frontend/app/(defaultLayout)/products/app-router/[handle]/page.tsx
@@ -1,8 +1,17 @@
 import { DEFAULT_STORE_CODE, IFIXIT_ORIGIN } from '@config/env';
 import { flags } from '@config/flags';
-import Product from '@pages/api/nextjs/cache/product';
+import { invariant } from '@ifixit/helpers';
+import Product, {
+   type Product as ProductType,
+} from '@pages/api/nextjs/cache/product';
 import { devSandboxOrigin, shouldSkipCache } from 'app/_helpers/app-helpers';
+import { defaultVariantId, imagesFor } from 'app/_helpers/product-helpers';
+import type { Metadata } from 'next';
 import { notFound, redirect } from 'next/navigation';
+import {
+   ProductBreadcrumbsJsonLDScript,
+   ProductJsonLDScript,
+} from './json-ld-scripts';
 
 export interface ProductPageProps {
    params: {
@@ -33,5 +42,89 @@ export default async function ProductPage({
 
    if (data.__typename === 'ProductRedirect') redirect(data.target);
 
-   return <div>Product: {data.title}</div>;
+   return (
+      <>
+         <ProductBreadcrumbsJsonLDScript product={data} />
+         <ProductJsonLDScript
+            product={data}
+            selectedVariantId={selectedVariantId(data, searchParams)}
+         />
+         <div>Product: {data.title}</div>
+      </>
+   );
+}
+
+export async function generateMetadata({
+   params,
+   searchParams,
+}: ProductPageProps): Promise<Metadata> {
+   const product = await Product.get(
+      {
+         handle: params.handle,
+         storeCode: DEFAULT_STORE_CODE,
+         ifixitOrigin: devSandboxOrigin() ?? IFIXIT_ORIGIN,
+      },
+      { forceMiss: shouldSkipCache(searchParams) }
+   );
+
+   if (product == null || product.__typename !== 'Product') return {};
+
+   return {
+      title: product.metaTitle,
+      description: product.shortDescription,
+      openGraph: {
+         title: product.metaTitle ?? undefined,
+         description: product.shortDescription ?? undefined,
+         type: 'website',
+         url: productCanonicalUrl(),
+         images: productImages().map((image) => ({ url: image.url })),
+      },
+      robots: shouldBlockCrawlers()
+         ? { follow: false, index: false }
+         : undefined,
+      alternates: {
+         canonical: shouldBlockCrawlers() ? undefined : productCanonicalUrl(),
+         languages: alternateLanguages(),
+      },
+   };
+
+   function alternateLanguages(): Record<string, string> {
+      invariant(product?.__typename === 'Product');
+      const result: Record<string, string> = {};
+      if (product.enabledDomains) {
+         for (const store of product.enabledDomains) {
+            for (const locale of store.locales) {
+               result[locale] = `${store.domain}/products/${product.handle}`;
+            }
+         }
+      }
+      return result;
+   }
+
+   function productCanonicalUrl(): string {
+      invariant(product?.__typename === 'Product');
+      return `${IFIXIT_ORIGIN}/products/${product.handle}`;
+   }
+
+   function shouldBlockCrawlers() {
+      invariant(product?.__typename === 'Product');
+      return isOnlyForPros() || !product.isEnabled || product.noindex;
+   }
+
+   function isOnlyForPros() {
+      invariant(product?.__typename === 'Product');
+      return product.tags.find((tag: string) => tag === 'Pro Only') != null;
+   }
+
+   function productImages() {
+      invariant(product?.__typename === 'Product');
+      return imagesFor(product, selectedVariantId(product, searchParams));
+   }
+}
+
+function selectedVariantId(
+   product: ProductType,
+   searchParams: ProductPageProps['searchParams']
+) {
+   return searchParams.variant ?? defaultVariantId(product);
 }
