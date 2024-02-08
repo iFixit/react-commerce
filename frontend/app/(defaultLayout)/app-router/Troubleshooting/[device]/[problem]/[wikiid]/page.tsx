31a915d3e2bf6f567634e9ea24dde1098ffcf7ab
Massimo De Marchi <massimo@inkofpixel.com>
2024-02-08 10:12:26 +0100

    refactor(troubleshooting): Add app router page under feature flags

---
+++ frontend/app/(defaultLayout)/app-router/Troubleshooting/[device]/[problem]/[wikiid]/page.tsx
@@ -0,0 +1,23 @@
+import { flags } from '@config/flags';
+import { notFound } from 'next/navigation';
+
+interface WikiPageProps {
+   params: {
+      device: string;
+      problem: string;
+      wikiid: string;
+   };
+   searchParams: {
+      disableCacheGets?: string | string[] | undefined;
+   };
+}
+
+export default async function WikiPage({ params }: WikiPageProps) {
+   if (!flags.APP_ROUTER_TROUBLESHOOTING_PAGE_ENABLED) notFound();
+
+   return (
+      <div>
+         WikiPage: <code>{JSON.stringify(params, null, 2)}</code>
+      </div>
+   );
+}
