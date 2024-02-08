17c5f12ead3fbfd6449e617f0fe49c8d8511220c
Massimo De Marchi <massimo@inkofpixel.com>
2024-02-08 16:10:01 +0100

    refactor(troubleshooting page): Refactor metadata to app router format

--- frontend/app/(defaultLayout)/app-router/Troubleshooting/[device]/[problem]/[wikiid]/page.tsx
+++ frontend/app/(defaultLayout)/app-router/Troubleshooting/[device]/[problem]/[wikiid]/page.tsx
@@ -2,6 +2,7 @@ import { flags } from '@config/flags';
 import { notFound, redirect } from 'next/navigation';
 import { findProblemWiki } from './data';
 import { invariant } from '@ifixit/helpers';
+import type { Metadata } from 'next';
 
 export const fetchCache = 'default-no-store';
 
@@ -52,4 +53,23 @@ export default async function ProblemWikiPage({
       if (Object.keys(searchParams).length === 0) return '';
       return `?${new URLSearchParams(searchParams)}`;
    }
+}
+
+export async function generateMetadata({
+   params,
+}: ProblemWikiPageProps): Promise<Metadata> {
+   const problemWiki = await findProblemWiki(params.wikiid);
+
+   if (problemWiki == null) return {};
+
+   return {
+      title: `${problemWiki.title} - iFixit Troubleshooting`,
+      description: problemWiki.metaDescription,
+      keywords: problemWiki.metaKeywords,
+      robots: { index: true, follow: true },
+      alternates: {
+         canonical: problemWiki.canonicalUrl,
+         languages: problemWiki.hreflangUrls,
+      },
+   };
 }
